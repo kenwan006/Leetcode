017dab2a8ded60950a2e2ff1fee2bff356573b8c
Chao <42952910+kenwan006@users.noreply.github.com>
2022-09-08 18:33:46 -0700

    Time: 1 ms (95.91%), Space: 42.4 MB (74.66%) - LeetHub

---
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.java
@@ -0,0 +1,39 @@
+/** Backtracking
+- The digit at ith position of digits maps to a string.
+- For loop to check each letter of the string.
+  - Every time check one letter, add it to the track,increment start by one 
+  - Implement function of backtrack 
+  - Once backtrack is done, remove the letter from the track
+
+*/
+
+class Solution {
+    String[] mapping = new String[] {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+    List<String> res = new LinkedList<>();
+    
+    public List<String> letterCombinations(String digits) {
+        if (digits.isEmpty()) return res;
+        StringBuilder track = new StringBuilder();
+        backtrack(digits, 0, track);
+        return res;
+    }
+    
+    public void backtrack(String digits, int start, StringBuilder track) {
+        // base case
+        if (track.length() == digits.length()) {
+            res.add(track.toString());
+            return;
+        }
+        
+        for (int i = start; i < digits.length(); i++) {
+            String s = mapping[digits.charAt(i) - '0'];
+            for (char ch : s.toCharArray()) {
+                track.append(ch);
+                backtrack(digits, i + 1, track);
+                track.deleteCharAt(track.length() - 1);
+            }
+        }
+    }
+    
+    
+}

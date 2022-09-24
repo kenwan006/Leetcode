62911ffbc9dfd22d02ccd0a644758516d5cd6d09
Chao <42952910+kenwan006@users.noreply.github.com>
2022-09-23 18:25:06 -0700

    Time: 9 ms (41.19%), Space: 49.9 MB (54.65%) - LeetHub

---
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.java
@@ -0,0 +1,40 @@
+/** Recursion
+* If both p and q smaller than root, then check the root.left;
+* If both p and q greater than root, then check the root.right;
+* Otherwise either p and q exist in the diffrent branches, or p or q equals to root, but will always return the root.
+*/
+
+/** Iterative 
+* This approach saves space.
+*/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        while (root != null) {
+            if (p.val < root.val && q.val < root.val) root = root.left;
+            else if (p.val > root.val && q.val > root.val) root = root.right;
+            else return root;
+        }
+        return root;
+    }
+}
+
+
+/** Recursion
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (p.val < root.val && q.val < root.val) return lowestCommonAncestor(root.left, p, q);
+        if (p.val > root.val && q.val > root.val) return lowestCommonAncestor(root.right, p, q);
+        return root;
+    }
+}
+*/

41d9fa72605168814d35aa43cb6bd114bf4bd539
Chao <42952910+kenwan006@users.noreply.github.com>
2022-09-26 12:32:06 -0700

    Time: 5 ms (55.57%), Space: 56.4 MB (39.23%) - LeetHub

--- 169-majority-element/169-majority-element.java
+++ 169-majority-element/169-majority-element.java
@@ -1,9 +1,18 @@
+/** Boyer- Moore Voting
+* Remember the premise is there exists such majority item!
+* We define a count as a marker
+* Iterate over the nums, when count = 0, choose current num as the candidate. Keep checking the next num, if equals to candidate, then count++, otherwise count--. 
+* Because there exists such majority item, which means the target item has more frequency than the total appearance of all the other non candidates, so we can assure the candidate by the end of the iterations will be the target. 
+eg,[7, 7, 5, 7, 5, 1 | 5, 7 | 5, 5, 7, 7 | 7, 7, 7, 7] target = 7
+eg,[7, 7, 5, 7, 5, 1 | 5, 7 | 5, 5, 7, 7 | 5, 5, 5, 5] target = 5
+*/
 class Solution {
     public int majorityElement(int[] nums) {
-        int count = 0, candidate = 0;
+        Integer candidate = null; // do not use 0 because num could be negative
+        int count = 0;
         for (int num : nums) {
-            if(count == 0) candidate = num;
-            count += (num == candidate? 1 : -1);
+            if (count == 0) candidate = num;
+            count += num == candidate ? 1 : -1;
         }
         return candidate;
     }

aa35d1de4d583949677854879b34fc329b9e59e4
Chao <42952910+kenwan006@users.noreply.github.com>
2022-09-26 18:27:02 -0700

    Time: 0 ms (100.00%), Space: 42.2 MB (88.37%) - LeetHub

--- 543-diameter-of-binary-tree/543-diameter-of-binary-tree.java
+++ 543-diameter-of-binary-tree/543-diameter-of-binary-tree.java
@@ -16,16 +16,17 @@
 class Solution {
     private int diameter = 0;
     public int diameterOfBinaryTree(TreeNode root) {
-        if(root == null) return 0;
-        depth(root);
-        return diameter; 
+        maxDepth(root);
+        return diameter;
     }
-    
-    public int depth(TreeNode node) {
-        if(node == null) return 0;
-        int leftDepth = depth(node.left);
-        int rightDepth = depth(node.right);
-        diameter = Math.max(diameter, leftDepth + rightDepth); // check the potential diameter at each node
-        return Math.max(leftDepth, rightDepth) + 1;
+    public int maxDepth(TreeNode root) {
+        if (root == null) return 0;
+        int left = maxDepth(root.left);
+        int right = maxDepth(root.right);
+        
+        // post-order traverse, keep track of latest max diameter when visiting each node
+        diameter = Math.max(diameter, left + right);
+        
+        return Math.max(left, right) + 1;
     }
 }

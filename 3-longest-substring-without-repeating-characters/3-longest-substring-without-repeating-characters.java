e62cc3e175bbda37a3b8bca399a1ef97143415e5
Chao <42952910+kenwan006@users.noreply.github.com>
2022-09-29 14:49:05 -0700

    Time: 11 ms (72.50%), Space: 44.5 MB (66.04%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.java
@@ -1,15 +1,21 @@
+/** Sliding window
+* Use a sliding window constrained by start index i and end index j
+* Use a hashmap to map each character to its most latest position
+* Check the next letter by incrementing j. If it's not in the hashmap, add it to the hashmap, and update the max length; 
+* If it's already in the hashmap, chekc if it's wintin the window. i = max{i, map.get(ch) + 1}.
+*/
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        HashMap<Character, Integer> map = new HashMap<>();
         int i = 0, maxLen = 0;
-        for(int j = 0; j < s.length(); j++) {
-            if(map.containsKey(s.charAt(j))) {
-                i = Math.max(i, map.get(s.charAt(j)) + 1);// update the starting index of sliding window           
-            }
-            map.put(s.charAt(j), j);
-            maxLen = Math.max(maxLen, j - i + 1);
+        Map<Character, Integer> map = new HashMap<>();
+        
+        for (int j = 0; j < s.length(); j++) {
+            char ch = s.charAt(j);
+            if (map.containsKey(ch)) 
+                i = Math.max(i, map.get(ch) + 1);
+            map.put(ch, j);
+            maxLen = Math.max(maxLen, j - i + 1); //update the maxLen after each iteration
         }
         return maxLen;
     }
 }
-  

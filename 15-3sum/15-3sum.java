540d831c8f2d3bf948515d96225cfcdf4f6e6d67
Chao <42952910+kenwan006@users.noreply.github.com>
2022-09-30 10:43:48 -0700

    Time: 1502 ms (5.02%), Space: 119.8 MB (17.00%) - LeetHub

--- 15-3sum/15-3sum.java
+++ 15-3sum/15-3sum.java
@@ -1,24 +1,20 @@
+/** Two pointers
+* sort the nums first;
+* fix one number,num[i],  and use two pointer lo and hi to represent the other two candidates
+*/
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> result = new ArrayList<>();
+        Set<List<Integer>> res = new HashSet<>(); // avoid adding repeated list to the result
         Arrays.sort(nums);
-        for(int i = 0; i < nums.length-2; i++) {
+        for (int i = 0; i < nums.length - 2; i++) {
             int lo = i + 1, hi = nums.length - 1;
-            if(nums[i] > 0) break; 
-            if(i >= 1 && nums[i] == nums[i-1]) continue; //skip the current iteration if next one is same as the previous
-            while(lo < hi) {
-                int sum = nums[lo] + nums[hi] + nums[i];
-                if(sum > 0) hi--;
-                else if(sum < 0) lo++;
-                else {
-                    result.add(Arrays.asList(nums[i], nums[lo], nums[hi]));
-                    while(lo < hi && nums[lo] == nums[lo+1]) lo++;
-                    while(lo < hi && nums[hi] == nums[hi-1]) hi--;
-                    lo++;
-                    hi--;
-                }
+            while (lo < hi) {
+                int sum = nums[i] + nums[lo] + nums[hi];
+                if (sum > 0) hi--;
+                else if (sum < 0) lo++;
+                else res.add(Arrays.asList(nums[i], nums[lo++], nums[hi--]));
             }
         }
-        return result;
+        return new ArrayList<>(res);
     }
 }

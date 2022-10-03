a08190f7df4384bd6bb91cf6326459216148947f
Chao <42952910+kenwan006@users.noreply.github.com>
2022-10-03 12:01:05 -0700

    Time: 3 ms (64.08%), Space: 57.3 MB (75.81%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.java
+++ 238-product-of-array-except-self/238-product-of-array-except-self.java
@@ -1,18 +1,21 @@
+/** Iterative
+* The first for loop, for the nums[i], calculate product of all the numbers at the left of nums[i], which is nums[0] * nums[1] *...*nums[i-1]
+* The second for loop, starting from the end, for the nums[i], calculate product of all numbers at the right of nums[i]
+*/
 class Solution {
     public int[] productExceptSelf(int[] nums) {
         int[] ans = new int[nums.length];
         ans[0] = 1;
-        for(int i = 1; i < ans.length; i++) {
-            ans[i] = ans[i -1] * nums[i - 1]; // product of all elements at left of nums[i]
+        
+        for (int i = 1; i < nums.length; i++) {
+            ans[i] = ans[i - 1] * nums[i - 1];
         }
         
-        // update ans by multiply the factor R
-        int R = 1; 
-        for(int i = ans.length - 1; i >= 0; i--) {
-            ans[i] = ans[i] * R;
-            R = R * nums[i];
+        int R = 1; // R represents product of all nums at right of nums[i]
+        for (int i = nums.length - 2; i >= 0; i--) {
+            R = R * nums[i + 1];
+            ans[i] = ans[i] * R; 
         }
-        
         return ans;
     }
 }

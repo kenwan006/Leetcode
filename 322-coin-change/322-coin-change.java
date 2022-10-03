ce9f52a63ab88adcbd891087a44245e024a3cc3f
Chao <42952910+kenwan006@users.noreply.github.com>
2022-10-03 10:51:48 -0700

    Time: 32 ms (51.32%), Space: 45 MB (60.57%) - LeetHub

--- 322-coin-change/322-coin-change.java
+++ 322-coin-change/322-coin-change.java
@@ -1,15 +1,22 @@
+/** DP
+* dp[i] means the fewest number of coins to make up to the amount i
+* dp[i] = min{dp[i], dp[i - coin_j]}
+*/
 class Solution {
     public int coinChange(int[] coins, int amount) {
-        int[] dp = new int [amount + 1];
-        Arrays.fill(dp, amount + 1);
+        int[] dp = new int[amount + 1]; // need to return dp[amount]
+        Arrays.fill(dp, amount + 1); // amount + 1 is the lagest number if choosing 1 every time
+        
+        //base case
         dp[0] = 0;
         
-        for (int i = 0; i < amount + 1; i++) {
-            for(int coin : coins) {
-                if(i - coin < 0) continue;
+        for (int i = 1; i < amount + 1; i++) {
+            for (int coin : coins) { // check every choice of coins for each amount
+                if (i - coin < 0) continue;
                 dp[i] = Math.min(dp[i], dp[i - coin] + 1);
             }
         }
-        return (dp[amount] == amount + 1) ? -1 : dp[amount];
+        
+        return dp[amount] == amount + 1? -1 : dp[amount];
     }
 }

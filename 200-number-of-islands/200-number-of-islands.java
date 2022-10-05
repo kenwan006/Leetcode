889c20e5131b444e935cd6e0adda8e74b8a16d1a
Chao <42952910+kenwan006@users.noreply.github.com>
2022-10-04 18:29:47 -0700

    Time: 4 ms (79.26%), Space: 57.6 MB (36.87%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.java
+++ 200-number-of-islands/200-number-of-islands.java
@@ -1,22 +1,24 @@
+/** DFS
+* Iterate over the grid, if the gird[i][j] = 1, count the island, count++, and run the dfs(i, j)
+* dfs - change all the neighboring "1" of grid[i][j] to 0;
+*/
 class Solution {
     public int numIslands(char[][] grid) {
-        int m = grid.length, n = grid[0].length, count = 0;
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++){
-                //Once found '1', increment count by 1 and do DFS
-                if(grid[i][j] == '1') {
+        int count = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == '1') {
                     count++;
-                    dfs(grid, i , j);
+                    dfs(grid, i, j);
                 }
             }
         }
         return count;
     }
     
-    public void dfs(char[][] grid, int i, int j) {
-        int m = grid.length, n = grid[0].length;
-        if (i < 0 || i > m - 1 || j < 0 || j > n - 1 || grid[i][j] == '0') return; // base case
-        grid[i][j] = '0'; // change '1' to '0'
+    private void dfs(char[][] grid, int i, int j) {
+        if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] == '0' ) return;
+        grid[i][j] = '0';
         dfs(grid, i - 1, j);
         dfs(grid, i + 1, j);
         dfs(grid, i, j - 1);

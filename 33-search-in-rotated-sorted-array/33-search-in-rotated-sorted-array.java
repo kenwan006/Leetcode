d2bc0f98eec086dbf0c69ba99ebef6f90ae470db
Chao <42952910+kenwan006@users.noreply.github.com>
2022-10-05 17:16:38 -0700

    Time: 1 ms (79.55%), Space: 42.9 MB (28.81%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.java
@@ -1,3 +1,8 @@
+/** Binary Search
+* Check which half is sorted (in ascending order).
+* If the mid > start, it means the left half is sorted; otherwise the right half is sorted
+* Binary search
+*/
 class Solution {
     public int search(int[] nums, int target) {
         int lo = 0, hi = nums.length - 1;
@@ -5,14 +10,14 @@ class Solution {
             int mid = lo + (hi - lo) / 2;
             if (nums[mid] == target) return mid;
             
-            if (nums[lo] <= nums[mid]) { //left half is sorted
-                if (target >= nums[lo] && target < nums[mid]) {//target is between low and middle
+            if (nums[mid] >= nums[lo]) { // the left half is sorted
+                if (target >= nums[lo] && target < nums[mid]) {
                     hi = mid - 1;
                 } else {
                     lo = mid + 1;
                 }
-            } else { //right half is sorted
-                if(target > nums[mid] && target <= nums[hi]) { // target is between middle and high
+            } else { // the right half is sorted
+                if (target > nums[mid] && target <= nums[hi]) { // need to include = num[hi]
                     lo = mid + 1;
                 } else {
                     hi = mid - 1;

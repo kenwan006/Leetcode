29df2a02419f1e4120a203298b3d49ce1b2fd9db
Chao <42952910+kenwan006@users.noreply.github.com>
2022-10-07 15:24:00 -0700

    Time: 3 ms (50.43%), Space: 44.9 MB (36.66%) - LeetHub

---
+++ 46-permutations/46-permutations.java
@@ -0,0 +1,27 @@
+class Solution {
+    List<List<Integer>> res = new LinkedList<>();
+    LinkedList<Integer> track = new LinkedList<>();
+    boolean[] visited;
+    
+    public List<List<Integer>> permute(int[] nums) {
+        visited = new boolean[nums.length];
+        backtrack(nums);
+        return res;
+    }
+    
+    private void backtrack(int[] nums) {
+        if (track.size() == nums.length) {
+            res.add(new LinkedList(track));
+            return;
+        }
+        
+        for (int i = 0; i < nums.length; i++) {
+            if (visited[i]) continue;
+            visited[i] = true;
+            track.add(nums[i]);
+            backtrack(nums);
+            track.removeLast();
+            visited[i] = false;
+        }
+    }
+}

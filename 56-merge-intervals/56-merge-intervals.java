73415375764ae7c51f4bf3eabcf062b3c03f0a8c
Chao <42952910+kenwan006@users.noreply.github.com>
2022-10-07 17:12:42 -0700

    Time: 19 ms (31.91%), Space: 55.8 MB (7.57%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.java
+++ 56-merge-intervals/56-merge-intervals.java
@@ -1,23 +1,25 @@
+/** Two pointers */
+
 class Solution {
     public int[][] merge(int[][] intervals) {
-        LinkedList<int[]> res = new LinkedList<>();
+        List<int[]> res = new LinkedList<>();
         
-        //sort the array
-        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
+        //sort the array by the start
+        Arrays.sort(intervals, (a, b) -> (a[0] - b[0]));
         
-        //use two pointers - start and end
+        //merge
         int start = intervals[0][0], end = intervals[0][1];
-        for (int[] interval : intervals) {
-            if (interval[0] > end) {
-                res.add(new int[]{start, end}); // no intersection between current interval and new interval
-                start = interval[0];
-                end = interval[1]; 
-            } else if (interval[1] > end) {
-                end = interval[1];
-            } 
+        for (int[] a : intervals) {
+            if (end < a[0] ) {
+                res.add(new int[]{start, end});
+                start = a[0];
+                end = a[1]; //move the pointer
+            } else {
+                end = Math.max(end, a[1]);
+            }
         }
+        res.add(new int[]{start, end});
         
-        res.add(new int[]{start, end}); 
-        return res.toArray(new int[res.size()][]);
+        return res.toArray(new int[res.size()][2]);
     }
 }

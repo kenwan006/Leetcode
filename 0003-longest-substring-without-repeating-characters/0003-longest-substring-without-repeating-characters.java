47ed46df518b5e9c2fc3bf204cea68912ac37de5
Chao <42952910+kenwan006@users.noreply.github.com>
2022-10-21 09:49:52 -0700

    Time: 12 ms (68.98%), Space: 44.3 MB (72.79%) - LeetHub

---
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -0,0 +1,20 @@
+/** Sliding window
+* Use a sliding window constrained by start index i and end index j
+* Use a hashmap to map each character to its *most latest position*
+* Check the next letter by incrementing j. If it's not in the hashmap, add it to the hashmap, and update the max length; 
+* If it's already in the hashmap, chekc if it's wintin the window. i = max{i, map.get(ch) + 1}.
+*/
+
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        int i = 0, maxLen = 0;
+        Map<Character, Integer> map = new HashMap<>();
+        for (int j = 0; j < s.length(); j++) {
+            char ch = s.charAt(j);
+            if (map.containsKey(ch)) i = Math.max(i, map.get(ch) + 1);
+            map.put(ch, j);
+            maxLen = Math.max(maxLen, j - i + 1);
+        }
+        return maxLen;
+    }
+}

622aac3b2f09763125e309ec1f156f1dc4d7f60b
Chao <42952910+kenwan006@users.noreply.github.com>
2022-11-02 15:03:41 -0700

    Time: 1 ms (74.99%), Space: 46 MB (34.62%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -9,6 +9,8 @@
  *     }
  * }
  */
+
+/** HashSet 
 public class Solution {
     public boolean hasCycle(ListNode head) {
         Set<ListNode> set = new HashSet<>();
@@ -20,4 +22,27 @@ public class Solution {
         return false;
     }
 }
+//Time: O(n); Space: O(n)
+// Two pointer is a better method as the space complexity can be optimized to O(1)
+*/
 
+/** Two pointers - fast and slow
+* Use two pointers, fast and slow
+* Move slow by one step and fast two steps every time. If there is no cycle, slow will never catch up with fast before it comes to the end; otherwise they will meet at some point.
+*/
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        if (head == null) return false;
+        ListNode slow = head;
+        ListNode fast = head.next;
+        while (slow != fast) { // until slow catches up with fast
+            if (fast == null || fast.next == null) { //fast pointer comes to the end of the linkedlist
+                return false;
+            }
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return true;
+    }
+}
+//Time: O(n); Space: O(1)

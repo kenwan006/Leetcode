1030cb5980ec529e98077d7b8f5729e23a542316
Chao <42952910+kenwan006@users.noreply.github.com>
2022-11-03 11:22:22 -0700

    Time: 7 ms (81.68%), Space: 75.9 MB (40.56%) - LeetHub

---
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
@@ -0,0 +1,26 @@
+/** Two pointer (Cycle detection)
+* We can convert this problem to a cycle detection case and then use two pointer method
+* For a nums[i], we say it stores the index j of the next num, j = nums[i], or we can say nums[i] points to nums[j], nums[j] = nums[nums[i]]. eg nums[2,5,9,6,9,3,8,9,7,1], nums[0] = 2, so we say nums[0] = 2 points to nums[2], nums[2]= 9, say it points to nums[9], and so on so forth, we have 2->9->1->5->3->6->8->7->9->1->5... we can find the cycle: 9->1->5->3->6->8->7->9
+* Find intersection point: Use two pointer, slow and fast, slow moves 1 steps every time and fast moves 2 steps every time, stop until they meet;
+* Find the entry point of the cycle, which should be the repeated number. fast starts at index 0 and slow starts at the meeting point, move them both at 1 step every time and until they meet. This new meeting point is the entry point. (See the Tortoise and Hare Algo for the proof)
+*/
+class Solution {
+    public int findDuplicate(int[] nums) {
+        int slow = nums[0], fast = nums[nums[0]];
+        //Find the meeting point
+        while (slow != fast) {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        }
+        
+        fast = 0; // send fast back to orginal start, it's 0 not nums[0]
+        
+        //Find the entry point
+        while (slow != fast) {
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+        
+        return slow;
+    }
+}

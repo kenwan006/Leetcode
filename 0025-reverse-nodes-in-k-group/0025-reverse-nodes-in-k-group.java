d4b3de7f71eacb402d2cc340860294e8e5cf2031
Chao <42952910+kenwan006@users.noreply.github.com>
2022-11-09 15:27:26 -0800

    Time: 1 ms (70.17%), Space: 46.2 MB (15.66%) - LeetHub

--- 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.java
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.java
@@ -12,7 +12,7 @@
 * Fisrt define a reverse function to reverse all nodes between start and end exclusively
   -reverse(start, end), it returns the tail node of the reversed list
 * Iterate through the original list, and count the nodes walked by, once count % k == 0, then run reveser(start, end)
-* eg: -1(dummy) -> 1 -> 2 ->3 ->4 -> 5 -> 6, k = 3, start = -1, ptr = 1, when count == 3, ptr walks to 4, and reverse nodes between -1 and 4, run reverse(-1, 4) we get new start = 1, -1(dummy) -> 3 -> 2-> 1(new start)-> 4 -> 5 -> 6
+* eg: -1(dummy) -> 1 -> 2 ->3 ->4 -> 5 -> 6, k = 3, start = -1, ptr = 1, when count == 3, ptr walks to 4, and reverse nodes between -1 and 4, run reverse(-1, 4) we get new start = 1, and the new list is: -1(dummy) -> 3 -> 2-> 1(new start)-> 4 -> 5 -> 6
 */
 class Solution {
     public ListNode reverseKGroup(ListNode head, int k) {
@@ -31,7 +31,7 @@ class Solution {
     }
     
     // reverse nodes between start and end, (start, end) exclusively 
-    //eg: 0(start) -> 1(revTail) -> 2 -> 3 -> 4(end), after the reverse 0 -> 3 -> 2 -> 1(revTail) -> 4, return the revTail 
+    //eg: -1(start) -> 1(revTail) -> 2 -> 3 -> 4(end), after the reverse -1 -> 3 -> 2 -> 1(revTail) -> 4, return the revTail 
     public ListNode reverse(ListNode start, ListNode end) {
         ListNode prev = start; // start always represent the end of the previously reversed list
         ListNode curr = start.next;
@@ -42,8 +42,8 @@ class Solution {
             prev = curr;
             curr = nxt;
         }
-        start.next = prev; //point 0(start) to 3(new head)
-        revTail.next = end; //point 1 to 4
+        start.next = prev; //point -1(start) to 3(new head)
+        revTail.next = end; //point the 1(new tail) to 4 (end)
         return revTail;
     }
 }

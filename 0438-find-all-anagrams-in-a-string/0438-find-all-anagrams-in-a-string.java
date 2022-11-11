de2648f4c5acf5f9642bd3e54339aac24178a5d9
Chao <42952910+kenwan006@users.noreply.github.com>
2022-11-10 16:14:44 -0800

    Time: 17 ms (75.67%), Space: 48.5 MB (33.87%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
@@ -3,6 +3,7 @@ Create a sliding window of the same size as string p
 Use an array to count the letter in p; Use an array to count the letter in the sliding window;
 Compare two arrays
 */
+/*
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
         List<Integer> res = new LinkedList<>();
@@ -29,18 +30,43 @@ class Solution {
         return res;
     }
 }
-
-/*
-/** Optimized sliding window method
-* No need to use Arrays.equals() !! But use a count;
-* Use a array to store freq of each char in string p, and count to represent the total freq, which is same as p.length();
-* Use two pointers, left and right, start moving right pointer and updating the freq and count
 */
-/*
+
+/** Optimized sliding window method
+* No need to use Arrays.equals() !!! But use a variable count;
+* Use an array to store freq of each char in string p, and count to represent the total freq, which is same as p.length();
+* Use two pointers, left and right, start moving right pointer and decrease the freq and count. When right - left == p.length() - 1, if count == 0, which means this windows matches; keeping moving right pointer, and also move left pointer. Before moving the left pointer, need to check if we have to add this char back to the freq.
+*/
+
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
         List<Integer> res = new LinkedList<>();
-      
+        int[] freq = new int[26];
+        int count = p.length();
+        for (char ch : p.toCharArray()) freq[ch - 'a']++; 
+        
+        int left = 0, right = 0;
+        while (right < s.length()) {
+            //only the char existing in p has frequency > 0
+            if (freq[s.charAt(right) - 'a'] > 0 ) count--; 
+            
+            //update the freq no matter if this char exist or not exist in p
+            freq[s.charAt(right) - 'a']--; 
+            
+            //count == 0 means all chars in p have been visited
+            if (count == 0) res.add(left);
+            
+            //when sliding window comes to the length of p, we have to update both left and right in the next move
+            if (right - left == p.length() - 1) {
+                //if the char was originally in the freq, then its frequence won't go below 0
+                if (freq[s.charAt(left) - 'a'] >= 0) count++;
+                freq[s.charAt(left) - 'a']++;
+                left++;
+            }
+            
+            right++;     
+        }
+        
+        return res;      
     }
 }
-*/

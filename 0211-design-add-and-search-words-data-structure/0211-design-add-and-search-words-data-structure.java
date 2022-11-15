8be7a4f13d631530fecbb14856389ca07c1d97d4
Chao <42952910+kenwan006@users.noreply.github.com>
2022-11-15 15:13:10 -0800

    Time: 405 ms (97.69%), Space: 91.9 MB (92.06%) - LeetHub

--- 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.java
+++ 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.java
@@ -1,6 +1,8 @@
 /** Trie + backtracking
-*
-*
+* Use a trie (root) to represent a dictionary containing all the words added. 
+ - Eg, if we add a word "bad", then at the first layer, children[1] is not null, at the second layer, children[0] is not null, the third layer children[3] is not null and mark isEnd as true. Every time adding a new word, we check each of its letters.
+* When searching a word, if it only contains letters, we just keep each letter of the word until the letter is not in the root or the end of word;
+If it contains '.', it could reprsent any letter from 'a' to 'z'. Then while walking to this layer of the root,  we can use a for loop to check each option. 
 */
 
 class WordDictionary {
@@ -26,11 +28,13 @@ class WordDictionary {
     public boolean search(String word) {
         return search(word.toCharArray(), 0, root);
     }
-    
+    //Define a helper function - backtracking
+    //use k to represent the index position of the checked character or depth layer of the trie
     private boolean search (char[] chs, int k, TrieNode cur) {
         if (chs.length == k) return cur.isEnd;
         if (chs[k] == '.') {
             for (int i = 0; i < 26; i++) {
+                //only check the branch which is not null
                 if (cur.children[i] != null && search(chs, k + 1, cur.children[i]))
                     return true;
             }

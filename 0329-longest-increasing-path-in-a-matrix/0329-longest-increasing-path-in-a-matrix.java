ac6805e1eda4210775bc45fe04ad9607cb652073
Chao <42952910+kenwan006@users.noreply.github.com>
2023-03-28 16:24:28 -0700

    Time: 8 ms (92.10%), Space: 42.6 MB (59.33%) - LeetHub

--- 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.java
+++ 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.java
@@ -3,18 +3,17 @@
 * For cell at (i, j), check all its neighbors to see which one bring about the longest increasing path
 */
 class Solution {
-    int[][] memo;
-    int[][] dirs = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+    private int[][] memo;
+    private static int[][] dirs = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
     
     public int longestIncreasingPath(int[][] matrix) {
         int m = matrix.length, n = matrix[0].length;
         memo = new int[m][n];
         int res = 0;
-        for (int i = 0; i < m; i++) Arrays.fill(memo[i], 1); //fill all cells with 1
         
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                if (memo[i][j] > 1) continue; // skip the already checked cell
+                if (memo[i][j] != 0) continue; // skip the already checked cell
                 res = Math.max(res, dfs(matrix, i, j));
             }
         }
@@ -24,14 +23,16 @@ class Solution {
     //dfs
     public int dfs (int[][]matrix, int i, int j) {
         int m = matrix.length, n = matrix[0].length;
-        if (memo[i][j] > 1) return memo[i][j];
+        if (memo[i][j] != 0) return memo[i][j];
         
+        int ans = 1;
         for (int[] dir : dirs) {
             int ii = i + dir[0], jj = j + dir[1];
             if (ii < 0 || ii >= m || jj < 0 || jj >= n || matrix[ii][jj] <=  matrix[i][j]) continue;
-            memo[i][j] = Math.max(memo[i][j], 1 + dfs(matrix, ii, jj));
+            ans= Math.max(ans, 1 + dfs(matrix, ii, jj));
         }
-        return memo[i][j];  
+        memo[i][j] = ans; //update the memo
+        return ans;
     }
 }
 //Time: O(m * n); Space: O(m * n)

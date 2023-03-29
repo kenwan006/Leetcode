a7962ad5a22e26e18190afb60072f869a74965a8
Chao <42952910+kenwan006@users.noreply.github.com>
2023-03-29 15:57:45 -0700

    Time: 36 ms (71.37%), Space: 42.5 MB (27.83%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.java
+++ 0279-perfect-squares/0279-perfect-squares.java
@@ -1,24 +1,18 @@
 /** DP
 * dp[i] - least number of perfect square numbers that sums up to i
-* dp[i] = dp[j] + dp[i - j], iterate j from i- 1 to 1 and find the min value
+* dp[i] = dp[i - j*j] + dp[j*j] = dp[i - j*j] + 1; iterate j from 1 to sqrt(i)
 */
 class Solution {
     public int numSquares(int n) {
         int[] dp = new int[n + 1];
+        Arrays.fill(dp, Integer.MAX_VALUE);
         dp[0] = 0;
         for (int i = 1; i <= n; i++) {
-            if (i == (int)Math.sqrt(i) * (int)Math.sqrt(i)) { //check if the number itself is squared
-                dp[i] = 1;
-                continue;
+            for (int j = 1; j * j <= i; j++) {
+                dp[i] = Math.min(dp[i], dp[i - j * j] + 1);
             }
-            
-            int min = Integer.MAX_VALUE;
-            for (int j = i - 1; j > 0; j--) {
-                min = Math.min(min, dp[j] + dp[i - j]);
-            }
-            dp[i] = min;
         }
         return dp[n];
     }
 }
-//Time: O (n ^ 2); Space: O(n)
+//Time: O (n * sqrt(n)); Space: O(n)

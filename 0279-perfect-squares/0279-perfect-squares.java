b020b056f0126970650288a931354121a607c241
Chao <42952910+kenwan006@users.noreply.github.com>
2023-03-29 17:55:06 -0700

    Time: 42 ms (50.78%), Space: 44.1 MB (16.62%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.java
+++ 0279-perfect-squares/0279-perfect-squares.java
@@ -1,18 +1,32 @@
-/** DP
-* dp[i] - least number of perfect square numbers that sums up to i
-* dp[i] = dp[i - j*j] + dp[j*j] = dp[i - j*j] + 1; iterate j from 1 to sqrt(i)
+/** BFS
+* keep substract a perfect square number from the target unitl it becomes 0
+* eg, n = 10, the first square number could be 1, 4, 9, after subtraction, the remainder would be 9, 6, 1. 
+* Add all the possible remainders to a queue, and poll one by one. For each polled number, repeate the above the process.
+* When the remainder becomes 0, the total subtractions would be the number of square numbers.
 */
 class Solution {
     public int numSquares(int n) {
-        int[] dp = new int[n + 1];
-        Arrays.fill(dp, Integer.MAX_VALUE);
-        dp[0] = 0;
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j * j <= i; j++) {
-                dp[i] = Math.min(dp[i], dp[i - j * j] + 1);
+        Queue<Integer> queue = new LinkedList<>();
+        Set<Integer> visited = new HashSet<>();
+        queue.offer(n);
+        int step = 0;
+        while (!queue.isEmpty()) {
+            step++;
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                int num = queue.poll();
+                //add the remainder to queue after subtraction to num
+                for (int j = 1; j * j <= num; j++) {
+                    int remainder = num - j * j;
+                    if (remainder == 0) return step;
+                    if (!visited.contains(remainder)) { //skip number that's already visited
+                        queue.offer(remainder);
+                        visited.add(remainder);
+                    }
+                }       
             }
         }
-        return dp[n];
+        return step;
     }
 }
-//Time: O (n * sqrt(n)); Space: O(n)
+//Time: O(n); Space: O(n)

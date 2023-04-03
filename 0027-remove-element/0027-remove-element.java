ebf3b072b54834322e132e144d7bb5f3513aef29
Chao <42952910+kenwan006@users.noreply.github.com>
2023-04-02 17:21:03 -0700

    Time: 0 ms (100.00%), Space: 41.1 MB (49.36%) - LeetHub

--- 0027-remove-element/0027-remove-element.java
+++ 0027-remove-element/0027-remove-element.java
@@ -1,18 +1,9 @@
-/** Two pointer */
+/** Two pointers */
 class Solution {
     public int removeElement(int[] nums, int val) {
         int i = 0;
-        //find the index of first num equal to val
-        while (i < nums.length && nums[i] != val) i++;
-        
-        //find the num not equal to val and move to the front
-        int j = i + 1;
-        while (j < nums.length) {
-            if (nums[j] == val) {
-                j++;
-            } else {
-                 nums[i++] = nums[j++];
-            }
+        for (int j = 0; j < nums.length; j++) {
+            if (nums[j] != val) nums[i++] = nums[j];
         }
         return i;
     }

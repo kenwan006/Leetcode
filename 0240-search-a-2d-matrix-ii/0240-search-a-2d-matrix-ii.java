81b6e3254ce319ca1a73e6da8b640b75f77b3454
Chao <42952910+kenwan006@users.noreply.github.com>
2023-04-05 18:22:44 -0700

    Time: 4 ms (100.00%), Space: 48.5 MB (28.26%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
@@ -1,27 +1,17 @@
-/** Divide & Conquer **/
+/** Search space reduction **/
 class Solution {
-    private int m, n;
     public boolean searchMatrix(int[][] matrix, int target) {
-        m = matrix.length;
-        n = matrix[0].length;
-        return search(matrix, target, 0, m -1, 0, n - 1); 
-    }
-    
-    public boolean search(int[][] matrix, int target, int top, int down, int left, int right) {
-        if (top > down || left > right) return false;
-        
-        int row = (top + down) /2, col = (left + right) / 2;
-        
-        if (matrix[row][col] == target) return true;
-        else if (matrix[row][col] > target) {
-            return search(matrix, target, row, down, left, col - 1) || 
-                   search(matrix, target, top, row - 1, col, right) ||
-                   search(matrix, target, top, row - 1, left, col - 1);
-        } else {
-            return search(matrix, target, row + 1, down, left, col) || 
-                   search(matrix, target, top, row, col + 1, right) ||
-                   search(matrix, target, row + 1, down, col + 1, right);
+        int m = matrix.length, n = matrix[0].length;
+        int row = 0, col = n - 1; //starting from the upper right corner
+        while (row < m && col >= 0) {
+            if (matrix[row][col] == target) return true;
+            else if (matrix[row][col] < target) { //go down for for larger number
+                row++;
+            } else { //go left for smaller number
+                col--;
+            }
         }
+        return false;
     }
 }
-//Time: O(log(m * n)); Space: O(log(m * n))
+//Time: O(m + n); Space: O(1)

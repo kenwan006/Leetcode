882ad5c2222025915812b1ec9760f3bfa62455c6
Chao <42952910+kenwan006@users.noreply.github.com>
2023-04-06 15:20:42 -0700

    Time: 2 ms (95.74%), Space: 43.3 MB (5.87%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
@@ -1,23 +1,27 @@
-/** Hashtable **/
+/** Two pointers 
+* For the follow up question, if the arrays are sorted, one can use two pointers.
+**/
 class Solution {
     public int[] intersect(int[] nums1, int[] nums2) {
-        if (nums1.length > nums2.length) return intersect(nums2, nums1);
-        
-        Map<Integer, Integer> map = new HashMap<>(); //map num to its count
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
         List<Integer> list = new ArrayList<>();
-        for (int num : nums1) {
-            map.put(num, map.getOrDefault(num, 0) + 1);
-        }
-        for (int num : nums2) {
-            if (map.containsKey(num) && map.get(num) > 0) {
-                list.add(num);
-                map.put(num, map.get(num) - 1); //update the count
+        int i = 0, j = 0;
+        while (i < nums1.length && j < nums2.length) {
+            if (nums1[i] == nums2[j]) {
+                list.add(nums1[i]);
+                i++;
+                j++;
+            } else if (nums1[i] < nums2[j]) {
+                i++;
+            } else {
+                j++;
             }
         }
         
         int[] res = new int[list.size()];
-        for (int i = 0; i < list.size(); i++) res[i] = list.get(i);
+        for (i = 0; i < list.size(); i++) res[i] = list.get(i);
         return res;
     }
 }
-//Time: O(m + n); Space: O(min(m, n));
+//Time: O(mlog(m) + nlog(n)); Space: O(m + n)

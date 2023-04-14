5ac21e52b11ca4f9d93bf4c6317323d7fa8d3762
Chao <42952910+kenwan006@users.noreply.github.com>
2023-04-14 12:26:33 -0700

    Time: 38 ms (79.08%), Space: 49.4 MB (84.15%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -5,21 +5,19 @@ class Solution {
         List<List<Integer>> res = new ArrayList<>();
         for (int i = 0; i < nums.length - 2; i++) {
             if (i > 0 && nums[i] == nums[i - 1]) continue; //skip duplicate nums
-            int target = -nums[i];
             int lo = i + 1, hi = nums.length - 1;
             
             //2Sum to find the target
             while (lo < hi) {
-                int left = nums[lo], right = nums[hi];
-                int sum = nums[lo] + nums[hi];
-                if (sum < target) {
-                    while (lo < hi && nums[lo] == left) lo++;
-                } else if (sum > target) {
-                    while (lo < hi && nums[hi] == right) hi--;
+                int sum = nums[lo] + nums[hi] + nums[i];
+                if (sum < 0) {
+                    lo++;
+                } else if (sum > 0) {
+                    hi--;
                 } else {
-                    res.add(Arrays.asList(nums[i], left, right));
-                    while (lo < hi && nums[lo] == left) lo++;
-                    while (lo < hi && nums[hi] == right) hi--;
+                    while (lo < hi && nums[lo] == nums[lo + 1]) lo++; //skip duplicate nums
+                    while (lo < hi && nums[hi] == nums[hi - 1]) hi--; //skip duplicate nums
+                    res.add(Arrays.asList(nums[i], nums[lo++], nums[hi--]));
                 }
             }
         }

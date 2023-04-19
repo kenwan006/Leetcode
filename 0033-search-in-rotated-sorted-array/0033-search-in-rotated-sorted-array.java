8f3a6ae8eaa4b6295dbd4087da626b5596c26bf6
Chao <42952910+kenwan006@users.noreply.github.com>
2023-04-18 22:05:37 -0700

    Time: 0 ms (100.00%), Space: 42 MB (72.28%) - LeetHub

---
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -0,0 +1,26 @@
+/** Binary Search **/
+class Solution {
+    public int search(int[] nums, int target) {
+        int lo = 0, hi = nums.length - 1;
+        while (lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
+            if (nums[mid] == target) return mid;
+            
+            if (nums[mid] >= nums[lo]) { //left half is sorted
+                if (target >= nums[lo] && target < nums[mid]) {
+                    hi = mid - 1;
+                } else {
+                    lo = mid + 1;
+                }
+            } else { //right half is sorted
+                if (target > nums[mid] && target <= nums[hi]) {
+                    lo = mid + 1;
+                } else {
+                    hi = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+}
+//Time: O(log(n)); Space: O(1)

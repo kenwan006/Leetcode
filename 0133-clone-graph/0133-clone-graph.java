e33036024ffa864c46ce905311a7071fb2ea6dba
Chao <42952910+kenwan006@users.noreply.github.com>
2023-04-27 15:38:27 -0700

    Time: 27 ms (20.44%), Space: 42.3 MB (94.02%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.java
+++ 0133-clone-graph/0133-clone-graph.java
@@ -18,27 +18,29 @@ class Node {
 }
 */
 
+/** bfs **/
 class Solution {
-    private Map<Node, Node> map = new HashMap<>();// node -> copy node
-    
     public Node cloneGraph(Node node) {
-        return clone(node);
-    }
-    
-    //dfs
-    public Node clone(Node node) {
         if (node == null) return null;
         
-        if (map.containsKey(node)) return map.get(node);
+        Map<Node, Node> map = new HashMap<>(); //map node -> copy node
+        map.put(node, new Node(node.val, new ArrayList<Node>()));
         
-        Node newNode = new Node(node.val, new ArrayList<Node>());
-        map.put(node, newNode);
+        Queue<Node> queue = new LinkedList<>();
+        queue.offer(node);
         
-        //copy the neighbors
-        for (Node neighbor : node.neighbors) {
-            newNode.neighbors.add(clone(neighbor));
+        while(!queue.isEmpty()) {
+            Node curr = queue.poll();
+            for (Node neighbor : curr.neighbors) {
+                if (!map.containsKey(neighbor)) {
+                    map.put(neighbor, new Node(neighbor.val, new ArrayList<Node>()));
+                    queue.offer(neighbor);
+                }
+                map.get(curr).neighbors.add(map.get(neighbor)); //add cloned neighbor to neighbors of cloned root
+            }
         }
-        return newNode;
+        return map.get(node);
     }
 }
+
 //Time: O(v + e); Space: O(v)

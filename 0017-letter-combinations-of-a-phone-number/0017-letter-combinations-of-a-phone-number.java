2f40f33940c24b2b10030a794c3d3c4de84f7804
Chao <42952910+kenwan006@users.noreply.github.com>
2023-04-26 22:51:03 -0700

    Time: 0 ms (100.00%), Space: 41.5 MB (54.73%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -1,38 +1,26 @@
-/** Backtracking
-- The digit at ith position of digits maps to a string.
-- For loop to check each letter of the string.
-  - Every time check one letter, add it to the track,increment start index i by one 
-  - Implement function of backtrack 
-  - Once backtrack is done, remove the letter from the track
-
-*/
-
 class Solution {
-    String[] mapping = new String[] {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+    String[] dict = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
     List<String> res = new LinkedList<>();
-    StringBuilder track = new StringBuilder();
+    StringBuilder sb = new StringBuilder();
     
     public List<String> letterCombinations(String digits) {
         if (digits.isEmpty()) return res;
-        StringBuilder track = new StringBuilder();
         backtrack(digits, 0);
         return res;
     }
     
-    public void backtrack(String digits, int i) { //i means ith digit in the digits
-        // base case
-        if (track.length() == digits.length()) {
-            res.add(track.toString());
+    public void backtrack(String digits, int i) { //ith digit
+        if (i == digits.length()) {
+            res.add(sb.toString());
             return;
         }
         
-        String s = mapping[digits.charAt(i) - '0'];
-        for (char ch : s.toCharArray()) {
-            track.append(ch);
-            backtrack(digits, i + 1); 
-            track.deleteCharAt(track.length() - 1);
-        }
+        int digit = Character.getNumericValue(digits.charAt(i));
         
+        for (char c : dict[digit].toCharArray()) {
+            sb.append(c);
+            backtrack(digits, i + 1); //do not use i++
+            sb.deleteCharAt(sb.length() - 1);
+        }
     }
-     
 }

b2ddec6a6f361f00e1197cb5b02910826aff6897
Chao <42952910+kenwan006@users.noreply.github.com>
2023-05-25 18:22:41 -0700

    Time: 39 ms (60.89%), Space: 58.7 MB (38.24%) - LeetHub

--- 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.java
+++ 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.java
@@ -1,28 +1,22 @@
 class Solution {
     public int minimumRounds(int[] tasks) {
-        //map task to its frequence
         Map<Integer, Integer> map = new HashMap<>();
-        int maxCount = 0;
         for (int task : tasks) {
             map.put(task, map.getOrDefault(task, 0) + 1);
-            maxCount= Math.max(maxCount, map.get(task));
-        }
-        if (maxCount < 2) return -1;
-        
-        //dp[i] - check if task of freq i can be completed
-        int[] dp = new int[maxCount + 1];
-        Arrays.fill(dp, tasks.length);
-        dp[0] = 0;
-        dp[2] = 1;
-        for (int i = 3; i <= maxCount; i++) {
-            dp[i] = Math.min(dp[i - 2], dp[i - 3]) + 1;
         }
         
-        //check all the freq for the tasks
         int res = 0;
         for (int count : map.values()) {
-            if (dp[count] == tasks.length) return -1;
-            res += dp[count];
+            if (count == 1) {
+                return -1;
+            } 
+            if (count % 3 == 0) {
+                res += count / 3;
+            } else {
+                // count % 3 = 1; (count/3 -1) triplet groups and 2 dual pairs
+                // count % 3 = 2; count/3 triplet groups and 1 dual pair
+                res += count / 3 + 1;
+            }
         }
         return res;
     }

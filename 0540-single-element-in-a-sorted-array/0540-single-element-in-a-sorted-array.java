c481412f0b5265f0eb88215bd074cd4f9834336c
Chao <42952910+kenwan006@users.noreply.github.com>
2023-05-26 15:27:14 -0700

    Time: 0 ms (100.00%), Space: 50.5 MB (6.56%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -6,16 +6,12 @@ class Solution {
         int lo = 0, mid = 0, hi = nums.length - 1;
         while (lo < hi) {
             mid = lo + (hi - lo) / 2;
-            boolean evenHalf = (mid - lo) % 2 == 0; //each half has even numbers
-            if (nums[mid] == nums[mid - 1]) {
-                if (evenHalf) hi = mid - 2;
-                else lo = mid + 1;
-            } else if (nums[mid] == nums[mid + 1]) {
-                if (evenHalf) lo = mid + 2;
-                else hi = mid - 1;
-            } else {
-                return nums[mid];
-            }
+            
+            //if the half is odd, mid-- so that left half and right half both have even numbers
+            if (mid % 2 == 1) mid--; 
+            
+            if (nums[mid] == nums[mid + 1]) lo = mid + 2;
+            else hi = mid;
         }
         return nums[lo];
     }

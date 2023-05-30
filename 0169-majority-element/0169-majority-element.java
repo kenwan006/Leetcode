feb16a62856c54e21ff271a1f3e6bccc83f96502
Chao <42952910+kenwan006@users.noreply.github.com>
2023-05-30 16:42:05 -0700

    Time: 2 ms (66.91%), Space: 49.4 MB (6.63%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,13 +1,30 @@
+/** Divide & conquer **/
 class Solution {
     public int majorityElement(int[] nums) {
-        int candidate = 0, buff = 0;
-        for (int num : nums) {
-            if (buff == 0) candidate = num;
-                
-            if (num == candidate) buff++;
-            else buff--;
+        return majorityElement(nums, 0, nums.length - 1);
+    }
+    
+    public int majorityElement(int[] nums, int lo, int hi) {
+        if (lo == hi) return nums[lo];
+        
+        int mid  = lo + (hi - lo) / 2;
+        int left = majorityElement(nums, lo, mid);
+        int right = majorityElement(nums, mid + 1, hi);
+        
+        if (left == right) return left; //when both halves have the sames majority element
+        
+        //check the count of "left" and "right" appearing in the entire nums
+        int countl = count(nums, lo, hi, left);
+        int countr = count(nums, lo, hi, right);
+        return countl > countr? left : right;
+    }
+    
+    public int count(int[] nums, int lo, int hi, int element) {
+        int ctn = 0;
+        for (int i = lo; i <= hi; i++) {
+            if (nums[i] == element) ctn++;
         }
-        return candidate;
+        return ctn;
     }
 }
-//Time: O(n); Space: O(1)
+//Time: O(n * log(n)); Space: O(1)

84195c2067e371778b7bd50adc7f082fccf2df43
Chao <42952910+kenwan006@users.noreply.github.com>
2023-05-31 11:40:50 -0700

    Time: 3 ms (40.62%), Space: 44.3 MB (19.04%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,14 +1,11 @@
-/** Greedy
-* find the farthest position one can jump to from nums[i]
-*/
 class Solution {
     public boolean canJump(int[] nums) {
-        int farthest = 0;
-        for (int i = 0; i < nums.length - 1; i++) {
+        int farthest = nums[0]; //keep updating the farthest position one cen jump to
+        for (int i = 1; i < nums.length; i++) {
+            if (farthest < i) return false;
             farthest = Math.max(farthest, i + nums[i]);
-            if (farthest == i) return false;
         }
-        return farthest >= nums.length - 1;
+        return true;
     }
 }
 //Time: O(n); Space: O(1)

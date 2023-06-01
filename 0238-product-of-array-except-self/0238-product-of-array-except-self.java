4851b927c86e9481f906d407082766186fd8d0fd
Chao <42952910+kenwan006@users.noreply.github.com>
2023-06-01 16:38:28 -0700

    Time: 1 ms (100.00%), Space: 52.8 MB (26.96%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -1,21 +1,19 @@
-/** prefix *
-* Calculate the prefix for each num -- product of all nums at the left side of nums[i]
-* Iterate the nums reversely, let R be the product of nums at the right side of nums[i]
-*/
 class Solution {
     public int[] productExceptSelf(int[] nums) {
         int n = nums.length;
         int[] res = new int[n];
-        res[0] = 1;
-        for (int i = 1; i < n; i++) { //product of nums[0] * nums[1] ..nums[i-1]
-            res[i] = res[i - 1] * nums[i - 1];
-        }
+        
+        res[0] = nums[0];
+        for (int i = 1; i < n; i++) {
+            res[i] = res[i - 1] * nums[i];
+        } 
         
         int R = 1;
-        for (int j = n - 1; j >= 0; j--) {//keep updating R - product of all nums at right side of nums[i]
-            res[j] = res[j] * R;
-            R *= nums[j];
+        for (int i = n - 1; i > 0; i--) {
+            res[i] = res[i - 1] * R;
+            R = R * nums[i];
         }
+        res[0] = R;
         return res;
     }
 }

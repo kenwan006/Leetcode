3d7443a68f41e36128f887f0277cebd13b3c13d3
Chao <42952910+kenwan006@users.noreply.github.com>
2023-06-01 17:11:58 -0700

    Time: 2 ms (99.58%), Space: 55.4 MB (85.75%) - LeetHub

--- 0134-gas-station/0134-gas-station.java
+++ 0134-gas-station/0134-gas-station.java
@@ -1,24 +1,21 @@
-/** Greedy **/
 class Solution {
     public int canCompleteCircuit(int[] gas, int[] cost) {
-        //check if the total gas greater than the total cost
-        int sum = 0;
-        for (int i = 0; i < gas.length; i++) sum += gas[i] - cost[i];
+        //check if total gas can cover the cost
+        int n = gas.length, sum = 0;
+        for (int i = 0; i < n; i++) sum += gas[i] - cost[i];
         if (sum < 0) return -1;
         
-        //iterate over the array
+        //find the start station
         int start = 0, carry = 0;
-        for (int i = 0; i < gas.length; i++) {
-            carry += gas[i] - cost[i]; 
-            
-            //If station i+1 cannot be reached starting from start station, it means it cannot be reached starting 
-            //any station between start and i. Then just move start to the next.
+        for (int i = 0; i < n; i++) {
+            carry += gas[i] - cost[i];
             if (carry < 0) {
-                start = i + 1;
                 carry = 0;
+                start = i + 1; //try to start from next station
             }
         }
         return start;
     }
 }
 //Time: O(n); Space: O(1)
+

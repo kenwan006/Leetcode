f8139d04693741b6be3945719844fc978e255d05
Chao <42952910+kenwan006@users.noreply.github.com>
2023-06-06 11:54:37 -0700

    Time: 38 ms (84.91%), Space: 50.3 MB (56.13%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,27 +1,26 @@
-/** Two pointers */
+/** Two pointer **/
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        Arrays.sort(nums);
         List<List<Integer>> res = new ArrayList<>();
+        Arrays.sort(nums);
         for (int i = 0; i < nums.length - 2; i++) {
-            if (i > 0 && nums[i] == nums[i - 1]) continue; //skip duplicate nums
-            int lo = i + 1, hi = nums.length - 1;
+            if (i > 0 && nums[i] == nums[i - 1]) continue; //Check 1st item, skip the duplicate 
             
-            //2Sum to find the target
+            //2-sum
+            int lo = i + 1, hi = nums.length - 1, target = -nums[i];
             while (lo < hi) {
-                int sum = nums[lo] + nums[hi] + nums[i];
-                if (sum < 0) {
-                    lo++;
-                } else if (sum > 0) {
-                    hi--;
-                } else {
-                    while (lo < hi && nums[lo] == nums[lo + 1]) lo++; //skip duplicate nums
-                    while (lo < hi && nums[hi] == nums[hi - 1]) hi--; //skip duplicate nums
+                int sum = nums[lo] + nums[hi];
+                if (sum > target) hi--;
+                else if (sum < target) lo++;
+                else {
+                    while (lo < hi && nums[lo] == nums[lo + 1]) lo++; //Check 2nd item, skip the duplicate 
+                    while (lo < hi && nums[hi] == nums[hi - 1]) hi--; //Check 3rd item, skip the duplicate 
                     res.add(Arrays.asList(nums[i], nums[lo++], nums[hi--]));
                 }
             }
         }
         return res;
-    } 
+    }
 }
-//Time: O(n ^ 2); Space: O(n)
+//Time: O(n * n); Space: O(n)
+

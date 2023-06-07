592947f865f1f7dd21a19e14ac351bd8d46ba77c
Chao <42952910+kenwan006@users.noreply.github.com>
2023-06-07 16:35:22 -0700

    Time: 1 ms (72.63%), Space: 44.7 MB (31.97%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,49 +1,41 @@
-/** Math
-* Use first row and first column as two indicators
-**/
 class Solution {
     public void setZeroes(int[][] matrix) {
         int m = matrix.length, n = matrix[0].length;
         
-        //check if 0 exists in the first row and first column of original array
-        boolean first_row = false, first_column = false;
+        //check if 0 exists in the first row and first col of original matrix
+        boolean first_row = false, first_col = false;
         for (int i = 0; i < m; i++) {
-            if (matrix[i][0] == 0) {
-                first_column = true;
-                break;
-            }
+            if (matrix[i][0] == 0) first_col = true;
         }
         for (int j = 0; j < n; j++) {
-            if (matrix[0][j] == 0) {
-                first_row = true;
-                break;
-            }
+            if (matrix[0][j] == 0) first_row = true;
         }
         
-        //update the indicators in first row and first column
+        //set the indicator in the first row and first col
         for (int i = 1; i < m; i++) {
             for (int j = 1; j < n; j++) {
-                if (matrix[i][j] == 0) { //set two indicators to 0 if current checked cell is 0
+                if (matrix[i][j] == 0) {
                     matrix[0][j] = 0;
                     matrix[i][0] = 0;
                 }
             }
         }
-        //set the cell to 0 if its corresponding indicator is 0
+        
+        //check each cell and set it to 0 if needed
         for (int i = 1; i < m; i++) {
             for (int j = 1; j < n; j++) {
-                if (matrix[0][j] == 0 || matrix[i][0] == 0) matrix[i][j] = 0;
+                if (matrix[0][j] == 0 || matrix[i][0] == 0) {
+                    matrix[i][j] = 0;
+                }
             }
         }
-        
-        //check first row and first column at last
-        if (first_column) {
-            for (int i = 0; i < m; i++) matrix[i][0] = 0;
-        }
+        //set first row and first col
         if (first_row) {
             for (int j = 0; j < n; j++) matrix[0][j] = 0;
+        }
+        if (first_col) {
+            for (int i = 0; i < m; i++) matrix[i][0] = 0;
         }
     }
 }
 //Time: O(m * n); Space: O(1)
-

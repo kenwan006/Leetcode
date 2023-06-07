6cbb1ed1246e6851bbb2da814435a247a224d2a5
Chao <42952910+kenwan006@users.noreply.github.com>
2023-06-06 17:42:59 -0700

    Time: 1 ms (100.00%), Space: 56.1 MB (5.34%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.java
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.java
@@ -1,17 +1,14 @@
-/** Sliding window **/
 class Solution {
     public int minSubArrayLen(int target, int[] nums) {
         int n = nums.length, res = n + 1;
-        int i = 0, j = 0, sum = nums[0];
-        while (i <= j && j < n) {
-            if (sum < target) {
-               if (++j < n) sum += nums[j];
-            } else {
-                res = Math.min(res, j - i + 1);
-                sum -= nums[i++];
+        int l = 0, sum = 0;
+        for (int r = 0; r < n; r++) {
+            sum += nums[r];
+            while (sum >= target) {//shrink window by moving left bound
+                res = Math.min(res, r - l + 1);
+                sum -= nums[l++];
             }
         }
-        return res == (n + 1)? 0 : res;
+        return res > n? 0 : res;
     }
 }
-//Time: O(n); Space: O(1)

3fe7afc999a205102cc77cb060ecbdc871fba95a
Chao <42952910+kenwan006@users.noreply.github.com>
2023-06-08 14:55:56 -0700

    Time: 19 ms (23.89%), Space: 41.4 MB (96.48%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -4,13 +4,10 @@ class Solution {
         Set<Character> seen = new HashSet<>(); //mark if char in t has been mapped
         for (int i = 0; i < s.length(); i++) {
             char c1 = s.charAt(i), c2 = t.charAt(i);
-            if (map.containsKey(c1)) {
-                if (map.get(c1) != c2) return false;
-            } else {
-                if (seen.contains(c2)) return false;
-                map.put(c1, c2);
-                seen.add(c2);
-            }
+            if (map.containsKey(c1) && map.get(c1) != c2)  return false;
+            if (!map.containsKey(c1) && seen.contains(c2)) return false;
+            map.put(c1, c2);
+            seen.add(c2);
         }
         return true;
     }

012ee93124725bd8138fc1e2f25640fb11e93bc0
Chao <42952910+kenwan006@users.noreply.github.com>
2023-06-08 12:09:25 -0700

    Time: 2 ms (99.03%), Space: 43.6 MB (37.24%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        if (ransomNote.length() > magazine.length()) return false;
+        int[] count = new int[26];
+        for (char c : magazine.toCharArray()) count[c - 'a']++;
+        for (char c : ransomNote.toCharArray()) {
+            if (--count[c - 'a'] < 0) return false;
+        }
+        return true;
+    }
+}

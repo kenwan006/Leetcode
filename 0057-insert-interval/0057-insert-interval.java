44d508951ef0960552b9699286b0572521377072
Chao <42952910+kenwan006@users.noreply.github.com>
2023-06-09 14:54:15 -0700

    Time: 2 ms (40.91%), Space: 44.3 MB (76.30%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.java
+++ 0057-insert-interval/0057-insert-interval.java
@@ -1,25 +1,26 @@
 class Solution {
     public int[][] insert(int[][] intervals, int[] newInterval) {
+        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
         List<int[]> list = new ArrayList<>();
         int start = newInterval[0], end = newInterval[1];
-        
-        for (int[] t : intervals) {
-            if (end < t[0]) {
+        for (int[] a : intervals) {
+            if (end < a[0]) {
                 list.add(new int[]{start, end});
-                start = t[0];
-                end = t[1];
-            } else if (start > t[1]) {
-                list.add(t);
+                start = a[0];
+                end = a[1];
+            } else if(start > a[1]) {
+                list.add(a);
             } else {
-                start = Math.min(start, t[0]);
-                end = Math.max(end, t[1]);
+                start = Math.min(start, a[0]);
+                end = Math.max(end, a[1]);
             }
         }
-        list.add(new int[]{start, end}); //need to add the last interval
+        list.add(new int[]{start, end});
         
         int[][] res = new int[list.size()][2];
         for (int i = 0; i < res.length; i++) res[i] = list.get(i);
         return res;
     }
 }
-//Time: O(n); Space: O(1)
+//Time: O(n * log(n)); Space: O(n)
+

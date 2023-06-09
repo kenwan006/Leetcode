36ef28d6e521297f27883c52132969bef123b45f
Chao <42952910+kenwan006@users.noreply.github.com>
2023-06-09 12:07:01 -0700

    Time: 5 ms (83.02%), Space: 40.8 MB (73.65%) - LeetHub

--- 0228-summary-ranges/0228-summary-ranges.java
+++ 0228-summary-ranges/0228-summary-ranges.java
@@ -1,14 +1,20 @@
 class Solution {
     public List<String> summaryRanges(int[] nums) {
         List<String> res = new ArrayList<>();
-        for (int i = 0; i < nums.length; i++) {
-            int start = nums[i];
-            while (i < nums.length - 1 && nums[i] + 1 == nums[i + 1]) i++;
-            
-            if (nums[i] != start) res.add(start + "->" + nums[i]);
-            if (nums[i] == start) res.add("" + start);
+        int n = nums.length, i = 0; //[i..j] nums are consecutive in this interval
+        if (n == 0) return res;
+        
+        for (int j = 0; j < n - 1; j++) {
+            if (nums[j] + 1 != nums[j + 1]) {
+                if (i != j) res.add(nums[i] + "->" + nums[j]);
+                if (i == j) res.add("" + nums[i]);
+                i = j + 1; //update the start of the new interval
+            }
         }
+        //check the last interval
+        if (i == n - 1) res.add("" + nums[n - 1]);
+        if (i != n - 1) res.add(nums[i] + "->" + nums[n - 1]);
         return res;
     }
 }
-//Time: O(n); Space: O(1)
+//Time: O(n); Space: O(n)

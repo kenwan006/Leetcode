217a7ad0b6512c84cdae731c8da10c2609a51a17
Chao <42952910+kenwan006@users.noreply.github.com>
2023-06-09 15:33:18 -0700

    Time: 74 ms (85.99%), Space: 77.1 MB (67.10%) - LeetHub

--- 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.java
+++ 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.java
@@ -1,34 +1,24 @@
-/** Sorting  + greedy *
-* Sort the points according to their Xend in ascending order
-* Start from first point, compare its Xend with the Xstart of next point, If there is intersection then they can be burst in one shot;
-* Otherwise, we need a new shot, use the Xend of this point as the new end..
-*/
 class Solution {
     public int findMinArrowShots(int[][] points) {
-        //Sort the points by their ends
-        //use a custom comparator to avoid int overflow 
         Comparator<int[]> comparator = new Comparator<>(){
             @Override
             public int compare(int[] a, int[] b) {
-                if (a[1] < b[1]) return -1;
-                else if (a[1] > b[1]) return 1;
+                if (a[1] > b[1]) return 1;
+                else if (a[1] < b[1]) return -1;
                 else return 0;
             }
         };
         Arrays.sort(points, comparator);
         
-        //Iterate over the points, compare current point with next one
-        int shot = 1;
-        int prevEnd = points[0][1];
-        int n = points.length;
-        for (int i = 1; i < n; i++) {
-            int start = points[i][0];
-            if (prevEnd < start) { //need a new shot
-                shot++;
-                prevEnd = points[i][1];//update the current end as the new preEnd
-            }
+        //always shoot the end 
+        int end = points[0][1], count = 1;
+        for (int[] p : points) {
+            if (end < p[0]) {
+                count++;
+                end = p[1]; // start a new burst
+            } 
         }
-        return shot;
+        return count;
     }
 }
-//Time: O(n * log(n)); Space: O(n)
+//Time: O(n * log(n)); Space: O(1)

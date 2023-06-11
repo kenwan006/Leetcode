06c0b62ae95940489eaae726561533abf8569595
Chao <42952910+kenwan006@users.noreply.github.com>
2023-06-10 22:30:57 -0700

    Time: 21 ms (80.00%), Space: 48.7 MB (40.00%) - LeetHub

--- 2734-lexicographically-smallest-string-after-substring-operation/2734-lexicographically-smallest-string-after-substring-operation.java
+++ 2734-lexicographically-smallest-string-after-substring-operation/2734-lexicographically-smallest-string-after-substring-operation.java
@@ -1,24 +1,16 @@
 class Solution {
     public String smallestString(String s) {
         int n = s.length(), i = 0;
-        StringBuilder sb = new StringBuilder();
+        char[] chars = s.toCharArray();
         
-         //skip the leading 'a'
-        while (i < n && s.charAt(i) == 'a') sb.append(s.charAt(i++));
+        //skip the leading 'a'
+        while (i < n && chars[i] == 'a') i++;
+        if (i == n) chars[n - 1] = 'z'; // s has all 'a' letters
         
-        //stop at the next 'a'
-        boolean change = false;
-        while (i < n && s.charAt(i) != 'a') {
-            char c = s.charAt(i++);
-            sb.append((char)(c - 1));
-            change = true;
-        }
+        //walk over the non 'a' and stop at the next 'a'
+        while (i < n && chars[i] != 'a') chars[i++]--;
         
-        //add the remaining letters
-        while (i < n) sb.append(s.charAt(i++));
-        
-        if (!change) sb.setCharAt(n - 1, 'z');
-        return sb.toString();
+        return new String(chars);
     }
 }
 //Time: O(n); Space: O(n)

b2199550f87a685791b6685b36488f3d6640fafa
Chao <42952910+kenwan006@users.noreply.github.com>
2023-06-21 16:01:30 -0700

    Time: 4 ms (95.98%), Space: 44.9 MB (32.67%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.java
+++ 0210-course-schedule-ii/0210-course-schedule-ii.java
@@ -1,37 +1,32 @@
-/** Topological sorting **/
-
 class Solution {
     public int[] findOrder(int numCourses, int[][] prerequisites) {
-        //build graph
-        List<Integer>[] graph = new List[numCourses];
-        for (int i = 0; i < numCourses; i++) graph[i] = new ArrayList<>();
+        int[] res = new int[numCourses];
+        //construct the graph
+        List<Integer>[] graph = new ArrayList[numCourses];
         int[] indeg = new int[numCourses];
-        for (int[] edge : prerequisites) {
-            int from = edge[1], to = edge[0];
-            graph[from].add(to);
-            indeg[to]++;
+        for (int i = 0; i < numCourses; i++) graph[i] = new ArrayList<>();
+        for (int[] e : prerequisites) {
+            int u  = e[1], v = e[0];
+            graph[u].add(v);
+            indeg[v]++;
         }
         
-        //find the vertices with 0 indegeree
+        //find the vertices with 0 indeg
         Queue<Integer> queue = new LinkedList<>();
         for (int i = 0; i < numCourses; i++) {
             if (indeg[i] == 0) queue.offer(i);
         }
         
-        //topological sorting
-        int[] res = new int[numCourses];
-        int i = 0; //count of courses added to the result
+        //toplogical sorting\
+        int ctn = 0;
         while (!queue.isEmpty()) {
-            int v = queue.poll();
-            res[i++] = v;
-            for (int u : graph[v]) {
-                if (--indeg[u] == 0) {
-                    queue.offer(u);
-                }
+            int u = queue.poll();
+            for (int v : graph[u]) {
+                if (--indeg[v] == 0) queue.offer(v);
             }
+            res[ctn++] = u;
         }
         
-        return i == numCourses? res : new int[0];
+        return ctn == numCourses? res : new int[]{};
     }
 }
-//Time: O(v + e); Space: O(v + e)

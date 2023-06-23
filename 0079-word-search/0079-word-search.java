76ffc7081a16c5ed762bf29bf46c48d7bec11bbd
Chao <42952910+kenwan006@users.noreply.github.com>
2023-06-23 15:48:07 -0700

    Time: 244 ms (34.55%), Space: 40.7 MB (75.08%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,34 +1,28 @@
 class Solution {
-    private int[][] dirs = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
-    private boolean[][] visited;
-    
     public boolean exist(char[][] board, String word) {
-        int m = board.length, n = board[0].length;
-        visited = new boolean[m][n];
-        
-        for (int i = 0; i < m; i ++) {
-            for (int j = 0; j < n; j++) {
-                if (board[i][j] == word.charAt(0)) {
-                    if(dfs(board, i, j, word, 0)) return true;
-                }
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                if (dfs(board, i, j, 0, word)) return true;
             }
         }
         return false;
     }
     
-    public boolean dfs(char[][] board, int i, int j, String word, int step) {
-        if (step == word.length()) return true;
+    int[][] dirs = {{-1, 0}, {1, 0}, {0, 1}, {0, -1}};
+    private boolean dfs(char[][] board, int i, int j, int k, String word) {
+        if (k == word.length()) return true;
+        if (i < 0 || j < 0 || i >= board.length || j >= board[0].length) return false;
         
-        if (i < 0 || j < 0 || i >= board.length || j >= board[0].length || visited[i][j]) return false;
+        char c = board[i][j];
+        if (c== '#'|| word.charAt(k) != c) return false;
         
-        if (board[i][j] != word.charAt(step)) return false;
-     
-        visited[i][j] = true;
+        board[i][j] = '#'; //mark as visited;
         for (int[] dir : dirs) {
-            if (dfs(board, i + dir[0], j + dir[1], word, step + 1)) return true;
+            if (dfs(board, i + dir[0], j + dir[1], k + 1, word)) return true;
         }
-        visited[i][j] = false;
+        board[i][j] = c; // remark as unvisited
         
         return false;
     }
 }
+//Time: O(m * n * 4 ^ L); Space: O()

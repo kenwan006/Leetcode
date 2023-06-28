15f069aec0f60fadb8f422fcc3ea00bf4c9d2ca7
Chao <42952910+kenwan006@users.noreply.github.com>
2023-06-28 15:50:55 -0700

    Time: 1 ms (95.60%), Space: 41.2 MB (53.84%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.java
+++ 0190-reverse-bits/0190-reverse-bits.java
@@ -3,10 +3,8 @@ public class Solution {
     public int reverseBits(int n) {
         int res = 0;
         for (int i = 0; i < 32; i++) {
-            res += (n & 1);
+            res = res * 2 + (n & 1);
             n = n >>> 1;
-            if (i == 31) break; //only need to shift 31 bits for the res
-            res = res << 1;
         }
         return res;
     }

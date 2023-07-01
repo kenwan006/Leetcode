9d1659fcd0b2120d4f7b020a587e093bf70e08e4
Chao <42952910+kenwan006@users.noreply.github.com>
2023-06-30 17:35:24 -0700

    Time: 0 ms (100.00%), Space: 39.9 MB (71.92%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,15 +1,13 @@
-/** dp **/
 class Solution {
     public int rob(int[] nums) {
         int n = nums.length;
-        if (n <= 1) return nums[0];
-        int[] dp = new int[n];
+        if (n == 1) return nums[0];
+        int[] dp = new int[n]; //dp[i] - the max amount you can rob from store 0 to store i
         dp[0] = nums[0];
         dp[1] = Math.max(nums[0], nums[1]);
         for (int i = 2; i < n; i++) {
-            dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i]);
+            dp[i] = Math.max(nums[i] + dp[i - 2], dp[i - 1]);
         }
         return dp[n - 1];
     }
 }
-//Time: O(n); Space: O(n)

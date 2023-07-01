f2cbe41422e07535cbdd090a1a5acf0abed1922e
Chao <42952910+kenwan006@users.noreply.github.com>
2023-06-30 18:29:18 -0700

    Time: 7 ms (65.27%), Space: 43.5 MB (37.42%) - LeetHub

--- 0139-word-break/0139-word-break.java
+++ 0139-word-break/0139-word-break.java
@@ -1,20 +1,17 @@
-/** dp **/
 class Solution {
     public boolean wordBreak(String s, List<String> wordDict) {
         int n = s.length();
-        boolean[] dp = new boolean[n + 1]; // dp[i] represents substring s[0..i) is breakable or not
-        dp[0] = true; 
-        for (int i = 1; i <= n; i++) {
+        boolean[] dp = new boolean[n + 1]; //dp[j] - s[0...j) can be segmented, j is exclusive
+        dp[0] = true;
+        for (int j = 0; j <= n; j++) {
             for (String word : wordDict) {
-                int len = word.length();
-                if (i < len) continue;
-                if(s.substring(i - len, i).equals(word) && dp[i - len]) {
-                    dp[i] = true;
-                    break; //once found, no need to check other words in the dict
+                if (word.length() > j) continue;
+                int i = j - word.length();
+                if (s.substring(i, j).equals(word) && dp[i]) {
+                    dp[j] = true;
                 }
             }
         }
-        return dp[n];    
+        return dp[n];
     }
 }
-//Time: O(n * m); Space: O(n)

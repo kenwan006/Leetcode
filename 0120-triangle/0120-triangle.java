30332501089ebad0712eefcd03bee4cac8ab04cb
Chao <42952910+kenwan006@users.noreply.github.com>
2023-07-05 17:30:18 -0700

    Time: 8 ms (22.96%), Space: 44 MB (59.04%) - LeetHub

--- 0120-triangle/0120-triangle.java
+++ 0120-triangle/0120-triangle.java
@@ -1,22 +1,22 @@
 class Solution {
     public int minimumTotal(List<List<Integer>> triangle) {
         int n = triangle.size();
-        int[][] path = new int[n][n]; // path[i][j] - refers to the path sum from top to the triangle[i][j]
-        for (int i = 0; i < n; i++) Arrays.fill(path[i], Integer.MAX_VALUE);
-        path[0][0] = triangle.get(0).get(0);
-        
+        //keep updating and store the min sum of path from top -> [i, j] at triangle[i][j]
         for (int i = 1; i < n; i++) {
             for (int j = 0; j <= i; j++) {
-                if (j > 0) path[i][j] = Math.min(path[i][j], path[i - 1][j - 1]);
-                if (j < n) path[i][j] = Math.min(path[i][j], path[i - 1][j]);
-                path[i][j] += triangle.get(i).get(j);
+                int smallest = Integer.MAX_VALUE;
+                if (j > 0) smallest = Math.min(smallest, triangle.get(i - 1).get(j - 1));
+                if (j < i) smallest = Math.min(smallest, triangle.get(i - 1).get(j));
+                int path = smallest + triangle.get(i).get(j); //at ith row, update the min path for each col
+                triangle.get(i).set(j, path);                  
             }
         }
         
-        //find the min at the (n-1)th row
+        //find the min at the bottom row
         int res = Integer.MAX_VALUE;
-        for (int j = 0; j < n; j++) res = Math.min(res, path[n-1][j]);
+        for (int j = 0; j < n; j++) res = Math.min(res, triangle.get(n - 1).get(j));
         return res;
     }
 }
-//Time: O(n ^ 2); Space: O(n ^ 2)
+//Time: O(n ^ 2); Space: O(1)
+

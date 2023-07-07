1b5535a27770a045c5e1c946407bfbfdf06903b8
Chao <42952910+kenwan006@users.noreply.github.com>
2023-07-07 16:14:03 -0700

    Time: 7 ms (51.76%), Space: 54.2 MB (62.24%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.java
+++ 0221-maximal-square/0221-maximal-square.java
@@ -1,39 +1,22 @@
 class Solution {
     public int maximalSquare(char[][] matrix) {
         int m = matrix.length, n = matrix[0].length;
-        int[][] dp = new int[m][n]; //dp[i][j] - refers to square area with all 1s ending with right bottom at matrix[i][j]
-        int maxArea = 0;
+        //dp[i][j] - refers to the edge length of the square with all 1s ending with right bottom at matrix[i-1][j-1]
+        int[][] dp = new int[m + 1][n + 1]; 
+        int maxLen = 0;
         
         //base case
-        for (int i = 0; i < m; i++) {
-            if (matrix[i][0] == '1') {
-                dp[i][0] = 1;
-                maxArea = 1;
-            } else {
-                dp[i][0] = 0;
-            }
-        }
-        for (int j = 0; j < n; j++) {
-            if (matrix[0][j] == '1') {
-                dp[0][j] = 1;
-                maxArea = 1;
-            } else {
-                dp[0][j] = 0;
-            }
-        }
-        
-        //dp
-        for (int i = 1; i < m; i++) {
-            for (int j = 1; j < n; j++) {
-                if (matrix[i][j] == '0') continue;
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (matrix[i - 1][j - 1] == '0') continue; //do not use matrix[i][j] for current cell
+                
                 //find the min among left square, upper sqaure and upper-left square
-                int L = (int) Math.sqrt(Math.min(dp[i - 1][j - 1], Math.min(dp[i - 1][j], dp[i][j - 1])));
-                dp[i][j] = (L + 1) * (L + 1);
-                maxArea = Math.max(maxArea, dp[i][j]);
+                dp[i][j] = Math.min(dp[i - 1][j - 1], Math.min(dp[i - 1][j], dp[i][j - 1])) + 1;
+                maxLen = Math.max(maxLen, dp[i][j]);
             }
         }
-        return maxArea;
+        return maxLen * maxLen;
     }
 }
-
 //Time: O(m * n); Space: O(m * n)
+

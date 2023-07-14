63084b87a75949cffe2a2b876c25d7f33434b2ef
Chao <42952910+kenwan006@users.noreply.github.com>
2023-07-14 14:44:24 -0700

    Time: 178 ms (43.33%), Space: 92.5 MB (42.00%) - LeetHub

---
+++ 1610-maximum-number-of-visible-points/1610-maximum-number-of-visible-points.java
@@ -0,0 +1,28 @@
+/** Sliding window **/
+class Solution {
+    public int visiblePoints(List<List<Integer>> points, int angle, List<Integer> location) {
+        List<Double> angles = new ArrayList<>();
+        int count = 0; //count the points at the location
+        for (List<Integer> point : points) {
+            int dx = point.get(0) - location.get(0); 
+            int dy = point.get(1) - location.get(1);
+            if (dx == 0 && dy == 0) {
+                count++;
+                continue;
+            }
+            double deg = Math.atan2(dy, dx) * (180 / Math.PI); 
+            angles.add(deg);
+            if (deg < 0) angles.add(deg + 360); //handle the edge of -180 and 180
+        }
+        Collections.sort(angles);
+        
+        //sliding window - [i, j]
+        int maxCount = 0, i = 0;
+        for (int j = 1; j < angles.size(); j++) {
+            while (angles.get(j) - angles.get(i) > angle) i++;
+            maxCount = Math.max(maxCount, j - i + 1);
+        }
+        return maxCount + count; 
+    }
+}
+//Time: O(n); Space: O(n)

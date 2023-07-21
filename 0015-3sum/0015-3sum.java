4d5e413460470b391a6ce7a9f18421c1be776bf4
Chao <42952910+kenwan006@users.noreply.github.com>
2023-07-21 12:27:32 -0700

    Time: 36 ms (92.99%), Space: 51.4 MB (37.36%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,26 +1,31 @@
-/** Two pointer **/
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         List<List<Integer>> res = new ArrayList<>();
+        int n = nums.length;
         Arrays.sort(nums);
-        for (int i = 0; i < nums.length - 2; i++) {
-            if (i > 0 && nums[i] == nums[i - 1]) continue; //Check 1st item, skip the duplicate 
+        
+        for (int i = 0; i < n - 2; i++) {
+            //chose the 1st num
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+            if (nums[i] > 0) continue;
+            int target = -nums[i];
             
-            //2-sum
-            int lo = i + 1, hi = nums.length - 1, target = -nums[i];
+            //2sum for 2nd  and 3rd num
+            int lo = i + 1, hi = n - 1;
             while (lo < hi) {
-                int sum = nums[lo] + nums[hi];
-                if (sum > target) hi--;
-                else if (sum < target) lo++;
-                else {
-                    while (lo < hi && nums[lo] == nums[lo + 1]) lo++; //Check 2nd item, skip the duplicate 
-                    while (lo < hi && nums[hi] == nums[hi - 1]) hi--; //Check 3rd item, skip the duplicate 
-                    res.add(Arrays.asList(nums[i], nums[lo++], nums[hi--]));
+                if (nums[lo] + nums[hi] < target){
+                    lo++;
+                } else if (nums[lo] + nums[hi] > target) {
+                    hi--;
+                } else {
+                    res.add(Arrays.asList(nums[i], nums[lo], nums[hi]));
+                    while (lo < hi && nums[hi] == nums[hi - 1]) hi--;
+                    while (lo < hi && nums[lo] == nums[lo + 1]) lo++;
+                    hi--;
+                    lo++;
                 }
             }
         }
         return res;
     }
 }
-//Time: O(n * n); Space: O(n)
-

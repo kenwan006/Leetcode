ed8bfd9701c09cd9b1a441a9b7ad629ea06e3703
Chao <42952910+kenwan006@users.noreply.github.com>
2023-07-27 17:32:41 -0700

    Time: 1 ms (100.00%), Space: 59.6 MB (52.92%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,21 +1,13 @@
-/** Two pointer
-* keep extending the right bound of the subarray.
-* If adding the next num cause the sum to be negative, then skip this num - move the left pointer to the next num
-* Otherwise add this num to current sum and move the right  pointer to the next
-*/
+/** Kadane's Algorithm **/
 class Solution {
     public int maxSubArray(int[] nums) {
-        int i = 0, j = 0, n = nums.length;
-        int sum = 0, res = Integer.MIN_VALUE;
-        while (j < n) {
-            sum += nums[j++];
-            res = Math.max(res, sum);
-            if (sum < 0) {
-                sum = 0;
-                i = j;
-            }
+        int globalMax = nums[0], localMax = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            if (localMax < 0) localMax = 0;
+            localMax += nums[i];
+            globalMax = Math.max(globalMax, localMax);
         }
-        return res;
+        return globalMax;
     }
 }
 //Time: O(n); Space: O(1)

f6024b851c1c198656ab0401c1658356f0b04a44
Chao <42952910+kenwan006@users.noreply.github.com>
2023-08-02 15:50:47 -0700

    Time: 8 ms (92.94%), Space: 44.3 MB (50.60%) - LeetHub

--- 0459-repeated-substring-pattern/0459-repeated-substring-pattern.java
+++ 0459-repeated-substring-pattern/0459-repeated-substring-pattern.java
@@ -1,10 +1,25 @@
-/** String concatenation *
-* If s = "abcabc", let t = s + s = "abcabcabcabc", then remove head and tail of t to get t = "bcabcabcab"
-* Check if t contains s, if yes then it means s has repeated pattern.
-**/
 class Solution {
     public boolean repeatedSubstringPattern(String s) {
-        String t = s + s;
-        return t.substring(1, t.length() - 1).contains(s);
+        int[] next = getNext(s);
+        int n = s.length();
+        int len = next[n - 1];
+        return len > 0 && len % (n - len) == 0;
+    }
+    
+    //KMP to get the array - next
+    private int[] getNext(String s) {
+        int n = s.length();
+        int[] next = new int[n];
+        int j = 0;
+        for (int i = 1; i <n; i++) {
+            while (j > 0 && s.charAt(i) != s.charAt(j)) {
+                j = next[j - 1];
+            }
+            if (s.charAt(i) == s.charAt(j)) {
+                j++;
+            }
+            next[i] = j;
+        }
+        return next;
     }
 }

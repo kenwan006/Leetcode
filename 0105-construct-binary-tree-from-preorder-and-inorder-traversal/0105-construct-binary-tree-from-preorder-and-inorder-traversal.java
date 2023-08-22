0ec731fefe85f9b2ee10bce706a897402078bd05
Chao <42952910+kenwan006@users.noreply.github.com>
2023-08-22 16:00:19 -0700

    Time: 2 ms (81.98%), Space: 43.1 MB (84.01%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -14,20 +14,23 @@
  * }
  */
 class Solution {
-    Map<Integer, Integer> valToIndex;
+    Map<Integer, Integer> valToIndex = new HashMap<>();
     public TreeNode buildTree(int[] preorder, int[] inorder) {
         int n = inorder.length;
-        valToIndex = new HashMap<>(); //map val -> index in inorder
-        for (int i = 0; i < n; i++) valToIndex.put(inorder[i], i);
+        for (int i = 0; i < n; i++) {
+            valToIndex.put(inorder[i], i);
+        }
         return buildTree(preorder, 0, n - 1, inorder, 0, n - 1);
     }
-    
-    private TreeNode buildTree(int[] preorder, int preStart, int preEnd, int[] inorder, int inStart,int inEnd) {
-        if (preEnd < preStart) return null;
-        int val = preorder[preStart], index = valToIndex.get(val);// index of root in the inorder
+    private TreeNode buildTree(int[] preorder, int preStart, int preEnd, int[] inorder, int inStart, int inEnd) {
+        if (preStart > preEnd) return null;
+        
+        int val = preorder[preStart]; //root val
+        int index = valToIndex.get(val);
         int lsize = index - inStart;
-        TreeNode left = buildTree(preorder, preStart + 1, preStart + lsize, inorder, inStart, index - 1);
-        TreeNode right = buildTree(preorder, preStart + lsize + 1, preEnd, inorder, index + 1, inEnd);
-        return new TreeNode(val, left, right);
+        TreeNode root = new TreeNode(val);
+        root.left = buildTree(preorder, preStart + 1, preStart + lsize, inorder, inStart, index - 1);
+        root.right = buildTree(preorder, preStart + lsize + 1, preEnd, inorder, index + 1, inEnd);
+        return root;
     }
 }

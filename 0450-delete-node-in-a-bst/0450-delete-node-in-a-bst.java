5cfca6444ed0c9e385be467ffcc1824cd8841380
Chao <42952910+kenwan006@users.noreply.github.com>
2023-08-24 12:24:26 -0700

    Time: 0 ms (100.00%), Space: 45 MB (78.44%) - LeetHub

--- 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.java
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.java
@@ -16,44 +16,24 @@
 class Solution {
     public TreeNode deleteNode(TreeNode root, int key) {
         if (root == null) return null;
-        if (root.val == key) return insert(root.left, root.right);
-        
-        //find the node to be deleted
-        TreeNode parent = null;
-        TreeNode curr = root;
-        while (curr != null) {
-            if (curr.val == key) break;
-            parent = curr;
-            if (curr.val < key) curr = curr.right;
-            else curr = curr.left;
+        if (root.val == key) {
+            //insert the left child to the leftmost of right child
+            if (root.right == null) return root.left;
+            if (root.left == null) return root.right;
+            
+            TreeNode curr = root.right;
+            while (curr.left != null) {
+                curr = curr.left;
+            }
+            curr.left = root.left;
+            root = root.right;
+            return root;
         }
         
-        //key is not in the BST
-        if (curr == null) return root;
+        if (root.val < key) root.right = deleteNode(root.right, key);
+        if (root.val > key) root.left = deleteNode(root.left, key);
         
-        //break the connection between parent and the deleted node
-        if (parent.left != null && parent.left.val == key) parent.left = null;
-        if (parent.right != null && parent.right.val == key) parent.right = null;
-        
-        //insert one child to the child
-        TreeNode child = insert(curr.left, curr.right);
-        
-        //break connection between deleted node and child nodes
-        curr.left = null;
-        curr.right = null;
-        
-        //insert the combined child to parent
-        insert(parent, child);
-        
-        return root;
-    }
-    
-    //insert a node to a BST
-    private TreeNode insert(TreeNode root, TreeNode node) {
-        if (root == null) return node;
-        if (node == null) return root;
-        if (root.val > node.val) root.left = insert(root.left, node);
-        else root.right = insert(root.right, node);
         return root;
     }
 }
+//Time: O(log(n)); Space: O(1)

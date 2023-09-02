209d1de9150d50edd92d83d4732269ee6700eb8c
Chao <42952910+kenwan006@users.noreply.github.com>
2023-09-01 17:41:01 -0700

    Time: 1 ms (100.00%), Space: 59.8 MB (29.56%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,13 +1,11 @@
-/** Kadane's Algorithm **/
 class Solution {
     public int maxSubArray(int[] nums) {
-        int globalMax = nums[0], localMax = nums[0];
-        for (int i = 1; i < nums.length; i++) {
-            if (localMax < 0) localMax = 0;
-            localMax += nums[i];
-            globalMax = Math.max(globalMax, localMax);
+        int sum = 0, maxSum = Integer.MIN_VALUE;
+        for (int num : nums) {
+            if (sum < 0) sum = 0;
+            sum += num;
+            maxSum = Math.max(maxSum, sum);
         }
-        return globalMax;
+        return maxSum;
     }
 }
-//Time: O(n); Space: O(1)

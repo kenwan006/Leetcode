6be67f291ae393623ea8ca475abad85c865f1e01
Chao <42952910+kenwan006@users.noreply.github.com>
2023-09-05 15:11:21 -0700

    Time: 1 ms (85.96%), Space: 44.1 MB (73.54%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -1,9 +1,23 @@
+/** Greey 
+** Peaks and valleys 
+*/
 class Solution {
     public int maxProfit(int[] prices) {
-        int profit = 0;
-        for (int i = 1; i < prices.length; i++) {
-            if (prices[i] > prices[i - 1]) profit += prices[i] - prices[i - 1];
+        int profit = 0, peak = prices[0], valley = prices[0];
+        int i = 0, n = prices.length;
+        while (i < n - 1) {
+            //find valley
+            while (i < n - 1 && prices[i] >= prices[i + 1]) i++;
+            valley = prices[i];
+            
+            //find peak
+            while (i < n - 1 && prices[i] <= prices[i + 1]) i++;
+            peak = prices[i];
+            
+            profit += peak - valley;
         }
         return profit;
     }
 }
+//Time: O(n); Space: O(1)
+

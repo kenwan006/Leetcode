4c1912181982bad961ed5f9f7a43eb417ec92954
Chao <42952910+kenwan006@users.noreply.github.com>
2023-09-05 15:54:26 -0700

    Time: 2 ms (80.26%), Space: 44.1 MB (68.91%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,17 +1,13 @@
 class Solution {
     public boolean canJump(int[] nums) {
-        int i = 1, n = nums.length;
-        int start = 1, end = nums[0];
-        int farthest = 0;
-        while (i < n) {
-            for (int j = start; j <= end && j < n; j++) {
-                farthest = Math.max(farthest, j + nums[j]);
+        int n = nums.length;
+        int start = 0, end = 0, farthest = 0;
+        for (int jump = 0; jump < n; jump++) {
+            for (int i = start; i <= end && i < n; i++) {
+                farthest = Math.max(farthest, i + nums[i]);
             }
-            //update starting position for the next jump
-            i++;
             start = end + 1;
             end = farthest;
-            if (end >= n - 1) return true;
         }
         return farthest >= n - 1;
     }

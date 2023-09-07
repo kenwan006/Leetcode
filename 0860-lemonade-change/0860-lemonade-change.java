f091bd3ea1abc613d00d4221b554834c1915a56e
Chao <42952910+kenwan006@users.noreply.github.com>
2023-09-07 10:57:03 -0700

    Time: 1 ms (100.00%), Space: 56.6 MB (56.24%) - LeetHub

--- 0860-lemonade-change/0860-lemonade-change.java
+++ 0860-lemonade-change/0860-lemonade-change.java
@@ -1,20 +1,20 @@
 class Solution {
     public boolean lemonadeChange(int[] bills) {
-        int[] count = new int[2]; //count of $5 and $10
+        int five = 0, ten = 0;
         for (int bill : bills) {
             if (bill == 5) {
-                count[0]++;
+                five++;
             } else if (bill == 10) {
-                count[1]++;
-                if (--count[0] < 0) return false;
+                ten++;
+                if (--five < 0) return false;
             } else {
-                if (count[1] > 0) {
-                    count[1]--;
-                    count[0]--;
+                if (ten > 0) {
+                    ten--;
+                    five--;
                 } else {
-                    count[0] -= 3;
+                    five -= 3;
                 }
-                if (count[0] < 0) return false;
+                if (five < 0) return false;
             }
         }
         return true;

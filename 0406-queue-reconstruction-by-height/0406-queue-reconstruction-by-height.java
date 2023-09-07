d49ea6f7626b2b89273f1e637b114b7d7530d7b4
Chao <42952910+kenwan006@users.noreply.github.com>
2023-09-07 12:05:54 -0700

    Time: 9 ms (26.96%), Space: 44.1 MB (93.51%) - LeetHub

--- 0406-queue-reconstruction-by-height/0406-queue-reconstruction-by-height.java
+++ 0406-queue-reconstruction-by-height/0406-queue-reconstruction-by-height.java
@@ -1,17 +1,9 @@
 /** Greedy **/
 class Solution {
     public int[][] reconstructQueue(int[][] people) {
-        Comparator<int[]> comparator = new Comparator<>(){
-            @Override
-            public int compare(int[] a, int[] b) {
-                int result = Integer.compare(b[0], a[0]);
-                if (result == 0) result = Integer.compare(a[1], b[1]);
-                return result;
-            }
-        };
         // Sort the height in descending order
         // For the same height, sort the k in ascending order
-        Arrays.sort(people, comparator); 
+        Arrays.sort(people, (a, b) -> a[0] == b[0]? a[1] - b[1] : b[0] - a[0]); 
         
         // Insert each person to the right place
         List<int[]> list = new LinkedList<>();

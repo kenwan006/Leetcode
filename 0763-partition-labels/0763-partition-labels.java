ccaad2afe53c64784cf848f57e6baaa777e998e3
Chao <42952910+kenwan006@users.noreply.github.com>
2023-09-08 12:27:45 -0700

    Time: 8 ms (52.91%), Space: 41.5 MB (42.48%) - LeetHub

--- 0763-partition-labels/0763-partition-labels.java
+++ 0763-partition-labels/0763-partition-labels.java
@@ -1,21 +1,21 @@
+/** Greedy **/
 class Solution {
     public List<Integer> partitionLabels(String s) {
-        int[] bounds = new int[26]; //right most index of the letter
-        for (int i = 0; i < s.length(); i++) bounds[s.charAt(i) - 'a'] = i;
+        int n = s.length();
+        Map<Character, Integer> map = new HashMap<>(); //map each letter -> farthest position in s
+        for (int i = 0; i < n; i++) map.put(s.charAt(i), i);
         
-        //partition
         List<Integer> res = new ArrayList<>();
         int start = 0, end = 0;
-        for (int i = 0; i < s.length(); i++) {
-            end = Math.max(end, bounds[s.charAt(i) - 'a']);
-            
-            //if the current char located at its rightmost position
+        for (int i = 0; i < n; i++) {
+            end = Math.max(end, map.get(s.charAt(i))); //update the max end of current partition
             if (i == end) {
                 res.add(end - start + 1);
-                start = end + 1; //start for next partition
+                start = end + 1;
             }
         }
         return res;
     }
 }
-//Time: O(n); Space: O(1)
+//Time: O(n); Space: O(n)
+

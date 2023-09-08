6bca74c614dc6c0ebadb6b3b81990a174f16dd21
Chao <42952910+kenwan006@users.noreply.github.com>
2023-09-08 15:06:06 -0700

    Time: 8 ms (93.42%), Space: 46.1 MB (13.95%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,29 +1,22 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-        Arrays.sort(intervals, (a, b) -> a[0] - b[0]); //sort by start of interval
-        List<int[]> list = new ArrayList<>();
-        int prevStart = intervals[0][0], prevEnd = intervals[0][1];
-        
-        for (int i = 1; i < intervals.length; i++) {
-            int start = intervals[i][0], end = intervals[i][1];
-            if (start > prevEnd) {
-                list.add(new int[]{prevStart, prevEnd});
-                prevStart = start;
-                prevEnd = end;
-            } else {
-                prevStart = Math.min(prevStart, start);
-                prevEnd = Math.max(prevEnd, end);
+        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
+        List<int[]> res = new ArrayList<>();
+        int start = intervals[0][0], end = intervals[0][1];
+        for (int[] interval : intervals) {
+            if (end < interval[0]) { //start a new interval
+                res.add(new int[]{start, end});
+                start = interval[0];
+                end = interval[1];
+            } else { //update the overlapped interval
+                end = Math.max(end, interval[1]);
             }
         }
         
         //add the last interval
-        list.add(new int[]{prevStart, prevEnd});
+        res.add(new int[]{start, end});
         
-        int[][] res = new int[list.size()][2];
-        for (int i = 0; i < res.length; i++) res[i] = list.get(i);
-        return res;
-                              
+        return res.toArray(new int[res.size()][2]);
     } 
 }
-//Time: O(n * log(n)); Space: O(1)
-
+//Time: O(n * log(n)); Space: O(log(n)) for sorting 

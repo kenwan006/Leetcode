cfa9057e6ce3e40df53a7fc732af5e0e07b392ee
Chao <42952910+kenwan006@users.noreply.github.com>
2023-09-08 11:27:26 -0700

    Time: 45 ms (94.75%), Space: 97.2 MB (66.12%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.java
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.java
@@ -1,12 +1,9 @@
+/** Greedy
+* Sort by the end in ascending order 
+*/
 class Solution {
     public int eraseOverlapIntervals(int[][] intervals) {
-        Arrays.sort(intervals, new Comparator<>(){
-            @Override
-            public int compare(int[] a, int[] b) {
-                if (a[1] == b[1]) return Integer.compare(a[0], b[0]);
-                else return Integer.compare(a[1], b[1]);
-            }
-        });
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[1], b[1]));
         int count = 1; //count of overlapped groups
         int end = intervals[0][1];
         for (int[] interval : intervals) {
@@ -18,3 +15,4 @@ class Solution {
         return intervals.length - count;
     }
 }
+//Time:  O(n * log(n)); Space: O(log(n))

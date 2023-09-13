17cd309c24b043154594203e0ee7dc8d12b572d3
Chao <42952910+kenwan006@users.noreply.github.com>
2023-09-13 12:11:25 -0700

    Time: 607 ms (17.31%), Space: 78.5 MB (24.34%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.java
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.java
@@ -1,61 +1,40 @@
-/** MST 
-* connect every two points, and sorting their length in ascending order. 
-* Everytime add the minimum edge to the mst if it's not been added previously.
-**/
+/** Prim MST **/
 class Solution {
     public int minCostConnectPoints(int[][] points) {
-        //edges between every two points i and j
-        int n = points.length;
-        List<int[]> edges = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                int d =  Math.abs(points[i][0] - points[j][0]) + Math.abs(points[i][1] - points[j][1]);
-                edges.add(new int[]{i, j, d});
+        int V = points.length;
+        
+        //build the graph
+        List<List<int[]>> adj = new ArrayList<>();
+        for (int i = 0; i < V; i++) adj.add(new ArrayList<>());
+        for (int i = 0; i < V; i++) {
+            for (int j = i + 1; j < V; j++) {
+                int w = Math.abs(points[i][0] - points[j][0]) + Math.abs(points[i][1] - points[j][1]);
+                adj.get(i).add(new int[]{j, w});
+                adj.get(j).add(new int[]{i, w});
             }
         }
-        Collections.sort(edges, (a, b) -> a[2] - b[2]); //sort the edge by their length
         
-        //kruskal algo
-        UF uf = new UF(n);
+        //prim mst
         int mst = 0;
-        for (int[] edge : edges) {
-            int u = edge[0], v = edge[1], d = edge[2];
-            if (!uf.union(u, v)) continue; //cycle detected when u and v already connected in mst
-            mst += d; 
+        boolean[] inMST = new boolean[V];
+        inMST[0] = true; //pick the vertex 0 as the start
+        PriorityQueue<int[]> pq = new PriorityQueue<>((o1, o2) -> o1[1] - o2[1]);
+        for (int[] e : adj.get(0)) pq.offer(e);
+        
+        while (!pq.isEmpty()) {
+            int[] curr = pq.poll();
+            int v = curr[0], w = curr[1];
+            if (inMST[v]) continue;
+            inMST[v] = true;
+            mst += w;
+            for (int[] e : adj.get(v)) {
+                if (!inMST[e[0]]) {
+                    pq.offer(e);
+                }
+            }
         }
+        
         return mst;
-        
-    }
-    
-    class UF {
-        int[] parent;
-        int[] size;
-        public UF(int n) {
-            parent = new int[n];
-            size = new int[n];
-            for (int i = 0; i < n; i++) parent[i] = i;
-        }
-        
-        public boolean union(int p, int q) {
-            int rootP = find(p), rootQ = find(q);
-            if (rootP == rootQ) return false;
-            
-            if (size[rootP] > size[rootQ]) {
-                parent[rootQ] = rootP;
-                size[rootP] += size[rootQ];
-            } else {
-                parent[rootP] = rootQ;
-                size[rootQ] += size[rootP];
-            }
-            return true;
-        }
-        
-        public int find(int x) {
-            if (parent[x] != x) {
-                parent[x] = find(parent[x]);
-            }
-            return parent[x];
-        }
     }
 }
-//Time: O(n ^2 * log(n)); Space: O(n ^2)
+//Time: O(n * n * log(n)); Space: (n * n)  n - number of points

162290ce722fb67e39279bef371389a9219903fe
Chao <42952910+kenwan006@users.noreply.github.com>
2023-09-15 15:05:08 -0700

    Time: 0 ms (100.00%), Space: 40.4 MB (69.49%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -1,17 +1,26 @@
 class Solution {
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-        int m = obstacleGrid.length, n = obstacleGrid[0].length;
-        int[][] dp = new int[m][n]; //dp[i][j] - refers to the number of path from top left to grid[i][j]
-        if (obstacleGrid[0][0] == 0) dp[0][0] = 1; //starting point could be an obstacle
+        //use obstacleGrid[i][j] to represent number of pathes from start to [i][j].
+        //No need to create a new dp[][]
         
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (obstacleGrid[i][j] == 1) continue; // skip the obstacle
-                if (i > 0 && obstacleGrid[i - 1][j] == 0) dp[i][j] += dp[i - 1][j];
-                if (j > 0 && obstacleGrid[i][j - 1] == 0) dp[i][j] += dp[i][j - 1];
-            }
+        int m = obstacleGrid.length, n = obstacleGrid[0].length;
+        
+        //check the start point - if it's an obstacle, set it to 0
+        obstacleGrid[0][0] = obstacleGrid[0][0] == 1? 0 : 1; 
+        
+        //check first column
+        for (int i = 1; i < m; i++) obstacleGrid[i][0] = obstacleGrid[i][0] == 1? 0 : obstacleGrid[i - 1][0];
+        
+        //check first row
+        for (int j = 1; j < n; j++) obstacleGrid[0][j] = obstacleGrid[0][j] == 1? 0 : obstacleGrid[0][j - 1];
+        
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (obstacleGrid[i][j] == 1) obstacleGrid[i][j] = 0;
+                else obstacleGrid[i][j] = obstacleGrid[i - 1][j] + obstacleGrid[i][j - 1];
+            } 
         }
-        return dp[m - 1][n - 1];
+        return obstacleGrid[m - 1][n - 1];
     }
 }
-//Time: O(n ^ 2); Space: O(n ^ 2)
+//Time: O(m * n); Space: O(1)

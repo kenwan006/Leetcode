4c734f7f6f9740dfbb0d901b843bd31826530a50
Chao <42952910+kenwan006@users.noreply.github.com>
2023-09-15 23:22:56 -0700

    Time: 0 ms (100.00%), Space: 38.4 MB (99.95%) - LeetHub

---
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int numTrees(int n) {
+        // dp[i] - means the number of unique bst given i nodes
+        // focus on the number of total nodes not the value of each node
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) { //i - total number of nodes in the bst
+            for (int j = 0; j < i; j++) { // j - nodes in the left child; i - 1 - j nodes in the right
+                dp[i] += dp[j] * dp[i - 1 - j];
+            }
+        }
+        return dp[n];
+    }
+}
+//Time: O(n); Space: O(n)

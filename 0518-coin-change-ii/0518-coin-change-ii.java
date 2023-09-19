360d367b4873595a72b929bf72d03c2bf6b5d6df
Chao <42952910+kenwan006@users.noreply.github.com>
2023-09-19 11:59:19 -0700

    Time: 2 ms (100.00%), Space: 40.3 MB (63.03%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -1,23 +1,12 @@
-/** dp **/
 class Solution {
     public int change(int amount, int[] coins) {
-        int n = coins.length;
-        int[][] dp = new int[n + 1][amount + 1];
-        
-        //for (int j = 1; j <= amount; j++) dp[0][j] = 0; //nothing to choose
-        for (int i = 0; i <= n; i++) dp[i][0] = 1; //volume is 0
-        
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= amount; j++) {
-                if (j < coins[i - 1]) {
-                    dp[i][j] = dp[i - 1][j];
-                } else {
-                    dp[i][j] = dp[i - 1][j] + dp[i][j - coins[i - 1]];
-                    //do not use dp[i][j] = dp[i - 1][j] + dp[i - 1][j - coins[i - 1]] as ith conin - coins[i - 1] can be used unlimited times
-                }
+        int[] dp = new int[amount + 1];
+        dp[0] = 1;
+        for (int coin : coins) {
+            for (int j = coin; j <= amount; j++) {
+                dp[j] += dp[j - coin];
             }
         }
-        return dp[n][amount];
+        return dp[amount];
     }
 }
-//Time: O(m * n); Space: O(m * n)

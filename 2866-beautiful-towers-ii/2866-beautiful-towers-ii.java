dacc6b2b6c3f0a504b0c118f0099d50337504051
Chao <42952910+kenwan006@users.noreply.github.com>
2023-09-24 11:38:43 -0700

    Time: 81 ms (100.00%), Space: 68.3 MB (33.33%) - LeetHub

--- 2866-beautiful-towers-ii/2866-beautiful-towers-ii.java
+++ 2866-beautiful-towers-ii/2866-beautiful-towers-ii.java
@@ -1,9 +1,13 @@
 class Solution {
     public long maximumSumOfHeights(List<Integer> maxHeights) {
         int n = maxHeights.size();
-        long[] left = new long[n];
+        long[] left = new long[n]; //left[i] - sum of the heights of the beautiful tower maxHeights[0..i]
         Stack<Integer> stack = new Stack<>();
         for (int i = 0; i < n; i++) {
+            /** Iterate over from left to right, if the height is increasing, then push its index to the stack;
+            **  Otherwise, keep popping out until find the index = prev_i on the top with height  <= curr height
+            **  - All the height between (prev_i, i] has to be set to curr height
+            **/
             int currHeight = maxHeights.get(i);
             while (!stack.isEmpty() && currHeight < maxHeights.get(stack.peek())) stack.pop();
             
@@ -17,8 +21,9 @@ class Solution {
         }
         
         stack.clear();
-        long[] right = new long[n];
+        long[] right = new long[n]; //right[i] - sum of the heights of the beautiful tower maxHeights[i.. n-1]
         for (int i = n - 1; i >= 0; i--) {
+            /** Iterate over from right to left, if the height is increasing, then push its index to the stack; Otherwise... **/
             int currHeight = maxHeights.get(i);
             while (!stack.isEmpty() && currHeight < maxHeights.get(stack.peek())) stack.pop();
             
@@ -33,9 +38,10 @@ class Solution {
         
         long res = 0;
         for (int i = 0; i < n; i++) {
-            long sum = left[i] + right[i] - maxHeights.get(i); //maxHeight[i] added twice
+            long sum = left[i] + right[i] - maxHeights.get(i); //maxHeights[i] added twice
             res = Math.max(res, sum);
         }
         return res;
     }
 }
+//Time: O(n); Space: O(n)

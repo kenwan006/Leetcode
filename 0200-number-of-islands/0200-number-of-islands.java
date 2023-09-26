d4c9aca7055cf981794cd0389a8c7b470b400622
Chao <42952910+kenwan006@users.noreply.github.com>
2023-09-26 12:07:04 -0700

    Time: 5 ms (29.66%), Space: 50.1 MB (21.78%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -5,22 +5,28 @@ class Solution {
             for (int j = 0; j < grid[0].length; j++) {
                 if (grid[i][j] == '1') {
                     count++;
-                    dfs(i, j, grid);
+                    bfs(i, j, grid);
                 }
             }
         }
         return count;
     }
     
-    int[][] dirs = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
-    
-    private void dfs(int i, int j, char[][] grid) {
-        if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] == '0') return;
+    int[][] dirs = {{-1, 0}, {1, 0}, {0, 1}, {0, -1}};
+    private void bfs(int i, int j, char[][] grid) {
+        Queue<int[]> queue = new LinkedList<>();
+        queue.add(new int[]{i, j});
         
-        grid[i][j] = '0'; //mark as visited, turn '1' to '0'
-        
-        for (int[] dir : dirs) {
-            dfs(i + dir[0], j + dir[1], grid);
+        while (!queue.isEmpty()) {
+            int[] curr = queue.poll();
+            
+            for (int[] dir : dirs) { //check neighbors
+                int ii = curr[0] + dir[0];
+                int jj = curr[1] + dir[1];
+                if (ii < 0 || jj < 0 || ii >= grid.length || jj >= grid[0].length || grid[ii][jj] == '0') continue;
+                grid[ii][jj] = '0'; //mark as visited, turn '1' to '0'
+                queue.add(new int[]{ii, jj});
+            }
         }
     }
 }

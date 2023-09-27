f8c09bf53aa1add6f842df2477504a3969c05758
Chao <42952910+kenwan006@users.noreply.github.com>
2023-09-27 16:48:19 -0700

    Time: 74 ms (74.72%), Space: 166.1 MB (33.43%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
@@ -1,31 +1,22 @@
 class Solution {
     public boolean validPath(int n, int[][] edges, int source, int destination) {
-        UF uf = new UF(n);
-        for (int[] edge : edges) {
-            int u = edge[0], v = edge[1];
-            uf.union(u, v);
+        List<List<Integer>> graph = new ArrayList<>();
+        for (int i = 0; i < n; i++) graph.add(new ArrayList<>());
+        for (int[] e : edges) {
+            int u = e[0], v = e[1];
+            graph.get(u).add(v);
+            graph.get(v).add(u);
         }
-        return uf.union(source, destination);
+        return dfs(source, destination, new boolean[n], graph);
     }
     
-    class UF {
-        int[] parent;
-        public UF(int n) {
-            parent = new int[n];
-            for (int i = 0; i < n; i++) parent[i] = i;
+    //dfs - starting from node u, check if we can reach dst
+    private boolean dfs(int u, int dst, boolean[] visited, List<List<Integer>> graph) {
+        if (u == dst) return true;
+        visited[u] = true;
+        for (int v : graph.get(u)) {
+            if (!visited[v] && dfs(v, dst, visited, graph)) return true;
         }
-        private boolean union(int p, int q) {
-            int rootP = find(p);
-            int rootQ = find(q);
-            if (rootP == rootQ) return true;
-            parent[rootP] = rootQ;
-            return false;
-        }
-        private int find(int x) {
-            if (x != parent[x]) {
-                parent[x] = find(parent[x]);
-            }
-            return parent[x];
-        }
+        return false;
     }
 }

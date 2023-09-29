874c5579cc9e1c2d766f8cb6acc0fc3efa9317a2
Chao <42952910+kenwan006@users.noreply.github.com>
2023-09-29 16:03:07 -0700

    Time: 0 ms (100.00%), Space: 44.8 MB (9.67%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,29 +1,44 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int[] res = {-1, -1};
-        if (nums.length == 0 || target < nums[0] || target > nums[nums.length - 1]) return res;
+        int n = nums.length, start = -1, end = -1;
+        if (n == 0 || target < nums[0] || target > nums[n - 1]) return new int[]{start, end};
         
-        int lo = 0, mid = 0, hi = nums.length - 1;
-        
-        //find the first one
-        while (lo <= hi) {
-            mid = lo + (hi - lo) / 2;
-            if (nums[mid] < target) lo = mid + 1;
-            else hi = mid - 1; //try to push the right bound to the left
+        start = leftBound(nums, target);
+        end = rightBound(nums, target);
+        return new int[]{start, end};
+    }
+    
+    //find  leftmost num == target
+    private int leftBound(int[] nums, int target) {
+        int lo = 0, hi = nums.length - 1, res = -1;
+        while (lo <= hi) { //should consider lo == hi
+            int mid = lo + (hi - lo) / 2;
+            if (nums[mid] > target) {
+                hi = mid - 1;
+            } else if (nums[mid] < target) {
+                lo = mid + 1;
+            } else {
+                res = mid;
+                hi = mid - 1;
+            }
         }
-        if (nums[lo] == target) res[0] = lo;
-        
-        //find the last one
-        lo = 0; hi = nums.length - 1;
+        return res;
+    }
+    
+    //find the right most num == target
+    private int rightBound(int[] nums, int target) {
+        int lo = 0, hi = nums.length - 1, res = -1;
         while (lo <= hi) {
-            mid = lo + (hi - lo) / 2;
-            if (nums[mid] > target) hi = mid - 1;
-            else lo = mid + 1;
-           
+            int mid = lo + (hi - lo) / 2;
+            if (nums[mid] > target) {
+                hi = mid - 1;
+            } else if (nums[mid] < target) {
+                lo = mid + 1;
+            } else {
+                res = mid;
+                lo = mid + 1;
+            }
         }
-        if (nums[hi] == target) res[1] = hi;
-        
         return res;
     }
 }
-//Time: O(log(n)); Space: O(1)

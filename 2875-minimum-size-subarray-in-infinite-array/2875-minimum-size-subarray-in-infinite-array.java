30278a2b6baea218a06faa04e2ec50da53c46620
Chao <42952910+kenwan006@users.noreply.github.com>
2023-10-01 16:08:53 -0700

    Time: 41 ms (66.67%), Space: 72.3 MB (33.33%) - LeetHub

--- 2875-minimum-size-subarray-in-infinite-array/2875-minimum-size-subarray-in-infinite-array.java
+++ 2875-minimum-size-subarray-in-infinite-array/2875-minimum-size-subarray-in-infinite-array.java
@@ -1,29 +1,26 @@
+/** Sliding window **/
 class Solution {
     public int minSizeSubarray(int[] nums, int target) {
-        int n = nums.length, sum = 0;
+        int n = nums.length;
+        long sum = 0;
         for (int num : nums) sum += num;
-        int k = target / sum; //repetitions of nums
+        int k = (int) (target / sum); //repetition of nums
         if (target % sum == 0) return k * n;
         
-        target -= k * sum;
+        target %= sum;
         
-        int i = 0, j = 0; //sliding window [i..j]
-        int subsum = nums[0], res = Integer.MAX_VALUE;
-        while (j < 2 * n) {
-            if (subsum == target) {
-                res = Math.min(res, j - i + 1);
-                subsum -= nums[i % n]; //move left bound
-                i++;
-            } else if (subsum < target) {
-                j++; //move right bound
-                subsum += nums[j % n];
-                
-            } else {
-                subsum -= nums[i % n];
-                i++; //move left bound
+        Map<Long, Integer> map = new HashMap<>(); //map sum of prefix - nums[0..i] to i
+        map.put(0L, -1); 
+        int res = Integer.MAX_VALUE;
+        long prefix = 0;
+        for (int i = 0; i < 2 * n; i++) { //expand the range to 2n
+            prefix += (long)nums[i % n];
+            if (map.containsKey(prefix - target)) {
+                res = Math.min(res, i - map.get(prefix - target));
             }
+            map.put(prefix, i);
         }
-        return res == Integer.MAX_VALUE? -1 : res + k * n;
+        return res == Integer.MAX_VALUE? -1 : res + n * k;  
     }
 }
 //Time: O(n); Space: O(1)

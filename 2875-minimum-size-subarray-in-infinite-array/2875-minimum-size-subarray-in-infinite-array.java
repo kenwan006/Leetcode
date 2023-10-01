6adf1efded62056fb1c6abf3d84f4b4b6e054faf
Chao <42952910+kenwan006@users.noreply.github.com>
2023-09-30 22:34:26 -0700

    Time: 5 ms (50.00%), Space: 53 MB (50.00%) - LeetHub

--- 2875-minimum-size-subarray-in-infinite-array/2875-minimum-size-subarray-in-infinite-array.java
+++ 2875-minimum-size-subarray-in-infinite-array/2875-minimum-size-subarray-in-infinite-array.java
@@ -2,11 +2,14 @@ class Solution {
     public int minSizeSubarray(int[] nums, int target) {
         int n = nums.length, sum = 0;
         for (int num : nums) sum += num;
-        int N = (target / sum + 2) * n;
+        int k = target / sum; //repetitions of nums
+        if (target % sum == 0) return k * n;
+        
+        target -= k * sum;
         
         int i = 0, j = 0; //sliding window [i..j]
         int subsum = nums[0], res = Integer.MAX_VALUE;
-        while (j < N) {
+        while (j < 2 * n) {
             if (subsum == target) {
                 res = Math.min(res, j - i + 1);
                 subsum -= nums[i % n]; //move left bound
@@ -20,7 +23,7 @@ class Solution {
                 i++; //move left bound
             }
         }
-        return res == Integer.MAX_VALUE? -1 : res;
+        return res == Integer.MAX_VALUE? -1 : res + k * n;
     }
 }
 //Time: O(n); Space: O(1)

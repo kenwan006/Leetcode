876e1a293d34707bd8b589f260a0c4cb34a9da9a
Chao <42952910+kenwan006@users.noreply.github.com>
2023-10-03 11:50:33 -0700

    Time: 0 ms (100.00%), Space: 40.5 MB (58.04%) - LeetHub

--- 0844-backspace-string-compare/0844-backspace-string-compare.java
+++ 0844-backspace-string-compare/0844-backspace-string-compare.java
@@ -1,39 +1,40 @@
+/** Two pointer **/
 class Solution {
     public boolean backspaceCompare(String s, String t) {
         int m = s.length(), n = t.length();
         int i = m - 1, j = n - 1;
-        int count1 = 0, count2 = 0;
+        int scount = 0, tcount = 0;
+        
         while (i >= 0 || j >= 0) {
-            // if '#', count++; if not '#', then count-- until count == 0
-            while (i >= 0 && s.charAt(i) == '#') {
-                count1++;
+            //count '#' and remove chars for s
+            while (i >= 0) {
+                if (s.charAt(i) == '#') {
+                    scount++;
+                } else {
+                    if (scount > 0) scount--;
+                    else break;
+                }
                 i--;
-                while (i >= 0 && count1 > 0 && s.charAt(i) != '#') {
-                    i--;
-                    count1--;
-                }
             }
-            
-            while (j >= 0 && t.charAt(j) == '#') {
-                count2++;
-                j--;
-                while (j >= 0 && count2 > 0 && t.charAt(j) != '#') {
-                    j--;
-                    count2--;
+            //count '#'' and remove chars for t
+            while (j >= 0) {
+                if (t.charAt(j) == '#') {
+                    tcount++;
+                } else {
+                    if (tcount > 0) tcount--;
+                    else break;
                 }
+                j--;
             }
             
             if (i < 0 && j < 0) return true;
             if (i < 0 || j < 0) return false;
             
-            //when both i and j within the bounds, compare the chars
-            if (s.charAt(i) == t.charAt(j)) {
-                i--;
-                j--;
-            } else {
-                return false;
-            }
+            //when both i and j within range, compare the chars
+            if (s.charAt(i) != t.charAt(j)) return false;
+            i--; j--;
         }
         return true;
     }
 }
+//Time: O(n); Space: O(1)

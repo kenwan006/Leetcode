7ca8369f407a0dbd182f2d26386f1cde4f4d915e
Chao <42952910+kenwan006@users.noreply.github.com>
2023-10-04 11:06:43 -0700

    Time: 1 ms (94.07%), Space: 39.1 MB (70.87%) - LeetHub

--- 0052-n-queens-ii/0052-n-queens-ii.java
+++ 0052-n-queens-ii/0052-n-queens-ii.java
@@ -1,38 +1,38 @@
 class Solution {
-    int N;
-    int ctn = 0;
-    char[][] board;
+    int count = 0;
     public int totalNQueens(int n) {
-        N = n;
-        board = new char[N][N];
-        for (int i = 0; i < N; i++) Arrays.fill(board[i], '.');
-        backtrack(board, 0);
-        return ctn;
+        char[][] board = new char[n][n];
+        for (int i = 0; i < n; i++) Arrays.fill(board[i], '.');
+        backtrack(0, board);
+        return count;
     }
     
-    private void backtrack(char[][] board, int i) {
-        if (i == N) ctn++;
+    private void backtrack(int i, char[][] board) {
+        if (i == board.length) {
+            count++;
+            return;
+        }
         
-        for (int j = 0; j < N; j++) {
-            if (!validate(i, j)) continue;
-            board[i][j] = '#';
-            backtrack(board, i + 1);
-            board[i][j] = '.';
-        }  
+        for (int j = 0; j < board[0].length; j++) {
+            if (validate(i, j, board)) {
+                board[i][j] = 'Q';
+                backtrack(i + 1, board);
+                board[i][j] = '.';
+            }
+        }
     }
     
-    private boolean validate(int row , int col) {
-        //check the left diagonal
-        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
-            if (board[i][j] == '#') return false;
-        }
-        //check the top
+    private boolean validate(int row, int col, char[][] board) {
         for (int i = row - 1; i >= 0; i--) {
-            if (board[i][col] == '#') return false;
+            if (board[i][col] == 'Q') return false;
         }
-        //check the right diagonal
-        for (int i = row - 1, j = col + 1; i >= 0 && j < N; i--, j++) {
-            if (board[i][j] == '#') return false;
+        
+        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
+            if (board[i][j] == 'Q') return false;
+        }
+        
+        for (int i = row - 1, j = col + 1; i >= 0 && j < board[0].length; i--, j++) {
+            if (board[i][j] == 'Q') return false;
         }
         return true;
     }

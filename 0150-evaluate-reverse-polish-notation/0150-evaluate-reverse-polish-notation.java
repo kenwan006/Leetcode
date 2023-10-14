fc89e016d16d41039d620d19503d2bbba797f61f
Chao <42952910+kenwan006@users.noreply.github.com>
2023-10-13 17:51:03 -0700

    Time: 6 ms (79.15%), Space: 43.7 MB (21.60%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
@@ -1,33 +1,33 @@
 class Solution {
     public int evalRPN(String[] tokens) {
-        int res = 0;
-        Stack<Integer> stack = new Stack<>();
         List<String> operators = Arrays.asList("+", "-", "*", "/");
-        
+        Stack<Integer> stack = new Stack<>();
         for (String s : tokens) {
-            if (!operators.contains(s)) stack.push(Integer.parseInt(s));
-            else {
-                int x = stack.pop();
-                int y = stack.pop();
-                int z = 0;
-                switch (s) {
-                    case "+":
-                        z = x + y;
-                        break;
-                    case "-":
-                        z = y - x;
-                        break;
-                    case "*":
-                        z = x * y;
-                        break;
-                    case "/":
-                        z = y / x;
-                        break;
-                }
-                stack.push(z);
+            if (!operators.contains(s)) {
+                stack.push(Integer.parseInt(s));
+                continue;
             }
+            int x = stack.pop();
+            int y = stack.pop();
+            int res = 0;
+            switch (s) {
+                case "+":
+                    res = x + y;
+                    break;
+                case "-":
+                    res = y - x;
+                    break;
+                case "*":
+                    res = x * y;
+                    break;
+                case "/":
+                    res = y / x;
+                    break;
+                default:
+                    break;
+            }
+            stack.push(res);
         }
         return stack.pop();
     }
 }
-//Time: O(n); Space: O(n)

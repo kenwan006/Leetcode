39ec111a046dcf90da2d07cc83ab8c58d9eb5d80
Chao <42952910+kenwan006@users.noreply.github.com>
2023-11-10 14:42:57 -0800

    Time: 182 ms (32.03%), Space: 40.5 MB (63.24%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -2,27 +2,22 @@ class Solution {
     public boolean exist(char[][] board, String word) {
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[0].length; j++) {
-                if (dfs(board, i, j, 0, word)) return true;
+                if (dfs(i, j, 0, board, word)) return true;
             }
         }
         return false;
     }
-    
     int[][] dirs = {{-1, 0}, {1, 0}, {0, 1}, {0, -1}};
-    private boolean dfs(char[][] board, int i, int j, int k, String word) {
+    private boolean dfs(int i, int j, int k, char[][] board, String word) {
         if (k == word.length()) return true;
-        if (i < 0 || j < 0 || i >= board.length || j >= board[0].length) return false;
+        if (i < 0 || i >= board.length || j < 0 || j >= board[0].length || board[i][j]  != word.charAt(k)) return false;
         
         char c = board[i][j];
-        if (c== '#'|| word.charAt(k) != c) return false;
-        
-        board[i][j] = '#'; //mark as visited;
+        board[i][j] = '#'; //mark as visited
         for (int[] dir : dirs) {
-            if (dfs(board, i + dir[0], j + dir[1], k + 1, word)) return true;
+            if (dfs(i + dir[0], j + dir[1], k + 1, board, word)) return true;
         }
-        board[i][j] = c; // remark as unvisited
-        
+        board[i][j] = c;
         return false;
     }
 }
-//Time: O(m * n * 4 ^ L); Space: O()

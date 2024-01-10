220323c40cd5145af677c160ea45306d08af8514
Ken <42952910+kenwan006@users.noreply.github.com>
2024-01-10 12:27:38 -0800

    Time: 1513 ms (5.47%), Space: 55.3 MB (60.19%) - LeetHub

--- 0398-random-pick-index/0398-random-pick-index.java
+++ 0398-random-pick-index/0398-random-pick-index.java
@@ -1,19 +1,28 @@
 class Solution {
-    Map<Integer, Deque<Integer>> map;
-
+    int[] nums;
+    Random random;
+    
     public Solution(int[] nums) {
-        map = new HashMap<>();
-        for (int i = 0; i < nums.length; i++) {
-            map.putIfAbsent(nums[i], new LinkedList<>());
-            map.get(nums[i]).add(i);
-        }
+        this.nums = nums;
+        random = new Random();
     }
     
     public int pick(int target) {
-        //get the index, remove it from front and add to the end for the future use
-        int index = map.get(target).removeFirst();
-        map.get(target).addLast(index);
-        return index;
+        int count = 0; //nums of target.
+        int res = -1;
+        for (int i = 0; i < nums.length; i++) {
+        /** 
+          Randomly select an int from 0 to the count
+          If x equals 0, set the res as the current index. The probability is always 1/count for the latest appeared number.
+          For example, 1 for 1st num, 1/2 for 2nd num, 1/3 for 3nd num (1/2 * 2/3 for each of the first 2 nums). 
+        **/
+            if (nums[i] == target) {
+                count++;
+                int x = random.nextInt(count);
+                res = x == 0? i : res; // the reason is prob for x==0 is always 1/count
+            }
+        }
+        return res;
     }
 }
 

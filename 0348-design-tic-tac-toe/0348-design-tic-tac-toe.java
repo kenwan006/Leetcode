d0a9c267b0c4ef300e864cab6d583534f3bdb2e8
Ken <42952910+kenwan006@users.noreply.github.com>
2024-01-10 15:56:29 -0800

    Time: 3 ms (99.34%), Space: 45.7 MB (11.65%) - LeetHub

--- 0348-design-tic-tac-toe/0348-design-tic-tac-toe.java
+++ 0348-design-tic-tac-toe/0348-design-tic-tac-toe.java
@@ -1,52 +1,31 @@
 class TicTacToe {
-    int[][] mat;
+    int[] rows;
+    int[] cols;
+    int diagonal;
+    int antiDiagonal;
     int n;
-
+    
     public TicTacToe(int n) {
-        mat = new int[n][n]; 
         this.n = n;
+        rows = new int[n]; //rows[i] - means the sum of this ith row
+        cols = new int[n];
     }
     
     public int move(int row, int col, int player) {
-        mat[row][col] = player; //fill the cell
-        if (checkRow(row, col, player) || checkCol(row, col, player) ||
-            (row == col) && checkDiagonal(player) || 
-            (row + col == n-1) && checkAntiDiagonal(player)) {
-            return player;
-        }
+        int toAdd = player == 1? 1 : -1;  //if it's player1, add 1; if it's player2, minus 1
+        int target = player == 1? n : -n; // if it's player1, its target is n; it's -n for the other
+        rows[row] += toAdd;
+        cols[col] += toAdd;
+        if (row == col) diagonal += toAdd;
+        if (row + col == n - 1) antiDiagonal += toAdd;
+        
+        if (rows[row] == target || cols[col] == target || diagonal == target || antiDiagonal == target) return player;
         return 0;
-    }
-    
-    private boolean checkRow(int row, int col, int player) {
-        for (int i = 0; i < n; i++) {
-            if (mat[row][i] != player) return false;
-        }
-        return true;
-    }
-    
-    private boolean checkCol(int row, int col, int player) {
-        for (int i = 0; i < n; i++) {
-            if (mat[i][col] != player) return false;
-        }
-        return true;
-    }
-    
-    private boolean checkDiagonal(int player) {
-        for (int i = 0; i < n; i++) {
-            if (mat[i][i] != player) return false;
-        }
-        return true;
-    }
-    
-    private boolean checkAntiDiagonal(int player) {
-        for (int i = 0; i < n; i++) {
-           if (mat[i][n - 1 - i] != player) return false;
-        }
-        return true;
     }
 }
 
-//Time: O(n); Space: O(n * n)
+//Time: O(1);Space: O(n)
+
 
 /**
  * Your TicTacToe object will be instantiated and called as such:

4bf33fe04cca2d662dc40e9ea9584eb95e48fb54
Ken <42952910+kenwan006@users.noreply.github.com>
2024-01-09 16:28:22 -0800

    Time: 1 ms (100.00%), Space: 42.4 MB (53.18%) - LeetHub

---
+++ 0415-add-strings/0415-add-strings.java
@@ -0,0 +1,20 @@
+class Solution {
+    public String addStrings(String num1, String num2) {
+        int i = num1.length() - 1, j = num2.length() - 1;
+        int carry = 0;
+        StringBuilder sb = new StringBuilder();
+        while (i >= 0 || j >= 0) {
+            int x = i >= 0? num1.charAt(i--) - '0' : 0;
+            int y = j >= 0? num2.charAt(j--) - '0' : 0;
+            int z = x + y + carry;
+            carry = z / 10;
+            z %= 10;
+            sb.append((char)(z + '0'));
+        }
+        
+        if (carry > 0) sb.append((char)(carry + '0'));
+        
+        return sb.reverse().toString();
+    }
+}
+//Time: O(n + m); Space: O(max(m, n));

d3a17626ca8d1d8d28fd3e3239b584ea95e5b973
Ken <42952910+kenwan006@users.noreply.github.com>
2024-01-18 11:16:53 -0800

    Time: 1 ms (99.69%), Space: 45.3 MB (42.73%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.java
+++ 0130-surrounded-regions/0130-surrounded-regions.java
@@ -1,34 +1,40 @@
+/** dfs 
+** Start from the 'O' on the borders, do the dfs traversal and turn all the adjacent 'O' to 'V'
+**  After dfs is done, turn all the remaining 'O's to 'X' because they are captured by 'X'
+** Turn all the 'V' back to 'O' because they are not captured
+**/
 class Solution {
     public void solve(char[][] board) {
         int m = board.length, n = board[0].length;
-        //check the borders
+        //start from top and bottom borders
+        for(int j = 0; j < n; j++) {
+            if (board[0][j] == 'O') dfs(0, j, board);
+            if (board[m - 1][j] == 'O') dfs(m - 1, j, board);
+        }
+        
+        //start from left and right borders
         for (int i = 0; i < m; i++) {
-            if (board[i][0] == 'O') dfs(board, i, 0);
-            if (board[i][n - 1] == 'O') dfs(board, i, n - 1);
+            if (board[i][0] == 'O') dfs(i, 0, board);
+            if (board[i][n - 1] == 'O') dfs(i, n - 1, board);
         }
         
-        for (int j = 0; j < n; j++) {
-            if (board[0][j] == 'O') dfs(board, 0, j);
-            if (board[m - 1][j] == 'O') dfs(board, m - 1, j);
-        }
-        
-        //set unchcked 'O' to 'X' and '#' back to 'O'
+        //turn the remaining 'O' to 'x' and turn 'V' back to 'O'
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if (board[i][j] == 'O') board[i][j] = 'X';
-                if (board[i][j] == '#') board[i][j] = 'O';
+                else if (board[i][j] == 'V') board[i][j] = 'O';
             }
         }
     }
     
-    int[][] dirs = {{-1, 0}, {1, 0}, {0, 1}, {0, -1}};
-    private void dfs(char[][] board, int i, int j) {
-        int m = board.length, n = board[0].length;
-        if (i < 0 || j < 0 || i >= m || j >= n || board[i][j] == 'X' || board[i][j] == '#') return;
-        
-        board[i][j] = '#'; //mark neighboring 'O' as visited
-        for (int[] dir : dirs) dfs(board, i + dir[0], j + dir[1]);
+    int[][] dirs = {{0, 1}, {1, 0}, {-1, 0}, {0, -1}};
+    
+    private void dfs(int i, int j, char[][] board) {
+        if (i < 0 || i >= board.length || j < 0 || j >= board[0].length) return;
+        if (board[i][j] == 'X' || board[i][j] == 'V') return; // X - no need to check; V - already checked
+        board[i][j] = 'V'; //mark the uncaptured 'O' as 'V'
+        for (int[] dir : dirs) {
+            dfs(i + dir[0], j + dir[1], board);
+        }
     }
 }
-//Time O(m * n); Space: O(1)
-

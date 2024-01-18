c62eed35ba5e2e47dad5a578e1365676ba197f7c
Ken <42952910+kenwan006@users.noreply.github.com>
2024-01-18 12:13:44 -0800

    Time: 9 ms (62.72%), Space: 47.9 MB (38.59%) - LeetHub

--- 0286-walls-and-gates/0286-walls-and-gates.java
+++ 0286-walls-and-gates/0286-walls-and-gates.java
@@ -1,31 +1,28 @@
-/** bfs **/
 class Solution {
     public void wallsAndGates(int[][] rooms) {
-        int m  = rooms.length, n = rooms[0].length;
         Queue<int[]> queue = new LinkedList<>();
+        int m = rooms.length, n = rooms[0].length;
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if (rooms[i][j] == 0) queue.offer(new int[]{i, j});
             }
         }
         
-        int[][] dirs = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+        int[][] dirs = {{0, 1}, {1, 0}, {-1, 0}, {0, -1}};
+        int dist = 1;
         while (!queue.isEmpty()) {
             int size = queue.size();
-            for (int k = 0; k < size; k++) {
-                int[] cell = queue.poll();
+            for (int i = 0; i < size; i++) {
+                int[] curr = queue.poll();
                 for (int[] dir : dirs) {
-                    int i = cell[0], j = cell[1];
-                    int ii = cell[0] + dir[0], jj = cell[1] + dir[1];
-                    
-                    if (ii < 0 || jj < 0 || ii >= m || jj >= n || rooms[ii][jj] == -1) continue;
-                    
-                    if (rooms[i][j] + 1 < rooms[ii][jj]) {
-                        rooms[ii][jj] = rooms[i][j] + 1;
-                        queue.offer(new int[]{ii, jj});
-                    }
+                    int r = curr[0] + dir[0];
+                    int c = curr[1] + dir[1];
+                    if (r < 0 || r >= m || c < 0 || c >= n || rooms[r][c] <  Integer.MAX_VALUE) continue;
+                    rooms[r][c] = dist;
+                    queue.offer(new int[]{r, c});
                 }
             }
+            dist++;
         }
     }
 }

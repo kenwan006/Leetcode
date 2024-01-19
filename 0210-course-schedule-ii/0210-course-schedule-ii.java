42bd49ccac266a538cae33a65b6d91dea88d0bd7
Ken <42952910+kenwan006@users.noreply.github.com>
2024-01-18 16:12:19 -0800

    Time: 4 ms (92.68%), Space: 45.9 MB (28.25%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.java
+++ 0210-course-schedule-ii/0210-course-schedule-ii.java
@@ -1,32 +1,30 @@
 class Solution {
     public int[] findOrder(int numCourses, int[][] prerequisites) {
-        int[] res = new int[numCourses];
-        //construct the graph
-        List<Integer>[] graph = new ArrayList[numCourses];
+        List<Integer>[] graph = new List[numCourses];
         int[] indeg = new int[numCourses];
+        
         for (int i = 0; i < numCourses; i++) graph[i] = new ArrayList<>();
         for (int[] e : prerequisites) {
-            int u  = e[1], v = e[0];
-            graph[u].add(v);
-            indeg[v]++;
+            int from = e[1], to = e[0];
+            indeg[to]++;
+            graph[from].add(to);
         }
         
-        //find the vertices with 0 indeg
+        //init queue - vertices with 0 indeg
         Queue<Integer> queue = new LinkedList<>();
         for (int i = 0; i < numCourses; i++) {
             if (indeg[i] == 0) queue.offer(i);
         }
         
-        //toplogical sorting\
-        int ctn = 0;
+        int[] res = new int[numCourses];
+        int idx = 0;
         while (!queue.isEmpty()) {
-            int u = queue.poll();
-            for (int v : graph[u]) {
-                if (--indeg[v] == 0) queue.offer(v);
+            int node = queue.poll(); //take the course 
+            res[idx++] = node;
+            for (int adj : graph[node]) {
+                if (--indeg[adj] == 0) queue.offer(adj);
             }
-            res[ctn++] = u;
         }
-        
-        return ctn == numCourses? res : new int[]{};
+        return idx == numCourses? res : new int[0];
     }
 }

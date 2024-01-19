1f0fbdf55ed4147aa88db61d22a121a7fe0ca845
Ken <42952910+kenwan006@users.noreply.github.com>
2024-01-19 11:22:39 -0800

    Time: 367 ms (32.86%), Space: 62.1 MB (47.32%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.java
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.java
@@ -1,40 +1,48 @@
-/** Prim MST **/
 class Solution {
     public int minCostConnectPoints(int[][] points) {
-        int V = points.length;
-        
-        //build the graph
-        List<List<int[]>> adj = new ArrayList<>();
-        for (int i = 0; i < V; i++) adj.add(new ArrayList<>());
-        for (int i = 0; i < V; i++) {
-            for (int j = i + 1; j < V; j++) {
-                int w = Math.abs(points[i][0] - points[j][0]) + Math.abs(points[i][1] - points[j][1]);
-                adj.get(i).add(new int[]{j, w});
-                adj.get(j).add(new int[]{i, w});
+        int n = points.length;
+        List<int[]> edges = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int dist = Math.abs(points[i][0] - points[j][0]) + Math.abs(points[i][1] - points[j][1]);
+                edges.add(new int[]{i, j, dist});
             }
         }
+        Collections.sort(edges, (o1, o2) -> o1[2] - o2[2]);
         
-        //prim mst
-        int mst = 0;
-        boolean[] inMST = new boolean[V];
-        inMST[0] = true; //pick the vertex 0 as the start
-        PriorityQueue<int[]> pq = new PriorityQueue<>((o1, o2) -> o1[1] - o2[1]);
-        for (int[] e : adj.get(0)) pq.offer(e);
-        
-        while (!pq.isEmpty()) {
-            int[] curr = pq.poll();
-            int v = curr[0], w = curr[1];
-            if (inMST[v]) continue;
-            inMST[v] = true;
-            mst += w;
-            for (int[] e : adj.get(v)) {
-                if (!inMST[e[0]]) {
-                    pq.offer(e);
-                }
-            }
+        UF uf = new UF(n);
+        int res = 0;
+        for (int i = 0; i < edges.size(); i++) {
+            int[] edge = edges.get(i);
+            int u = edge[0], v = edge[1], w = edge[2];
+            if (uf.union(u, v)) continue;
+            res += w;
         }
-        
-        return mst;
+        return res;
+    }
+    
+}
+
+class UF {
+    int n;
+    int[] parent;
+    public UF(int n) {
+        this.n = n;
+        parent = new int[n];
+        for (int i = 0; i < n; i++) parent[i] = i;
+    }
+    
+    public boolean union(int p, int q) {
+        int rootP = find(p), rootQ = find(q);
+        if (rootP == rootQ) return true;
+        parent[rootQ] = rootP;
+        return false;
+    }
+    
+    public int find(int x) {
+        if (parent[x] != x) {
+            parent[x] = find(parent[x]);
+        }
+        return parent[x];
     }
 }
-//Time: O(n * n * log(n)); Space: (n * n)  n - number of points

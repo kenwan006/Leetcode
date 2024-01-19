b823f02e7aeb195ff5bc54d2fa8cdc7ed93c3150
Ken <42952910+kenwan006@users.noreply.github.com>
2024-01-18 17:34:36 -0800

    Time: 0 ms (100.00%), Space: 42.9 MB (73.12%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.java
+++ 0684-redundant-connection/0684-redundant-connection.java
@@ -2,30 +2,36 @@ class Solution {
     public int[] findRedundantConnection(int[][] edges) {
         int n = edges.length;
         UF uf = new UF(n);
-        for (int[] edge : edges) {
-            if (uf.union(edge[0], edge[1])) return edge;
+        for (int[] e : edges) {
+            if (uf.union(e[0], e[1])) return e;
         }
         return null;
     }
     
-    class UF {
-        int[] parent;
-        public UF(int n) {
-            parent = new int[n + 1]; //0, 1, 2..n
-            for (int i = 0; i <= n; i++) parent[i] = i;
+}
+
+class UF {
+    int n;
+    int[] parent;
+    
+    public UF(int n) {
+        this.n = n;
+        parent = new int[n + 1];
+        for (int i = 0; i <= n; i++) parent[i] = i;
+    }
+    
+    
+    public boolean union(int p, int q) {
+        int rootP = find(p), rootQ = find(q);
+        if (rootP == rootQ) return true;
+        parent[rootQ] = rootP;
+        return false;
+    }
+    
+    public int find(int x) {
+        if (parent[x] != x) {
+            parent[x] = find(parent[x]);
         }
-        private boolean union(int p, int q) {
-            int rootP = find(p);
-            int rootQ = find(q);
-            if (rootP == rootQ) return true;
-            parent[rootP] = rootQ;
-            return false;
-        }
-        private int find(int x) {
-            if (parent[x] != x) {
-                parent[x] = find(parent[x]);
-            }
-            return parent[x];
-        }
+        return parent[x];
     }
 }

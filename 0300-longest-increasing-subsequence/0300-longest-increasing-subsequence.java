2bf16f254edad07814f972c9215de678e43da99f
Ken <42952910+kenwan006@users.noreply.github.com>
2024-02-09 11:24:00 -0800

    Time: 39 ms (61.47%), Space: 44.1 MB (52.34%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,26 +1,18 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
-        List<Integer> res = new ArrayList<>(); // monotonic list
-        res.add(nums[0]);
-        for (int i = 1; i < nums.length; i++) {
-            if (nums[i] > res.get(res.size() - 1)) res.add(nums[i]);
-            else {
-                int idx = search(res, nums[i]); //find position and replace with nums[i]
-                res.set(idx, nums[i]);
+        int n = nums.length;
+        int[] dp = new int[n]; //dp[i] - length of LIS ending with nums[i]
+        Arrays.fill(dp, 1);
+        int res = 1;
+        for (int i = 1; i < n; i++) {
+            //find all the nums[j] that is smaller than nums[i]
+            int j = i - 1;
+            while (j >= 0){
+                if (nums[j] < nums[i]) dp[i] = Math.max(dp[i], dp[j] + 1);
+                j--;
             }
+            res = Math.max(res, dp[i]);
         }
-        return res.size();
-    }
-    
-    //find the smallest number in list greater than or equal to the target
-    private int search(List<Integer> list, int target) {
-        int lo = 0, hi = list.size() - 1;
-        while (lo < hi) {
-            int mid = lo + (hi - lo) / 2;
-            if (list.get(mid) < target) lo = mid + 1;
-            else  hi = mid;
-        }
-        return lo;
+        return res;
     }
 }
-//Time: O(n * log(n)); Space: O(n)

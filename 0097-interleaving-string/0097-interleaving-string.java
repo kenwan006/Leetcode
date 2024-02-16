07374f272489af7bb1bc384c0f68784c1b083996
Ken <42952910+kenwan006@users.noreply.github.com>
2024-02-16 11:17:16 -0800

    Time: 6 ms (36.47%), Space: 41.6 MB (73.15%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.java
+++ 0097-interleaving-string/0097-interleaving-string.java
@@ -1,26 +1,24 @@
 class Solution {
     public boolean isInterleave(String s1, String s2, String s3) {
-        int m = s1.length(), n = s2.length(), p = s3.length();
-        if (m + n != p) return false;
+        int m = s1.length(), n = s2.length(), len = s3.length();
+        if (m + n != len) return false;
         
-        boolean[][] dp = new boolean[m + 1][n + 1]; //dp[i][j] - refers to substring s1[0..i) interleavse with subsring s2[0..j)
+        boolean[][] dp = new boolean[m + 1][n + 1];
         dp[0][0] = true;
         
-        //if s1 is empty s1[0..0)
-        for (int j = 1; j <= n; j++) dp[0][j] = s2.charAt(j - 1) == s3.charAt(j - 1) && dp[0][j - 1];
-        //if s2 is empty  s2[0..0)
-        for (int i = 1; i <= m; i++) dp[i][0] = s1.charAt(i - 1) == s3.charAt(i - 1) && dp[i - 1][0];
+        //if s1 is empty
+        for (int j = 1; j <= n; j++) dp[0][j] = s2.charAt(j - 1) == s3.charAt(j - 1)? dp[0][j - 1] : false;
+        //if s2 is empty
+        for (int i = 1; i <= m; i++) dp[i][0] = s1.charAt(i - 1) == s3.charAt(i - 1)? dp[i - 1][0] : false;
         
-        //dp
+        
         for (int i = 1; i <= m; i++) {
             for (int j = 1; j <= n; j++) {
-                char c1 = s1.charAt(i - 1);
-                char c2 = s2.charAt(j - 1);
-                char c3 = s3.charAt(i + j - 1);
-                dp[i][j] = (c1 == c3 && dp[i - 1][j]) || (c2 == c3 && dp[i][j - 1]);
+                char c1 = s1.charAt(i - 1), c2 = s2.charAt(j - 1), c3 = s3.charAt(i + j - 1);
+                if ((c1 == c3 && dp[i - 1][j]) || (c2 == c3 && dp[i][j - 1])) dp[i][j] = true;
             }
         }
+        
         return dp[m][n];
     }
 }
-//Time: O(m * n); Space: O(m * n)

5f6d31f98c74a66b4764a21c6b5f71e8df3f7ecf
Ken <42952910+kenwan006@users.noreply.github.com>
2024-02-16 15:14:45 -0800

    Time: 5 ms (71.44%), Space: 45.2 MB (25.28%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -1,22 +1,24 @@
 class Solution {
     public int minDistance(String word1, String word2) {
         int m = word1.length(), n = word2.length();
-        int[][] dp = new int[m + 1][n + 1]; //dp[i][j] - refers to distance from substring of word1[0..i) to word2[0..j)
+        int[][] dp = new int[m + 1][n + 1]; //dp[i][j] - convert wrod1[0..i) to word2[0..j)
         
-        //word1.substring[0..0) - empty
+        //word1 is empty - just inert
         for (int j = 0; j <= n; j++) dp[0][j] = j;
-        //word2.substring[0, 0) - empty
-        for (int i = 0; i <= m; i++) dp[i][0] = i;
+        //word2 is empty - just delete
+        for (int i = 0; i <= m; i++) dp[i][0] = i; 
         
-        //dp
         for (int i = 1; i <= m; i++) {
             for (int j = 1; j <= n; j++) {
                 if (word1.charAt(i - 1) == word2.charAt(j - 1)) dp[i][j] = dp[i - 1][j - 1];
-                /** insert - dp[i][j-1]; delete - dp[i-1][j]; replace - dp[i-1][j-1] **/
-                else dp[i][j] = Math.min(dp[i][j - 1], Math.min(dp[i - 1][j], dp[i - 1][j - 1])) + 1;
+                else {
+                    int dist1 = dp[i][j - 1];
+                    int dist2 = dp[i - 1][j];
+                    int dist3 = dp[i - 1][j - 1];
+                    dp[i][j] = Math.min(dist1, Math.min(dist2, dist3)) + 1;
+                }
             }
         }
         return dp[m][n];
     }
 }
-//Time: O(m * n); Space: O(m * n)

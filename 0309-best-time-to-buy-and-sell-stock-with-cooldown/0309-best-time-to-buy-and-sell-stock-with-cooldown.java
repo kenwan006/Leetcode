549190945afcf8997f046826252a05866b3d394b
Ken <42952910+kenwan006@users.noreply.github.com>
2024-02-16 16:38:08 -0800

    Time: 0 ms (100.00%), Space: 41.4 MB (87.87%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.java
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.java
@@ -1,24 +1,28 @@
-/** State machine + dp **/
 class Solution {
     public int maxProfit(int[] prices) {
-        int n = prices.length;
-        //state[i] - action happens on ith day and state turns into held[i]
-        int[] held = new int[n];  
-        int[] sold = new int[n]; //max profix if stock sold the end of ith day
-        int[] rest = new int[n]; 
+        /**
+        Here are three states, held, sold, rest
+        held - either last state is already held, or last state is rest and buy a stock now
+        sold - last state must be held
+        rest - either last state is already rest, or last state is sold
+        **/
         
-        //base case 
+        int n = prices.length;
+        int[] held = new int[n];
+        int[] sold = new int[n];
+        int[] rest = new int[n];
+        
+        //base case
         held[0] = -prices[0];
-        sold[0] = -prices[0];
+        sold[0] = 0;
         rest[0] = 0;
         
-        //dp
         for (int i = 1; i < n; i++) {
             held[i] = Math.max(held[i - 1], rest[i - 1] - prices[i]);
-            sold[i] = held[i -1] + prices[i]; // hold stock on (i-1)th day and sell on ith day
-            rest[i] = Math.max(rest[i - 1], sold[i - 1]);
+            sold[i] = held[i - 1] + prices[i];
+            rest[i] = Math.max(rest[i -1], sold[i - 1]);
         }
-        return Math.max(rest[n - 1], sold[n - 1]);
+        
+        return Math.max(sold[n - 1], rest[n - 1]);
     }
 }
-//Time: O(n); Space: O(n)

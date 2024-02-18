4351d3217da6c6f9f5b11ea1fd0087ae8c19a4bc
Ken <42952910+kenwan006@users.noreply.github.com>
2024-02-18 15:44:19 -0800

    Time: 32 ms (66.67%), Space: 56.3 MB (33.33%) - LeetHub

--- 3043-find-the-length-of-the-longest-common-prefix/3043-find-the-length-of-the-longest-common-prefix.java
+++ 3043-find-the-length-of-the-longest-common-prefix/3043-find-the-length-of-the-longest-common-prefix.java
@@ -1,23 +1,41 @@
 class Solution {
     public int longestCommonPrefix(int[] arr1, int[] arr2) {
-        //put all possible prefix of num in arr1 to a set
-        Set<Integer> set = new HashSet<>();
+        //insert all num in the arr1 to Trie
+        TrieNode root = new TrieNode();
         for (int num : arr1) {
-            String str = num + "";
-            for (int i = 1; i <= str.length(); i++) {
-                set.add(Integer.parseInt(str.substring(0, i)));
+            String s = num + "";
+            TrieNode curr = root;
+            for (int i = 0; i < s.length(); i++) {
+                int idx = s.charAt(i) - '0';
+                if (curr.children[idx] == null) curr.children[idx] = new TrieNode();
+                curr = curr.children[idx];
             }
+            curr.isEnd = true;
         }
         
-        //find the prefix in arr2 and check if it's included in the set
+        //for each num in arr2, search it in the already built Trie
         int res = 0;
         for (int num : arr2) {
-            String str = num + "";
-            for (int i = 1; i <= str.length(); i++) {
-                int prefix = Integer.parseInt(str.substring(0, i));
-                if (set.contains(prefix)) res = Math.max(res, i);
+            String s = num + "";
+            TrieNode curr = root;
+            int len = 0;
+            for (int i = 0; i < s.length(); i++) {
+                int idx = s.charAt(i) - '0';
+                if (curr.children[idx] != null) {
+                    curr = curr.children[idx];
+                    len++;
+                } else {
+                    break; //stop the current search
+                }
             }
+            res = Math.max(res, len);
         }
         return res;
+    }
+    
+    class TrieNode{
+        TrieNode[] children = new TrieNode[10];
+        boolean isEnd;
+        public TrieNode(){};
     }
 }

fdf7cc23d75ba0a1b5a966597ff99d41e32580b6
Ken <42952910+kenwan006@users.noreply.github.com>
2024-02-19 12:27:57 -0800

    Time: 115 ms (32.78%), Space: 42.9 MB (24.62%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.java
+++ 0312-burst-balloons/0312-burst-balloons.java
@@ -1,33 +1,27 @@
-/** dp 
-* dp[l][r] - means the max collection for nums[l..r];
-* Iterate the size of the window [lo..hi] from 1 to nums.length
-* Let i be the **last** burst balloon in the nums[lo..hi]  gain = nums[lo-1] * nums[i] * nums[hi+1], remaining = dp[lo][i-1] + dp[i+1][hi]
-* Check each i and find the max one, then dp[lo][hi] = max{gain + remaining}
-* Note: extend the nums to left and right by 1 in order to handle the boundary issue
-*/
 class Solution {
+    int[][] dp;
     public int maxCoins(int[] nums) {
-        //extend to left and right by 1, and copy nums to new nums
-        int n = nums.length + 2; 
-        int[] newNums = new int[n];
-        for (int i = 0; i < nums.length; i++) newNums[i + 1] = nums[i];
-        newNums[0] = 1;
-        newNums[n - 1] = 1;
+        int n = nums.length;
+        dp = new int[n][n];
+        return burst(0, n - 1, nums);
+    }
+    
+    private int burst(int lo, int hi, int[] nums) {
+        if (lo > hi) return 0;
+        if (dp[lo][hi] != 0) return dp[lo][hi]; //already checked
         
-        int[][] dp = new int[n][n]; 
-        
-        for (int len = 1; len <= n - 2; len++) {
-            for (int lo = 1; lo <= n - len - 1; lo++) {
-                int hi = lo + len - 1;
-                for (int i = lo; i <= hi; i++) {
-                    int remaining = dp[lo][i - 1] + dp[i + 1][hi];
-                    //i is the last balloon to burst
-                    int gain = newNums[lo - 1] * newNums[i] * newNums[hi + 1];
-                    dp[lo][hi] = Math.max(dp[lo][hi], gain + remaining);
-                }
-            }
+        int coins = 0;
+        for (int i = lo; i <= hi; i++) { //i - the last balloon to burst
+            int gain = nums[i];
+            if (lo - 1 >= 0) gain *= nums[lo - 1];
+            if (hi + 1 < nums.length) gain *= nums[hi + 1];
+            
+            gain += burst(lo, i - 1, nums) + burst(i + 1, hi, nums);
+            
+            coins = Math.max(coins, gain);
         }
-        return dp[1][n -2];
+        
+        dp[lo][hi] = coins;
+        return coins;
     }
 }
-//Time: O(n ^ 3); Space: O(n ^ 2)

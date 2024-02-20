0a6c8e9709d82dbfb245d44d7a3cbad56a8971da
Ken <42952910+kenwan006@users.noreply.github.com>
2024-02-20 15:27:22 -0800

    Time: 2 ms (99.52%), Space: 56.7 MB (79.82%) - LeetHub

--- 0134-gas-station/0134-gas-station.java
+++ 0134-gas-station/0134-gas-station.java
@@ -1,23 +1,19 @@
 class Solution {
     public int canCompleteCircuit(int[] gas, int[] cost) {
-        int n = gas.length, sum = 0;
-        // lefover of each station, and sum them up
-        for (int i = 0; i < n; i++) {
-            gas[i] -= cost[i];
-            sum += gas[i];
-        }
+        int sum = 0, n = gas.length;
+        for (int i = 0; i < n; i++) sum += (gas[i] - cost[i]); // check if the sum of all gas > sum of all cost
         if (sum < 0) return -1;
         
-        //find the start station - that is guaranteed to be unique
-        int carry = 0, start = 0;
-        for (int i = 0; i < n; i++) {
-            carry += gas[i];
+        //if total cost >= gas, we can always find a valid starting to travel around all stations
+        // it can be proved intuitively
+        int target = 0, carry = 0;
+        for (int i = 0; i < n - 1; i++) {
+            carry += gas[i] - cost[i];
             if (carry < 0) {
+                target = i + 1;
                 carry = 0;
-                start = i + 1;
             }
         }
-        return start;
+        return target;
     }
 }
-//Time: O(n); Space: O(1)

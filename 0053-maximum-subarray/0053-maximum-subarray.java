f13c99d784f62040dc9d128cef29065aedf3ba1c
Ken <42952910+kenwan006@users.noreply.github.com>
2024-02-19 18:12:35 -0800

    Time: 10 ms (5.50%), Space: 57.3 MB (27.25%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,11 +1,32 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-        int sum = 0, maxSum = Integer.MIN_VALUE;
-        for (int num : nums) {
-            if (sum < 0) sum = 0;
-            sum += num;
-            maxSum = Math.max(maxSum, sum);
+        return maxSubArray(0, nums.length - 1, nums);
+    }
+    
+    private int maxSubArray(int lo, int hi, int[] nums) {
+        if (hi < lo) return Integer.MIN_VALUE;
+        
+        int mid = lo + (hi - lo) / 2;
+        
+        int lsum = 0; // max sum of subarray ending with mid-1
+        int sum = 0;
+        for (int i = mid - 1; i >= lo; i--) {
+            sum += nums[i];
+            lsum = Math.max(lsum, sum);
         }
-        return maxSum;
+        
+        int rsum = 0; // max sum of subarray starting with mid+1
+        sum = 0;
+        for (int i = mid + 1; i <= hi; i++) {
+            sum += nums[i];
+            rsum = Math.max(rsum, sum);
+        }
+        
+        sum = lsum + nums[mid] + rsum; //max sum of subarray containing nums[mid]
+        
+        int lsubarray = maxSubArray(lo, mid - 1, nums); 
+        int rsubarray = maxSubArray(mid + 1, hi, nums);
+        
+        return Math.max(sum, Math.max(lsubarray, rsubarray));
     }
 }

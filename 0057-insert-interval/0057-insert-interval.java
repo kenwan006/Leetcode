b6bef082f9c98118294cb1449c7c60d3682eaddf
Ken <42952910+kenwan006@users.noreply.github.com>
2024-02-21 12:33:34 -0800

    Time: 1 ms (98.94%), Space: 44.9 MB (31.22%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.java
+++ 0057-insert-interval/0057-insert-interval.java
@@ -1,19 +1,23 @@
 class Solution {
     public int[][] insert(int[][] intervals, int[] newInterval) {
-        int i = 0, n = intervals.length, start = newInterval[0], end = newInterval[1];
         List<int[]> list = new ArrayList<>();
-        while (i < n && intervals[i][1] < start) list.add(intervals[i++]);
-        while (i < n && intervals[i][0] <= end) {
-            start = Math.min(start, intervals[i][0]);
-            end = Math.max(end, intervals[i][1]);
-            i++;
+        int start = newInterval[0], end = newInterval[1];
+        for (int[] interval : intervals) {
+            if (interval[0] > end) {
+                list.add(new int[]{start, end});
+                start = interval[0];
+                end = interval[1];
+            } else if (interval[1] < start) {
+                list.add(interval);
+            } else {
+                start = Math.min(start, interval[0]);
+                end = Math.max(end, interval[1]);
+            }
         }
         list.add(new int[]{start, end});
-        while (i < n && intervals[i][0] > end) list.add(intervals[i++]);
         
-        int[][] res = new int[list.size()][2];
-        for (i = 0; i < res.length; i++) res[i] = list.get(i);
+        //covnert list to array
+        int[][] res = list.toArray(new int[list.size()][2]);
         return res;
     }
 }
-//Time: O(n); Space: O(1)

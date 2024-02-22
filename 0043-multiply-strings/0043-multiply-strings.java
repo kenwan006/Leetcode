cd40dcea9488a50f79fc1e94c98ad43ac73b1724
Ken <42952910+kenwan006@users.noreply.github.com>
2024-02-22 15:42:43 -0800

    Time: 7 ms (45.54%), Space: 43.3 MB (21.95%) - LeetHub

--- 0043-multiply-strings/0043-multiply-strings.java
+++ 0043-multiply-strings/0043-multiply-strings.java
@@ -1,20 +1,15 @@
-/** Math
-* product[i + j + 1] = num1[i] * num2[j];
-*/
 class Solution {
     public String multiply(String num1, String num2) {
         int m = num1.length(), n = num2.length();
-        int[] product = new int[m + n];
+        int[] product = new int[m + n]; // res[k] - sum of all possible num1[i] * num2[j] that i + j - 1 = k;
         
-        for (int i = m - 1; i >= 0; i--) {
-            for (int j = n - 1; j >= 0; j--) {
-                int a = num1.charAt(i) - '0';
-                int b = num2.charAt(j) - '0';
-                product[i + j + 1] += a * b;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j  < n; j++) {
+                product[i + j + 1] += (num1.charAt(i) - '0') * (num2.charAt(j) - '0');
             }
         }
         
-        //move carry to higher position
+        //if the lower product is greater than 10, only keep the digit at ones place
         int carry = 0;
         for (int i = m + n - 1; i >= 0; i--) {
             int d = product[i] + carry;
@@ -22,12 +17,11 @@ class Solution {
             carry = d / 10;
         }
         
-        //remove the leading 0s
+        //remove the leading zeros
         int i = 0;
-        while (i < product.length && product[i] == 0) i++;
-        StringBuilder sb = new StringBuilder();
-        while (i < product.length) sb.append(product[i++]);
-        return sb.isEmpty()? "0" : sb.toString();
+        while (i < m + n && product[i] == 0) i++;
+        String s = "";
+        for (; i < m + n; i++) s += product[i];
+        return s.isEmpty()? "0" : s;
     }
 }
-//Time: O(m + n); Space: O(m + n)

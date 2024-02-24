6550c8750b0701f1fb360f596976246306121a03
Ken <42952910+kenwan006@users.noreply.github.com>
2024-02-23 17:19:38 -0800

    Time: 0 ms (100.00%), Space: 41.8 MB (49.91%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.java
+++ 0190-reverse-bits/0190-reverse-bits.java
@@ -1,14 +1,12 @@
 public class Solution {
     // you need treat n as an unsigned value
     public int reverseBits(int n) {
-        int[] bits = new int[32];
-        for (int i = 0; i < 32; i++) {
-            bits[i] = n & 1;
-            n >>>= 1;
-        }
-        
         int res = 0;
-        for (int bit : bits) res = res * 2 + bit;
+        for (int i = 0; i < 32; i++) {
+            int bit = n & 1;  //bit at least significant position of n
+            n >>>= 1;
+            res = res * 2 + bit;
+        }
         return res;
     }
 }

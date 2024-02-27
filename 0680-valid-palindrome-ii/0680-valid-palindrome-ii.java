502c9194bdecc45338a4813365fede8e6543d5d0
Ken <42952910+kenwan006@users.noreply.github.com>
2024-02-27 11:57:36 -0800

    Time: 4 ms (99.71%), Space: 45.3 MB (65.28%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
@@ -1,14 +1,12 @@
 class Solution {
     public boolean validPalindrome(String s) {
-        int m = s.length();
-        int i = 0, j = m - 1;
+        int i = 0, j = s.length() - 1;
         while (i < j) {
             if (s.charAt(i) == s.charAt(j)) {
                 i++;
                 j--;
             } else {
-                if (isPalindrome(s, i, j - 1) || isPalindrome(s, i + 1, j)) return true;
-                else return false;
+                return isPalindrome(s, i, j - 1) || isPalindrome(s, i + 1, j);
             }
         }
         return true;

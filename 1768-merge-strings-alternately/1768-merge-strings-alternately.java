175960e411cd52503d2e0d08ff0e89be821d8501
Ken <42952910+kenwan006@users.noreply.github.com>
2024-03-29 15:33:22 -0700

    Time: 7 ms (20.35%), Space: 42.8 MB (10.17%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.java
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.java
@@ -3,12 +3,11 @@ class Solution {
         String s = "";
         int m = word1.length(), n = word2.length();
         int i = 0, j = 0;
-        while (i < m && j < n) {
-            s += word1.charAt(i++);
-            s += word2.charAt(j++);
+        while (i < m || j < n) {
+            if (i < m) s += word1.charAt(i++);
+            if (j < n) s += word2.charAt(j++);
         }
         
-        s += i < m? word1.substring(i, m) : word2.substring(j, n);
         return s;
     }
 }

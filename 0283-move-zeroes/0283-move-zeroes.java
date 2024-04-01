323d4ef7964c2c2b57cc90c0a0c1f68f49ab3927
Ken <42952910+kenwan006@users.noreply.github.com>
2024-04-01 11:49:21 -0700

    Time: 2 ms (84.75%), Space: 45.9 MB (72.94%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,11 +1,11 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int j = 0; // j points to the next non-zero num
+        int j = 0; // the next spot to store non zero element
         for (int i = 0; i < nums.length; i++) {
-            if (nums[i] != 0) nums[j++] = nums[i];
+            if (nums[i] == 0) continue;
+            nums[j++] = nums[i];
         }
         
-        //set the remaining to 0s
-        for (; j < nums.length; j++) nums[j] = 0;
+        for (; j < nums.length; j++) nums[j] = 0; 
     }
 }

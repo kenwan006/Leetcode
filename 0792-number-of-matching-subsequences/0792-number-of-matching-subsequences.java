f5d1b50994f26439ef485a8a93efa9533adb6a1c
Ken <42952910+kenwan006@users.noreply.github.com>
2024-04-02 10:41:24 -0700

    Time: 54 ms (77.73%), Space: 46.8 MB (48.32%) - LeetHub

--- 0792-number-of-matching-subsequences/0792-number-of-matching-subsequences.java
+++ 0792-number-of-matching-subsequences/0792-number-of-matching-subsequences.java
@@ -1,24 +1,34 @@
 class Solution {
     public int numMatchingSubseq(String s, String[] words) {
         int count = 0;
-        Map<String, Integer> map = new HashMap<>();
-        for (String word : words) map.put(word, map.getOrDefault(word, 0) + 1);
-
-        for (String word : map.keySet()) {
-            if (match(s, word)) count+= map.get(word);
+        
+        //each pair{i, j} in the list - jth letter of ith word
+        List<int[]> [] waiting = new List[26]; 
+        for (int i = 0; i < 26; i++) waiting[i] = new ArrayList<>();
+        
+        //group by their 0th letter
+        for (int i = 0; i < words.length; i++) {
+            int idx = words[i].charAt(0) - 'a';
+            waiting[idx].add(new int[]{i, 0});
+        }
+        
+        //check each letter of s and see which group to advance
+        for (char c : s.toCharArray()) {
+            int idx = c - 'a';
+            List<int[]> advance = waiting[idx]; //copy the idx waiting group to advance 
+            waiting[idx] = new ArrayList<>();  //and reset this waiting group for futher potential insertion
+             
+            for (int[] arr : advance) {
+                int i = arr[0], j = arr[1];
+                j++; //advance jth letter of ith word to j+1
+                if (j == words[i].length()) {
+                    count++;
+                }else {
+                    idx = words[i].charAt(j) - 'a';
+                    waiting[idx].add(new int[]{i, j});
+                }
+            }
         }
         return count;
-    }
-    
-    //check if string t is subsequence of string s
-    private boolean match(String s, String t) {
-        int m = t.length(), n = s.length();
-        if (m > n) return false;
-        int j = 0;
-        for (int i = 0; i < n; i++) {
-            if (s.charAt(i) == t.charAt(j)) j++;
-            if (j == m) return true;
-        }
-        return false;
     }
 }

ef1b5753e2bea98acf353986642fe8bf236c53a3
Ken <42952910+kenwan006@users.noreply.github.com>
2024-04-07 18:47:18 -0700

    Time: 1 ms (99.69%), Space: 41.9 MB (10.40%) - LeetHub

--- 0291-word-pattern-ii/0291-word-pattern-ii.java
+++ 0291-word-pattern-ii/0291-word-pattern-ii.java
@@ -1,30 +1,35 @@
-/** Backtracking 
-* Vertically, check each char of pattern 
-* Horizontally, check each substring of s, which is s[sPos, i], i range (sPos, s.length())
-*/
 class Solution {
+    Map<Character, String> map = new HashMap<>();
+    Set<String> set = new HashSet<>();
     public boolean wordPatternMatch(String pattern, String s) {
-        Map<Character, String> map = new HashMap<>();
-        return isMatch(pattern, 0, s, 0, map);
+        map = new HashMap<>();
+        set = new HashSet<>();
+        return isMatched(pattern, 0, s, 0);
     }
     
-    public boolean isMatch(String p, int pPos, String s, int sPos, Map<Character, String> map) {
+    private boolean isMatched(String p, int pPos, String s, int sPos) {
         if (pPos == p.length() && sPos == s.length()) return true;
         if (pPos == p.length() || sPos == s.length()) return false;
-    
+        
         char c = p.charAt(pPos);
+        
+        //c already checked
+        if (map.containsKey(c)) {
+            String substr = map.get(c);
+            //check if s[sPos..] starts with substr
+            if (!s.substring(sPos).startsWith(substr)) return false;
+            return isMatched(p, pPos + 1, s, sPos + substr.length());
+        }
+        
+        //c not been checked
         for (int i = sPos; i < s.length(); i++) {
             String substr = s.substring(sPos, i + 1);
-            
-            if (map.containsKey(c) && map.get(c).equals(substr)) {
-                return isMatch(p, pPos + 1, s, i + 1, map);
-            }
-            
-            if (!map.containsKey(c) && !map.containsValue(substr)) { //ensure this substring not been mapped by another char in pattern
-                map.put(c, substr);
-                if (isMatch(p, pPos + 1, s, i + 1, map)) return true;
-                map.remove(c);
-            }
+            if (set.contains(substr)) continue; //skip if this substr has already been mapped by another char in pattern
+            set.add(substr);
+            map.put(c, substr);
+            if (isMatched(p, pPos + 1, s, i + 1)) return true;
+            set.remove(substr);
+            map.remove(c);
         }
         return false;
     }

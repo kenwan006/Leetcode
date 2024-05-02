cecd3e215d1f229ccc8aa24e7f89c37d24052f36
Ken <42952910+kenwan006@users.noreply.github.com>
2024-05-02 14:37:35 -0700

    Time: 8 ms (67.84%), Space: 45.5 MB (90.74%) - LeetHub

--- 0253-meeting-rooms-ii/0253-meeting-rooms-ii.java
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.java
@@ -1,16 +1,22 @@
 class Solution {
     public int minMeetingRooms(int[][] intervals) {
+        //sort the intervals by the start time in ascending order
         Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
         
-        //use a min heap to store the ending time of each conference room
+        //add the end time of each room to a min heap
         PriorityQueue<Integer> heap = new PriorityQueue<>();
         heap.offer(intervals[0][1]);
         
         for (int i = 1; i < intervals.length; i++) {
-            int start = intervals[i][0], end = intervals[i][1];
-            if (start >= heap.peek()) heap.poll();
-            heap.offer(end);
+            int[] curr = intervals[i];
+            int start = curr[0], end = curr[1];
+            
+            if (start >= heap.peek()) heap.poll(); //update end time for a room
+            
+            heap.offer(curr[1]);
         }
+        
         return heap.size();
+        
     }
 }

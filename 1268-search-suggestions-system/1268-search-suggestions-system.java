843848edb5d91314b3098c1b26972cc7d314146d
Ken <42952910+kenwan006@users.noreply.github.com>
2024-05-02 23:14:03 -0700

    Time: 16 ms (79.62%), Space: 50 MB (25.12%) - LeetHub

---
+++ 1268-search-suggestions-system/1268-search-suggestions-system.java
@@ -0,0 +1,37 @@
+class Solution {
+    public List<List<String>> suggestedProducts(String[] products, String searchWord) {
+        Arrays.sort(products);
+        
+        //build trie
+        Trie root = new Trie();
+        for (String p : products) {
+            Trie curr = root;
+            for (char c : p.toCharArray()) {
+                if (curr.children[c - 'a'] == null) curr.children[c - 'a'] = new Trie();
+                curr = curr.children[c - 'a'];
+                if (curr.list.size() < 3) curr.list.add(p); //at each node we store at most 3 product words
+            }
+        }
+        
+        //search each prefix of searchWord on the trie
+        List<List<String>> res = new ArrayList<>();
+        Trie curr = root;
+        for (char c : searchWord.toCharArray()) {
+            if (curr == null) {
+                res.add(new ArrayList<>());
+            } else {
+                curr = curr.children[c - 'a'];
+                if (curr != null) res.add(curr.list);
+                else res.add(new ArrayList<>());
+            }
+        }
+        
+        return res;
+    }
+    
+    class Trie {
+        List<String> list = new ArrayList<>();
+        Trie[] children = new Trie[26];
+        public Trie(){}
+    }
+}

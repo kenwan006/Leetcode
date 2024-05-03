51c329c73f089233a4492a23ce6fa94a7c54be01
Ken <42952910+kenwan006@users.noreply.github.com>
2024-05-03 14:48:18 -0700

    Time: 2 ms (72.51%), Space: 42 MB (87.60%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -1,37 +1,35 @@
 class Solution {
     public int orangesRotting(int[][] grid) {
-        int m = grid.length, n = grid[0].length;
         Queue<int[]> queue = new LinkedList<>();
-        int step = -1; 
-        int count = 0; //count of fresh orange
-        //put all the rotton orangs to a queue
+        int m = grid.length, n = grid[0].length;
+        int fresh = 0; // count of fresh orange
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                if (grid[i][j] == 2) queue.add(new int[]{i, j});
-                if (grid[i][j] == 1) count++;
+                if (grid[i][j] == 2) queue.offer(new int[]{i, j});
+                if (grid[i][j] == 1) fresh++;
             }
         }
         
-        if (count == 0) return 0; //no fresh orange left in the beginning
-        
-        int[][] dirs = {{-1, 0}, {0, -1}, {0, 1}, {1, 0}};
-        
-        //start rotting the fresh oranges
-        while (!queue.isEmpty()) {
+        //start rotting
+        int time = 0;
+        int[][] dirs = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+        while (!queue.isEmpty() && fresh > 0) {
             int size = queue.size();
-            for (int i = 0; i < size; i++) {
+            for (int k = 0; k < size; k++) {
                 int[] curr = queue.poll();
+                int i = curr[0], j = curr[1];
                 for (int[] dir : dirs) {
-                    int r = curr[0] + dir[0], c = curr[1] + dir[1];
-                    if (r < 0 || r >= m || c < 0 || c >= n || grid[r][c] == 0 || grid[r][c] == 2) continue;
-                    grid[r][c] = 2; //rot the fresh orange
-                    count--;
-                    queue.offer(new int[]{r, c});
+                    int ii = i +  dir[0], jj = j + dir[1];
+                    if (ii < 0 || jj < 0 || ii >= m || jj >= n || grid[ii][jj] == 0 || grid[ii][jj] == 2) continue;
+                    grid[ii][jj] = 2; //fresh orange rot
+                    fresh--;
+                    queue.offer(new int[]{ii, jj});
                 }
             }
-            step++; // the final loop should not be counted
+            time++;
         }
-        
-        return count == 0? step : -1;
+        return fresh == 0? time : -1;
     }
 }
+
+//Time: O(m * n); Space: O(m * n)

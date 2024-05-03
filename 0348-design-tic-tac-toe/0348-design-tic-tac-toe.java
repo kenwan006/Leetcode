5ba0a299c2dc703bb37bf6cb2da5fff8ed7bfdef
Ken <42952910+kenwan006@users.noreply.github.com>
2024-05-03 11:15:36 -0700

    Time: 3 ms (99.85%), Space: 45.1 MB (93.88%) - LeetHub

--- 0348-design-tic-tac-toe/0348-design-tic-tac-toe.java
+++ 0348-design-tic-tac-toe/0348-design-tic-tac-toe.java
@@ -1,46 +1,27 @@
 class TicTacToe {
-    int[][] board;
+    int[] rows;
+    int[] cols;
+    int diagonal;
+    int antiDiagonal;
+    int n;
 
     public TicTacToe(int n) {
-        board = new int[n][n];
+        this.n = n;
+        rows = new int[n];
+        cols = new int[n];
     }
     
     public int move(int row, int col, int player) {
-        board[row][col] = player; //occupy the position
+        int target = player == 1? n : -n;
+        int toAdd = player == 1? 1 : -1;
+        rows[row] += toAdd;
+        cols[col] += toAdd;
+        if (row == col) diagonal += toAdd;
+        if (row + col == n - 1) antiDiagonal += toAdd;
         
-        if (validCol(row, col, player) || validRow(row, col, player) || validDiagonal(row, col, player) || validAntiDiagonal(row, col, player)) return player;
+        if (rows[row] == target || cols[col] == target || diagonal == target || antiDiagonal == target) return player;
+        
         return 0;
-    }
-    
-    private boolean validRow(int row, int col, int player) {
-        for (int j = 0; j < board.length; j++) {
-            if (board[row][j] != player) return false;
-        }
-        return true;
-    }
-    
-    private boolean validCol(int row, int col, int player) {
-        for (int i = 0; i < board.length; i++) {
-            if (board[i][col] != player) return false;
-        }
-        return true;
-    }
-    
-    private boolean validDiagonal(int row, int col, int player) {
-        if (row != col) return false;
-        for (int i = 0; i < board.length; i++) {
-            if (board[i][i] != player) return false;
-        }
-        return true;
-    }
-    
-    private boolean validAntiDiagonal(int row, int col, int player) {
-        int n = board.length;
-        if (row + col != n - 1) return false;
-        for (int i = 0; i < n; i++) {
-            if (board[i][n - 1 - i] != player) return false;
-        }
-        return true;
     }
 }
 

16fd5e970bee28c3d33932177adb0371465757da
Ken <42952910+kenwan006@users.noreply.github.com>
2024-05-03 11:47:14 -0700

    Time: 5 ms (68.21%), Space: 44.4 MB (69.50%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,30 +1,40 @@
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        List<Integer> [] graph = new List[numCourses];
+        List<Integer>[] graph = new List[numCourses];
+        for (int i = 0; i < numCourses; i++) graph[i] = new ArrayList<>();
+        
         int[] indeg = new int[numCourses];
         
-        for (int i = 0; i < numCourses; i++) graph[i] = new ArrayList<>();
+        //build the graph and indeg
         for (int[] e : prerequisites) {
-            int from = e[1], to = e[0];
-            indeg[to]++;
+            int from  = e[1], to = e[0];
             graph[from].add(to);
+            graph[to].add(from);
+            
+            indeg[to]++;
         }
         
-        //init queue - all vertices with indeg 0
+        //find the node with 0 indeg
         Queue<Integer> queue = new LinkedList<>();
         for (int i = 0; i < numCourses; i++) {
             if (indeg[i] == 0) queue.offer(i);
         }
         
-        int count = 0;
-        while(!queue.isEmpty()) {
-            int i = queue.poll(); //take course i
-            for (int adj : graph[i]) {
+        //topological sorting
+        int count = 0; // count the courses selected
+        while (!queue.isEmpty()) {
+            int src = queue.poll();
+            count++; 
+            
+            //update indeg for adjacents of src if we remove the src
+            for (int adj : graph[src]) {
                 if (--indeg[adj] == 0) queue.offer(adj);
             }
-            count++;
+            
         }
         
         return count == numCourses;
     }
 }
+
+//Time: O(v + e); Space: O(v + e)

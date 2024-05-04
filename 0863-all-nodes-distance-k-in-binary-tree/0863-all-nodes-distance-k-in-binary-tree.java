985f29020b4503ad3f3a73833c40c238cf89839a
Ken <42952910+kenwan006@users.noreply.github.com>
2024-05-04 14:28:56 -0700

    Time: 14 ms (45.21%), Space: 42.9 MB (23.04%) - LeetHub

---
+++ 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.java
@@ -0,0 +1,57 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<Integer> distanceK(TreeNode root, TreeNode target, int k) {
+        //build graph
+        Map<Integer, List<Integer>> graph = new HashMap<>();
+        build(root, null, graph);
+        
+        //bfs - find the node with distance k from the target node
+        LinkedList<Integer> queue = new LinkedList<>();
+        Set<Integer> visited = new HashSet<>();
+        queue.offer(target.val);
+        visited.add(target.val);
+        
+        while (!queue.isEmpty() && k > 0) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                int curr = queue.poll();
+                for (int next : graph.get(curr)) {
+                    if (visited.contains(next)) continue;
+                    visited.add(next);
+                    queue.offer(next);
+                }
+            }
+            k--;
+        }
+        return queue;
+    }
+    
+    //build a undirected graph
+    private void build(TreeNode root, TreeNode parent, Map<Integer, List<Integer>> graph) {
+        if (root == null) return;
+        graph.put(root.val, new ArrayList<>());
+        
+        if (parent != null) {
+            graph.get(root.val).add(parent.val);
+        }
+        
+        if (root.left != null) {
+            graph.get(root.val).add(root.left.val);
+            build(root.left, root, graph);
+        }
+        if (root.right != null) {
+            graph.get(root.val).add(root.right.val);
+            build(root.right, root, graph);
+        }
+    }
+}
+
+//Time: O(v); Space: O(v + e)

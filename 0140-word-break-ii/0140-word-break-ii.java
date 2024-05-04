f74b28ae4cbd5ae76ffd236de2228441e07e44e5
Ken <42952910+kenwan006@users.noreply.github.com>
2024-05-03 18:27:48 -0700

    Time: 4 ms (53.44%), Space: 42 MB (19.00%) - LeetHub

--- 0140-word-break-ii/0140-word-break-ii.java
+++ 0140-word-break-ii/0140-word-break-ii.java
@@ -1,22 +1,23 @@
 class Solution {
-    
     public List<String> wordBreak(String s, List<String> wordDict) {
-        Map<String, List<String>> map = new HashMap<>();
-        return wordBreak(s, wordDict, map);
+        return wordBreak(s, 0, new HashSet<String>(wordDict), new HashMap<String, List<String>>());
     }
     
-    private List<String> wordBreak(String s, List<String> wordDict, Map<String, List<String>> map) {
-        if (map.containsKey(s)) return map.get(s);
+    private List<String> wordBreak(String s, int i, HashSet<String> wordDict, HashMap<String, List<String>> map) {
+        if (map.containsKey(s.substring(i))) return map.get(s.substring(i));
         
         List<String> res = new ArrayList<>();
-        if (s == "") {
+        
+        if (i == s.length()) {
             res.add("");
             return res;
         }
         
-        for (String word : wordDict) {
-            if (s.startsWith(word)) {
-                List<String> sublist = wordBreak(s.substring(word.length()), wordDict, map);
+        
+        for (int j = i + 1; j <= s.length(); j++) {
+            String word = s.substring(i, j);
+            if (wordDict.contains(word)) {
+                List<String> sublist = wordBreak(s, j, wordDict, map);
                 for (String sub : sublist) {
                     if (sub == "") res.add(word);
                     else res.add(word + " " + sub);

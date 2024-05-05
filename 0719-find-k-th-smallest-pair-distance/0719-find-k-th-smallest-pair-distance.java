05d54c46803bc94c3e9f06924b0683c6c49fa8ff
Ken <42952910+kenwan006@users.noreply.github.com>
2024-05-05 15:16:18 -0700

    Time: 22 ms (16.11%), Space: 44.7 MB (17.88%) - LeetHub

--- 0719-find-k-th-smallest-pair-distance/0719-find-k-th-smallest-pair-distance.java
+++ 0719-find-k-th-smallest-pair-distance/0719-find-k-th-smallest-pair-distance.java
@@ -5,13 +5,8 @@ class Solution {
         * count the pairs whoes distances <= mid, and compare the count with k
         */
         Arrays.sort(nums);
-        
-        int minDist = 1000000, n = nums.length;
-        for (int i = 1; i < n; i++) {
-            minDist = Math.min(minDist, nums[i] - nums[i - 1]);
-        }
-        
-        int lo = minDist, hi = nums[n - 1] - nums[0];
+        int n = nums.length;
+        int lo = 0, hi = nums[n - 1] - nums[0];
         while (lo < hi) {
             int mid = lo + (hi - lo) / 2;
             int ctn = count(nums, mid);
@@ -25,14 +20,29 @@ class Solution {
     }
     
     //count pair(num1, nums), whose distance <= target
-    private int count(int[] nums, int target) {
+    private int count(int[] nums, int dist) {
+        /**
+        * say we have a pair - {nums[i], nums[j]}, find the upperbound of j such that
+        * each nums[j] - nums[i] <= dist
+        */
         int ctn = 0;
         for (int i = 0; i < nums.length; i++) {
-            int j = i + 1;
-            while (j < nums.length && nums[j++] - nums[i] <= target) ctn++;
+            int upperBound = firstGreaterThan(nums, nums[i] + dist);
+            ctn += upperBound - i - 1;
         }
         return ctn;
     }
+    
+    //find the first num > target
+    private int firstGreaterThan(int[] nums, int target) {
+        int lo = 0, hi = nums.length; //don't use nums.length - 1
+        while (lo < hi) {
+            int mid = lo + (hi - lo) / 2;
+            if (nums[mid] <= target) lo = mid + 1;
+            else hi = mid;
+        }
+        return lo;
+    }
 }
 
-//Time: O(n * n * log(m)); Space: O(1)
+//Time: O(nlog(n) + n * log(m) * log(m)); Space: O(1)

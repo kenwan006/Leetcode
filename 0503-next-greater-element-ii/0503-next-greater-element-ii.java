40f01e4d8307a1af364e4ba6fb61ba0b8889c03a
Ken <42952910+kenwan006@users.noreply.github.com>
2024-05-21 11:18:10 -0700

    Time: 91 ms (5.91%), Space: 45.5 MB (97.06%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.java
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.java
@@ -1,27 +1,16 @@
-/** Monotonic stack **/
 class Solution {
     public int[] nextGreaterElements(int[] nums) {
         int n = nums.length;
         int[] res = new int[n];
-        Arrays.fill(res, -1);
-        
-        Stack<Integer> stack = new Stack<>();
         for (int i = 0; i < n; i++) {
-            while (!stack.isEmpty() && nums[i] > nums[stack.peek()]) {
-                res[stack.pop()] = nums[i];
+            res[i] = -1;
+            for (int j = i + 1; j < n * 2; j++) {
+                if (nums[j % n] > nums[i]) {
+                    res[i] = nums[j % n];
+                    break;
+                }
             }
-            stack.push(i);
         }
-        
-        // The stack may not be empty so far. Need to iterate over the nums again
-        for (int i = 0; i < n; i++) {
-            while (!stack.isEmpty() && nums[i] > nums[stack.peek()]) {
-                res[stack.pop()] = nums[i];
-            }
-            stack.push(i);
-        }
-        
         return res;
     }
 }
-//Time: O(n); Space: O(n)

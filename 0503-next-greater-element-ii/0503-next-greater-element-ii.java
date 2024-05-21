cf4f70472a394653c7bfc2af9271f26e927dc28a
Ken <42952910+kenwan006@users.noreply.github.com>
2024-05-21 11:20:10 -0700

    Time: 90 ms (5.91%), Space: 45.8 MB (83.09%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.java
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.java
@@ -3,13 +3,15 @@ class Solution {
         int n = nums.length;
         int[] res = new int[n];
         for (int i = 0; i < n; i++) {
-            res[i] = -1;
+            int curr_max = nums[i];
             for (int j = i + 1; j < n * 2; j++) {
                 if (nums[j % n] > nums[i]) {
-                    res[i] = nums[j % n];
+                    curr_max = nums[j % n];
                     break;
                 }
             }
+            if (curr_max == nums[i]) res[i] = -1;
+            else res[i] = curr_max;
         }
         return res;
     }

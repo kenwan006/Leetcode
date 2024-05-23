b0b12aa6a3c3754b3669ed0dd7e3f9c4a8db8760
Ken <42952910+kenwan006@users.noreply.github.com>
2024-05-23 10:52:36 -0700

    Time: 4 ms (95.39%), Space: 45.4 MB (45.07%) - LeetHub

--- 0271-encode-and-decode-strings/0271-encode-and-decode-strings.java
+++ 0271-encode-and-decode-strings/0271-encode-and-decode-strings.java
@@ -2,27 +2,27 @@ public class Codec {
 
     // Encodes a list of strings to a single string.
     public String encode(List<String> strs) {
-        //use word length + a divider '#' to seperate the words from each other
-        StringBuilder sb = new StringBuilder();
+        StringBuilder encoder = new StringBuilder();
         for (String s : strs) {
-            sb.append(s.length()).append('#');
-            sb.append(s);
+            encoder.append(s.length()).append('#').append(s);
         }
-        return sb.toString();
+        return encoder.toString();
     }
 
     // Decodes a single string to a list of strings.
     public List<String> decode(String s) {
-        List<String> res = new ArrayList<>();
+        List<String> list = new ArrayList<>();
         int i = 0, n = s.length();
         while (i < n) {
-            int j = i; //find the length of a string
+            //find the j - s[i..j) is the length of next string
+            int j = i;
             while (j < n && s.charAt(j) != '#') j++;
             int len = Integer.parseInt(s.substring(i, j));
-            i = j + 1 + len; //next start
-            res.add(s.substring(j + 1, i));
+            i = j + 1;
+            list.add(s.substring(i, i + len));
+            i = i + len;
         }
-        return res;
+        return list;
     }
 }
 

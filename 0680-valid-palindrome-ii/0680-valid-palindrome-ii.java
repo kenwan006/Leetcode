b380cfe61be67f57760b4328b49800d3ac0fe6b6
Ken <42952910+kenwan006@users.noreply.github.com>
2024-05-24 18:03:11 -0700

    Time: 4 ms (99.71%), Space: 45.6 MB (8.71%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
@@ -1,23 +1,21 @@
 class Solution {
     public boolean validPalindrome(String s) {
         int i = 0, j = s.length() - 1;
-        while (i < j) {
-            if (s.charAt(i) == s.charAt(j)) {
-                i++;
-                j--;
-            } else {
-                return isPalindrome(s, i, j - 1) || isPalindrome(s, i + 1, j);
-            }
-        }
-        return true;
-    }
-    
-    private boolean isPalindrome(String s, int i, int j) {
-        while (i < j) {
-            if (s.charAt(i) != s.charAt(j)) return false;
+        while (i < j && s.charAt(i) == s.charAt(j)) {
             i++;
             j--;
         }
+        
+        if (i >= j) return true; // no need to delete any char as s is arleady palindrome
+        
+        return isPalindrome(s.substring(i, j)) || isPalindrome(s.substring(i + 1, j + 1));
+    }
+    
+    private boolean isPalindrome(String s) {
+        int i = 0, j = s.length() - 1;
+        while (i < j) {
+            if (s.charAt(i++) != s.charAt(j--)) return false;
+        }
         return true;
-    }    
+    }
 }

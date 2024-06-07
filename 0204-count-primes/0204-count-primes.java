ce9b468eda4b4d99339dd031e4593b2674609173
Ken <42952910+kenwan006@users.noreply.github.com>
2024-06-07 11:15:12 -0700

    Time: 183 ms (12.78%), Space: 64 MB (13.70%) - LeetHub

--- 0204-count-primes/0204-count-primes.java
+++ 0204-count-primes/0204-count-primes.java
@@ -1,26 +1,22 @@
 class Solution {
     public int countPrimes(int n) {
-        int[]dp = new int[n]; // dp[i] - 1 means i is prime, -1 means non prime
+        int[] dp = new int[n]; // dp[i] = 1 if i is prime, 0 if it's not
         Arrays.fill(dp, 1);
         
         for (int i = 2; i < n; i++) {
-            if (dp[i] == -1) continue;
+            if (dp[i] == 0) continue;
             
-            //all the number 2i, 3i, 4i...are non primes
+            //set k * i to 0
             for (int k = 2; k * i < n; k++) {
-                dp[k * i] = -1;
+                dp[k * i] = 0;
             }
-            
         }
         
-        int count = 0;
+        //count the prime
+        int res = 0;
         for (int i = 2; i < n; i++) {
-            if (dp[i] == 1) count++;
+            if (dp[i] == 1) res++;
         }
-        return count;
-        
+        return res;
     }
 }
-
-//Time complexity: O(n/2 + n/3 + n/5 + n/7 +...+ n/largest prime smaller than n) < O(n * ln(n)) 
-//Space complexity: O(n)

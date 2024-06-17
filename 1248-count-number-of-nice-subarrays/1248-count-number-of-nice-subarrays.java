4305351821a34aff8d113de807f1b861c5a5a225
Ken <42952910+kenwan006@users.noreply.github.com>
2024-06-17 11:05:22 -0700

    Time: 14 ms (36.80%), Space: 55.1 MB (28.07%) - LeetHub

--- 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.java
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.java
@@ -1,20 +1,22 @@
 class Solution {
     public int numberOfSubarrays(int[] nums, int k) {
-        //change all even nums to 0, and odd to 1
-        int n = nums.length;
-        for (int i = 0; i < n; i++) nums[i] = nums[i] % 2 == 1? 1 : 0;
-        
-        //calculate the prefix sum and use a hashmap to store its freq
-        Map<Integer, Integer> map = new HashMap<>(); 
-        int res = 0;
-        int prefix = 0;
-        for (int num : nums) {
-            prefix += num;
-            if (prefix == k) res++;
-            res += map.getOrDefault(prefix - k, 0);
-            map.put(prefix, map.getOrDefault(prefix, 0) + 1); //update the freq of this freq in map
-        }
-        
-        return res;
+        return atMost(nums, k) - atMost(nums, k - 1);
     }
+    
+    //number of subarrys which contain at most k odd number
+    private int atMost(int[] nums, int k) {
+        int i = 0;
+        int res = 0;
+        int odds = 0;
+        for (int j = 0; j < nums.length; j++) {
+            if (nums[j] % 2 == 1) odds++;
+            while (odds > k) {
+                if (nums[i++] % 2 == 1) odds--;
+            }
+            //we maintain a sliding window [i..j]. within it, each subarray ending with nums[j] have at most k odd numbers
+            //count of such subarray ending with nums[j] is j - i + 1;
+            res += (j - i + 1);
+        }
+        return res;
+    }  
 }

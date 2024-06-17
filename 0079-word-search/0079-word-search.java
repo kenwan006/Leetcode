aa0732aa802761772a46dea95e0a7044a5653824
Ken <42952910+kenwan006@users.noreply.github.com>
2024-06-17 14:52:50 -0700

    Time: 228 ms (17.07%), Space: 41.9 MB (24.33%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,45 +1,34 @@
 class Solution {
     boolean[][] visited;
     public boolean exist(char[][] board, String word) {
-        TrieNode root = new TrieNode();
-        TrieNode curr = root;
-        for (char c : word.toCharArray()) {
-            int idx = c - 'A';
-            curr.children[idx] = new TrieNode();
-            curr = curr.children[idx];
-        }
-        curr.isEnd = true;
-        
         int m = board.length, n = board[0].length;
         visited = new boolean[m][n];
+        
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                if (dfs(i, j, board, root)) return true;
+                if (dfs(board, i, j, word, 0)) return true;
             }
         }
         return false;
     }
     
-    int[][] dirs = {{-1, 0}, {1, 0}, {0, 1}, {0, -1}};
-    private boolean dfs(int i, int j, char[][] board, TrieNode curr) {
-        if (i < 0 || i >= board.length || j < 0 || j >= board[0].length || visited[i][j]) return false;
-        int idx = board[i][j] - 'A';
-        if (curr.children[idx] == null) return false;
-        curr = curr.children[idx];
-        if (curr.isEnd) return true;
-  
+    int[][] dirs = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+    
+    private boolean dfs(char[][] board, int i, int j, String word, int idx) {
+        if (idx == word.length()) return true; 
+        
+        if (i < 0 || j < 0 || i >= board.length || j >= board[0].length) return false;
+        if (visited[i][j] || board[i][j] != word.charAt(idx)) return false;
+        
         visited[i][j] = true;
+        
         for (int[] dir : dirs) {
-            if (dfs(i + dir[0], j + dir[1], board, curr)) return true;
+            int ii = i + dir[0], jj = j + dir[1];
+            if (dfs(board, ii, jj, word, idx + 1)) return true;
         }
+        
         visited[i][j] = false;
         
         return false;
-    }
-    
-    class TrieNode{
-        TrieNode[] children = new TrieNode[58];
-        boolean isEnd;
-        public TrieNode(){};
     }
 }

a0e6c3dc7b905390a91bffafdfb0a09832995e05
Ken <42952910+kenwan006@users.noreply.github.com>
2024-06-18 14:58:43 -0700

    Time: 9 ms (54.45%), Space: 46.6 MB (31.54%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,24 +1,22 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
+        Arrays.sort(intervals, (a, b)-> a[0] - b[0]);
         
-        List<int[]> list = new ArrayList<>();
+        List<int[]> res = new ArrayList<>();
         int start = intervals[0][0], end = intervals[0][1];
+        
         for (int[] interval : intervals) {
             if (end < interval[0]) {
-                list.add(new int[]{start, end});
+                res.add(new int[]{start, end});
                 start = interval[0];
                 end = interval[1];
             } else {
-                start = Math.min(start, interval[0]);
                 end = Math.max(end, interval[1]);
             }
         }
-        list.add(new int[]{start, end});
         
-        //convert list to array
-        int[][] res = list.toArray(new int[list.size()][2]);
-        return res;
+        res.add(new int[]{start, end});
         
+        return res.toArray(new int[res.size()][2]);
     }
 }

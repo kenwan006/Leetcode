542d1d3cd7b2fff1262126c90f1688746fa18ecf
Ken <42952910+kenwan006@users.noreply.github.com>
2024-06-29 18:55:40 -0700

    Time: 17 ms (77.09%), Space: 48.2 MB (71.94%) - LeetHub

--- 0706-design-hashmap/0706-design-hashmap.java
+++ 0706-design-hashmap/0706-design-hashmap.java
@@ -1,21 +1,53 @@
 class MyHashMap {
-    int[] bucket;
+    List<Node>[] buckets;
+    int bucketNum = 2069;
     public MyHashMap() {
-        bucket = new int[1000001];
-        Arrays.fill(bucket, -1); //-1 mean this bucket is empty
+        buckets = new List[bucketNum];
     }
     
     public void put(int key, int value) {
-        bucket[key] = value;
+        int bucketIdx = key % bucketNum;
+        if (buckets[bucketIdx] == null) buckets[bucketIdx] = new ArrayList<>();
+        for (Node node : buckets[bucketIdx]) {
+            if (node.key == key) {
+                node.value = value;
+                return;
+            }
+        }
+        buckets[bucketIdx].add(new Node(key, value));
     }
     
     public int get(int key) {
-        return bucket[key];
+        int bucketIdx = key % bucketNum;
+        List<Node> bucket = buckets[bucketIdx];
+        if (bucket == null) return -1;
+        for (Node node : bucket) {
+            if (node.key == key) return node.value;
+        }
+        return -1;
     }
     
     public void remove(int key) {
-        bucket[key] = -1;
+        int bucketIdx = key % bucketNum;
+        List<Node> bucket = buckets[bucketIdx];
+        if (bucket == null) return;
+        for (Node node : buckets[bucketIdx]) {
+            if (node.key == key) {
+                buckets[bucketIdx].remove(node);
+                return;
+            }
+        }
     }
+    
+    class Node {
+        int key;
+        int value;
+        public Node(int key, int value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+    
 }
 
 /**

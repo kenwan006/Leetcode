6f7732935845ecd004ac7138b95261e98b04411b
Ken <42952910+kenwan006@users.noreply.github.com>
2024-07-10 10:40:30 -0700

    Time: 3 ms (34.01%), Space: 44.5 MB (40.65%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,27 +1,28 @@
 class Solution {
-    List<List<Integer>> res = new LinkedList<>();
-    LinkedList<Integer> track = new LinkedList<>();
-    int sum = 0;
-    
+    List<List<Integer>> res;
+    LinkedList<Integer> track;
+    int trackSum = 0;
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        backtrack(candidates, target, 0);
+        res = new LinkedList<>();
+        track = new LinkedList<>();
+        backtrack(candidates, 0, target);
         return res;
     }
     
-    private void backtrack(int[] candidates, int target, int start) {
-        if (sum > target) return;
-        if (sum == target) {
+    private void backtrack(int[] candidates, int start, int target) {
+        if (trackSum == target) {
             res.add(new LinkedList(track));
             return;
-        }
+        } 
+        if (trackSum > target) return;
         
         for (int i = start; i < candidates.length; i++) {
-            int num = candidates[i];
-            sum += num;
-            track.add(num);
-            backtrack(candidates, target, i);
+            int c = candidates[i];
+            trackSum += c;
+            track.add(c);
+            backtrack(candidates, i, target);
+            trackSum -= c;
             track.removeLast();
-            sum -= num;
         }
     }
 }

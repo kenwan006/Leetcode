5dcd507ce615e399fe1c90677ca57f2f7976246f
Ken <42952910+kenwan006@users.noreply.github.com>
2024-07-10 10:56:44 -0700

    Time: 204 ms (22.87%), Space: 41.4 MB (52.49%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,32 +1,28 @@
 class Solution {
-    boolean[][] visited;
+    private boolean[][] visited;
+    int[][] dirs = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+    
     public boolean exist(char[][] board, String word) {
         int m = board.length, n = board[0].length;
         visited = new boolean[m][n];
-        
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
+                if (board[i][j] != word.charAt(0)) continue;
                 if (dfs(board, i, j, word, 0)) return true;
             }
         }
         return false;
     }
-    
-    int[][] dirs = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
-    
+
     private boolean dfs(char[][] board, int i, int j, String word, int idx) {
-        if (idx == word.length()) return true; 
-        
+        if (idx == word.length()) return true;
         if (i < 0 || j < 0 || i >= board.length || j >= board[0].length) return false;
-        if (visited[i][j] || board[i][j] != word.charAt(idx)) return false;
+        if (board[i][j] != word.charAt(idx) || visited[i][j]) return false;
         
         visited[i][j] = true;
-        
         for (int[] dir : dirs) {
-            int ii = i + dir[0], jj = j + dir[1];
-            if (dfs(board, ii, jj, word, idx + 1)) return true;
+            if (dfs(board, i + dir[0], j + dir[1], word, idx + 1)) return true;
         }
-        
         visited[i][j] = false;
         
         return false;

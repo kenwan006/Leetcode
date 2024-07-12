85eeff277c058e27d627ee8870863c336c66bb61
Ken <42952910+kenwan006@users.noreply.github.com>
2024-07-12 15:04:51 -0700

    Time: 24 ms (95.14%), Space: 42.8 MB (29.52%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.java
+++ 0133-clone-graph/0133-clone-graph.java
@@ -21,19 +21,21 @@ class Node {
 class Solution {
     public Node cloneGraph(Node node) {
         if (node == null) return null;
+        
         Map<Node, Node> map = new HashMap<>();
+        map.put(node, new Node(node.val));
+        
         Queue<Node> queue = new LinkedList<>();
-        map.put(node, new Node(node.val, new ArrayList<Node>()));
         queue.offer(node);
         
-        while(!queue.isEmpty()) {
+        while (!queue.isEmpty()) {
             Node curr = queue.poll();
-            for (Node neighbor : curr.neighbors) {
-                if (!map.containsKey(neighbor)) {
-                    queue.offer(neighbor);
-                    map.put(neighbor, new Node(neighbor.val, new ArrayList<Node>()));
+            for (Node adj : curr.neighbors) {
+                if (!map.containsKey(adj)) {
+                    map.put(adj, new Node(adj.val));
+                    queue.offer(adj);
                 }
-                map.get(curr).neighbors.add(map.get(neighbor));
+                map.get(curr).neighbors.add(map.get(adj));
             }
         }
         return map.get(node);

29846016db9ddaf423f406401bf719b19f08a89b
Ken <42952910+kenwan006@users.noreply.github.com>
2024-07-12 15:35:45 -0700

    Time: 4 ms (97.32%), Space: 46.3 MB (6.01%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.java
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.java
@@ -1,42 +1,36 @@
-/** dfs **/
 class Solution {
     public List<List<Integer>> pacificAtlantic(int[][] heights) {
         int m = heights.length, n = heights[0].length;
-        boolean[][] pac = new boolean[m][n]; //pac[i][j] - means water at (i, j) can flow to pacific
-        boolean[][] atl = new boolean[m][n];
         
-        //flow from left boundary and right boundary
-        for (int i = 0; i < m; i++) {
-            dfs(heights, i, 0, pac, 0);
-            dfs(heights, i, n - 1, atl, 0);
-        }
+        boolean[][] pac_reachable = new boolean[m][n];
+        boolean[][] atl_reachable = new boolean[m][n];
         
-        // flow from top boundary and bottom boundary
-        for (int j = 0; j < n; j++) {
-            dfs(heights, 0, j, pac, 0);
-            dfs(heights, m - 1, j, atl, 0);
-        }
+        //let's flow from pacific back to island
+        for (int j = 0; j < n; j++) dfs(heights, pac_reachable, 0, j, 0);
+        for (int i = 0; i < m; i++) dfs(heights, pac_reachable, i, 0, 0);
         
-        //find the cell that can flow to both pacific and atlantic
+        //let's flow from atlantic back to island
+        for (int j = 0; j < n; j++) dfs(heights, atl_reachable, m - 1, j, 0);
+        for (int i = 0; i < m; i++) dfs(heights, atl_reachable, i, n - 1, 0);
+        
         List<List<Integer>> res = new LinkedList<>();
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                if (pac[i][j] && atl[i][j]) res.add(Arrays.asList(i, j));
+                if (pac_reachable[i][j] && atl_reachable[i][j]) res.add(Arrays.asList(i, j));
             }
         }
+        
         return res;
     }
     
-    int[][] dirs = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
-    public void dfs(int[][] heights, int i, int j, boolean[][] reachable, int preHeight) {
-        if (i < 0 || j < 0 || i >= heights.length || j >= heights[0].length) return;
-        if (heights[i][j] < preHeight || reachable[i][j]) return; 
+    private int[][] dirs = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+    
+    private void dfs(int[][] heights, boolean[][] reachable, int i, int j, int pre_height) {
+        if (i < 0 || i >= heights.length || j < 0 || j >= heights[0].length || heights[i][j] < pre_height || reachable[i][j]) return;
         
         reachable[i][j] = true;
-        
         for (int[] dir : dirs) {
-            dfs(heights, i + dir[0], j + dir[1], reachable, heights[i][j]);
+            dfs(heights, reachable, i + dir[0], j + dir[1], heights[i][j]);
         }
     }
 }
-//Time: O(m * n); Space: O(m * n)

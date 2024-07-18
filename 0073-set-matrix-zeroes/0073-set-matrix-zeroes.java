24c333c6c5063233870855fd6b483bab3cea4a5e
Ken <42952910+kenwan006@users.noreply.github.com>
2024-07-18 16:51:28 -0700

    Time: 0 ms (100.00%), Space: 44.7 MB (99.88%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,47 +1,43 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
-        //use row 0 and column 0 as two indicators
-        
-        //check if row 0 and column 0 already includes 0
-        boolean inRow = false, inCol = false;
+        boolean top = false, left = false;
         int m = matrix.length, n = matrix[0].length;
+        
+        //check if top row or left column already has 0
         for (int i = 0; i < m; i++) {
-            if (matrix[i][0] == 0) {
-                inCol = true;
-                break;
-            }
-        }
-        for (int j = 0; j < n; j++) {
-            if (matrix[0][j] == 0) {
-                inRow = true;
-                break;
-            }
+            if (matrix[i][0] == 0) left = true; 
         }
         
-        //update the indicators
+        for (int j = 0; j < n; j++) {
+            if (matrix[0][j] == 0) top = true;
+        }
+        
+        //use the top row and left column to store the value that if this row or column has 0
         for (int i = 1; i < m; i++) {
             for (int j = 1; j < n; j++) {
                 if (matrix[i][j] == 0) {
-                    matrix[i][0] = 0; //column 0 indicator
-                    matrix[0][j] = 0; // row 0 indicator
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
                 }
             }
         }
         
-        //set zeros based on the indicators
+        //set the whole row or whole column to 0 as needed
         for (int i = 1; i < m; i++) {
-            for (int j = 1; j < n; j++) {
-                if (matrix[0][j] == 0 || matrix[i][0] == 0) matrix[i][j] = 0;
-            }
+            if (matrix[i][0] != 0) continue;
+            for (int j = 1; j < n; j++) matrix[i][j] = 0;
+        }
+        for (int j = 1; j < n; j++) {
+            if (matrix[0][j] != 0) continue;
+            for (int i = 1; i < m; i++) matrix[i][j] = 0;
         }
         
-        //set row 0 and column 0 
-        if (inRow) {
-            for (int j = 0; j < n; j++) matrix[0][j] = 0; //set the row 0 to zeros
+        //set the top row and left column to 0 as needed
+        if (top) {
+            for (int j = 0; j < n; j++) matrix[0][j] = 0;
         }
-        if (inCol) {
-            for (int i = 0; i < m; i++) matrix[i][0] = 0; //set the column 0 to zeros
+        if (left) {
+            for (int i = 0; i < m; i++) matrix[i][0] = 0;
         }
-        
     }
 }

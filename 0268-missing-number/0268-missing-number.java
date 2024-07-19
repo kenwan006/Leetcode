7e61303b1b01021e2aafe480920c344bac53be68
Ken <42952910+kenwan006@users.noreply.github.com>
2024-07-18 17:34:42 -0700

    Time: 0 ms (100.00%), Space: 45.6 MB (9.72%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,11 +1,11 @@
 class Solution {
     public int missingNumber(int[] nums) {
+        //a ^ b = b ^ a, a ^ b ^ c = a ^ c ^ b...
+        int res = 0;
         int n = nums.length;
-        int diff = n;
-        for (int i = 0; i < n; i++) {
-            diff += (i - nums[i]);
-        }
-        return diff;
+        for (int i = 0; i <= n; i++) res ^= i;
+        for (int num : nums) res ^= num;
         
+        return res;
     }
 }

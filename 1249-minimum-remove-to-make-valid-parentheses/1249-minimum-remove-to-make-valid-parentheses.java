59589a1a9be8e0718f692a5978253e5f864beb16
Ken <42952910+kenwan006@users.noreply.github.com>
2024-07-30 17:05:30 -0700

    Time: 28 ms (25.64%), Space: 45.3 MB (69.12%) - LeetHub

--- 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.java
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.java
@@ -1,24 +1,21 @@
 class Solution {
     public String minRemoveToMakeValid(String s) {
-        Stack<Integer> stack = new Stack<>(); //indices of parentheis not removed yet
-       
-        for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-            if (Character.isLetter(c)) continue;
-            if (c == '(') stack.push(i);
-            if (c == ')') {
-                if (!stack.isEmpty() && s.charAt(stack.peek()) == '(') stack.pop();
-                else stack.push(i);
-            }  
+        Stack<Character> stack = new Stack<>();
+        int count = 0; // remaining '('
+        for (char c : s.toCharArray()) {
+            if (c == '(') count++;
+            if (c == ')') count--;
+            
+            if (count >= 0) stack.push(c);
+            else count = 0;  //we should not have ')' more than '('
         }
-        
-        Set<Integer> set = new HashSet<>(); //indices of parentheis that has to be removed
-        while (!stack.isEmpty()) set.add(stack.pop());
         
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < s.length(); i++) {
-            if (!set.contains(i)) sb.append(s.charAt(i));
+        while (!stack.isEmpty()) {
+            char c = stack.pop();
+            if (c == '(' && count > 0) count--; //remove the remaining '('
+            else sb.append(c);
         }
-        return sb.toString();
+        return sb.reverse().toString();
     }
 }
